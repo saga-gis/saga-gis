7ebe4eeb782e3cc6ea653399d81e4a5530f976af
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2007-02-27 13:46:10 +0000

    no message

--- saga_2/src/saga_core/saga_gui/saga_frame.cpp
+++ saga_2/src/saga_core/saga_gui/saga_frame.cpp
@@ -787,7 +787,7 @@ void CSAGA_Frame::Set_Project_Name(wxString Project_Name)
 {
 	if( Project_Name.Length() > 0 )
 	{
-		SetTitle(wxString::Format("%s [%s]", SAGA_CAPTION, Project_Name.c_str()));
+		SetTitle(wxString::Format(wxT("%s [%s]"), SAGA_CAPTION, Project_Name.c_str()));
 	}
 	else
 	{
