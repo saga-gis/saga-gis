3f03f38f33c3e3a5465e6afdae529a2bd3ea5a37
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2007-06-08 13:02:00 +0000

    no message

--- saga_2/src/saga_core/saga_api/saga_api_to_python_win.bat
+++ saga_2/src/saga_core/saga_api/saga_api_to_python_win.bat
@@ -1,18 +1,15 @@
 @echo off
 
-rem set SWIG_PATH=D:\tools\develop\swigwin-1.3.25
-set SWIG_PATH=%SWIG%
-
-rem set PYTHON_PATH=C:\Program Files\Python23
-set PYTHON_PATH=C:\Programme\Python23
+rem set SWIG=C:\Program Files\swigwin-1.3.25
+rem set PYTHON=C:\Program Files\Python23
 
 echo SWIG compilation...
-"%SWIG_PATH%\swig" -c++ -python -includeall saga_api.h
+"%SWIG%\swig" -c++ -python -includeall saga_api.h
 echo SWIG compilation finished.
 
 echo Python compilation...
-"%PYTHON_PATH%\python.exe" saga_api_to_python_win.py install
-copy saga_api.py "%PYTHON_PATH%\Lib\site-packages\saga_api.py"
+"%PYTHON%\python.exe" saga_api_to_python_win.py install
+copy saga_api.py "%PYTHON%\Lib\site-packages\saga_api.py"
 echo Python compilation finished.
 
 pause
