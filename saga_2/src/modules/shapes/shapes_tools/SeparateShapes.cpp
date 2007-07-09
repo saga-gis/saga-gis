cb76f4c5aa9669a848dd406683111aa5e3de9111
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2007-07-09 14:37:22 +0000

    no message

--- saga_2/src/modules/shapes/shapes_tools/SeparateShapes.cpp
+++ saga_2/src/modules/shapes/shapes_tools/SeparateShapes.cpp
@@ -19,93 +19,84 @@
 
 #include "SeparateShapes.h"
 
-#define NAMING_NUMERIC 0
-#define NAMING_FIELD 1
+#define NAMING_NUMERIC	0
+#define NAMING_FIELD	1
 
-CSeparateShapes::CSeparateShapes(void){
-
+CSeparateShapes::CSeparateShapes(void)
+{
 	CSG_Parameter *pNode;
+
+	Set_Name		(_TL("Separate Shapes"));
+	Set_Author		(_TL("(c) 2005 by Victor Olaya"));
+	Set_Description	(_TW("Separate Shapes."));
+
+	pNode = Parameters.Add_Shapes(
+		NULL	, "SHAPES"	, _TL("Shapes"),
+		_TL(""),
+		PARAMETER_INPUT
+	);
 	
-	Parameters.Set_Name(_TL("Separate Shapes"));
-	Parameters.Set_Description(_TW("(c) 2005 by Victor Olaya."));
+	Parameters.Add_FilePath(
+		NULL	, "PATH"	, _TL("shp files folder"),
+		_TL("shp files folder"),
+		_TL(""), _TL(""), true, true
+	);
 
-	pNode = Parameters.Add_Shapes(NULL, 
-						"INPUT", 
-						_TL("Shapes"), 
-						_TL(""), 
-						PARAMETER_INPUT);
+	Parameters.Add_Choice(
+		NULL	, "NAMING"	, _TL("File Naming"),
+		_TL(""),
 
-	Parameters.Add_Table_Field(pNode,
-								"FIELD",
-								_TL("Field"),
-								_TL(""));
-	
-	Parameters.Add_FilePath(NULL,
-							"OUTPUTPATH",
-							_TL("shp files folder"),
-							_TL("shp files folder"),
-							_TL(""),
-							_TL(""),
-							true,
-							true);
+		CSG_String::Format(SG_T("%s|%s|"),
+			_TL("By number"),
+			_TL("Use field content")
+		), 0
+	);
 
-	Parameters.Add_Choice(NULL, 
-						"NAMING",
-						_TL("File Naming"),
-						_TL(""),
-						_TW("By number|" 
-						"Use field content|"), 
-						0);
-
-}//constructor
-
+	Parameters.Add_Table_Field(
+		pNode	, "FIELD"	, _TL("Field for File Naming"),
+		_TL("")
+	);
+}
 
 CSeparateShapes::~CSeparateShapes(void)
 {}
 
+bool CSeparateShapes::On_Execute(void)
+{
+	int			iNaming, iField;
+	CSG_String	sPath, sName, sFile;
+	CSG_Shapes	*pShapes, *pShape;
 
-bool CSeparateShapes::On_Execute(void){
+	pShapes	= Parameters("SHAPES")	->asShapes();
+	iNaming	= Parameters("NAMING")	->asInt();
+	iField	= Parameters("FIELD")	->asInt();
+	sPath	= Parameters("PATH")	->asString();
 
-	int i,j;
-	int iNaming = Parameters("NAMING")->asInt();
-	int iField = Parameters("FIELD")->asInt();
-	CSG_Shapes *pInput = Parameters("INPUT")->asShapes();
-	CSG_Shapes *pOutput; 
-	CSG_String sPath = Parameters("OUTPUTPATH")->asString();;
-	CSG_String sFilename;
-	CSG_String sName;
-	CSG_Table *pTable;
-
-	pTable	= &pInput->Get_Table();
-
-	if( !SG_Dir_Create(sPath) )
+	if( SG_Dir_Create(sPath) )
 	{
-		return( false );
+		for(int iShape=0; iShape<pShapes->Get_Count(); iShape++)
+		{
+			switch( iNaming )
+			{
+			case NAMING_NUMERIC:	default:
+				sName.Printf(SG_T("%s_%04d"), SG_File_Get_Name(pShapes->Get_Name(), false).c_str(), iShape + 1);
+				break;
+
+			case NAMING_FIELD:
+				sName.Printf(SG_T("%s_%s")	, SG_File_Get_Name(pShapes->Get_Name(), false).c_str(), pShapes->Get_Table().Get_Record(iShape)->asString(iField));
+				break;
+			}
+
+			sFile	= SG_File_Make_Path(sPath, sName, SG_T("shp"));
+			pShape	= SG_Create_Shapes(pShapes->Get_Type(), sName, &pShapes->Get_Table());
+			pShape	->Add_Shape(pShapes->Get_Shape(iShape), true);
+			pShape	->Save(sFile);
+
+			delete(pShape);
+		}
+
+		return( true );
 	}
- 
-	for (i = 0; i < pInput->Get_Count(); i++){		
-		if (iNaming == NAMING_NUMERIC){
-			sName.Printf(SG_T("%s_%s"), pInput->Get_Name(), SG_Get_String(i).c_str());
-		}//if
-		else{
-			sName.Printf(SG_T("%s"), pTable->Get_Record(i)->asString(iField));
-		}//else
 
-		pOutput = SG_Create_Shapes(pInput->Get_Type());
-		pOutput->Set_Name(sName.c_str());
-
-		sFilename	= SG_File_Make_Path(sPath, sName, SG_T("shp"));
-		
-		for (j=0; j<pTable->Get_Field_Count(); j++){
-			pOutput->Get_Table().Add_Field(pTable->Get_Field_Name(j), pTable->Get_Field_Type(j));	
-		}//for
-
-		pOutput->Add_Shape()->Assign(pInput->Get_Shape(j));
-		pOutput->Save(sFilename.c_str());
-
-		delete pOutput;
-	}//for
-	
-	return true;
-
-}//method
+	return( false );
+}
