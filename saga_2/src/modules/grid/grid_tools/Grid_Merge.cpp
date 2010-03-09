178a2f46ddc4e8bd62d4697197c718278408eca4
Volker Wichmann <wichmann@laserdata.at>
2010-03-09 22:43:49 +0000

    fix previous change "Fix memory leak"

--- saga_2/src/modules/grid/grid_tools/Grid_Merge.cpp
+++ saga_2/src/modules/grid/grid_tools/Grid_Merge.cpp
@@ -385,8 +385,9 @@ bool CGrid_Merge::On_Execute(void)
                         pMerged->Set_Value(x, y, pMerged->asDouble(x, y) / pCount->asInt(x, y));
                 }
             }
+			delete (pCount);
         }
-	pCount->Destroy();
+
 		return( true );
 	}
 
