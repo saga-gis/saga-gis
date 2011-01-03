220a787c3ed459a3ba0818cfe141ba6ff74044e0
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2011-01-03 15:34:30 +0000

    principal components analysis for tables added

--- saga_2/src/modules/table/table_calculus/table_pca.h
+++ saga_2/src/modules/table/table_calculus/table_pca.h
@@ -6,14 +6,14 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                    Table_Calculus                     //
+//                  Geostatistics_Grid                   //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                      table_pca.h                      //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
-//                    Andre Ringeler                     //
+//                 Copyright (C) 2010 by                 //
+//                     Jan Papmeier                      //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
@@ -37,100 +37,68 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     aringel@gwdg.de                        //
 //                                                       //
-//    contact:    Andre Ringeler                         //
-//                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
-//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                                                       //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-
-
-///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
-///////////////////////////////////////////////////////////
+#ifndef HEADER_INCLUDED__table_pca_H
+#define HEADER_INCLUDED__table_pca_H
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
-
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-const SG_Char * Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Table - Calculus") );
-
-	case MLB_INFO_Author:
-		return( _TL("SAGA User Group Associaton (c) 2002") );
-
-	case MLB_INFO_Description:
-		return( _TL("") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Table|Calculus") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "Fit.h"
-#include "Table_Calculator.h"
-#include "Table_CalculatorShapes.h"
-#include "Table_Trend.h"
-#include "table_running_average.h"
-#include "table_running_average.h"
-#include "table_cluster_analysis.h"
-#include "table_pca.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case 0:		return( new CFit );
-	case 1:		return( new CTableCalculator );
-	case 2:		return( new CTableCalculatorShapes );
-	case 3:		return( new CTable_Trend );
-	case 4:		return( new CTable_Trend_Shapes );
-	case 5:		return( new CTable_Running_Average );
-	case 6:		return( new CTable_Cluster_Analysis );
-	case 7:		return( new CTable_PCA );
-	}
-
-	return( NULL );
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //
+//                                                       //
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CTable_PCA : public CSG_Module
+{
+public:
+	CTable_PCA(void);
 
-	MLB_INTERFACE
 
-//}}AFX_SAGA
+protected:
+
+	virtual int				On_Parameter_Changed	(CSG_Parameters *pParameters, CSG_Parameter *pParameter);
+
+	virtual bool			On_Execute				(void);
+
+
+private:
+
+	int						m_Method, m_nFeatures, *m_Features;
+
+	CSG_Table				*m_pTable;
+
+
+	bool					Get_Fields				(void);
+
+	bool					is_NoData				(int iElement);
+	double					Get_Value				(int iFeature, int iElement);
+	
+	bool					Get_Matrix				(CSG_Matrix &Matrix);
+
+	bool					Get_Components			(CSG_Matrix &Eigen_Vectors, CSG_Vector &Eigen_Values);
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                                                       //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__table_pca_H
