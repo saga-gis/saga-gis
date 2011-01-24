6285747e2b72f813227f437da18e84ebecce4222
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2011-01-24 11:33:07 +0000

    vegetation index tools moved from grid_analysis library

---
+++ saga_2/src/modules_images/modules_imagery/imagery_tools/Image_VI_Distance.h
@@ -0,0 +1,47 @@
+/*******************************************************************************
+    Image_VI_Distance.h
+    Copyright (C) Victor Olaya
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*******************************************************************************/ 
+
+//---------------------------------------------------------
+#ifndef HEADER_INCLUDED__Image_VI_Distance_H
+#define HEADER_INCLUDED__Image_VI_Distance_H
+
+//---------------------------------------------------------
+#include "MLB_Interface.h"
+
+//---------------------------------------------------------
+class CImage_VI_Distance : public CSG_Module_Grid {
+
+public:
+	CImage_VI_Distance(void);
+	virtual ~CImage_VI_Distance(void);
+
+	virtual const SG_Char *	Get_MenuPath	(void)	{	return( _TL("R:Image Analysis") );	}
+
+
+protected:
+	virtual bool On_Execute(void);
+
+private:
+	double getPVI(double,double,double,double);
+	double getPVI1(double,double,double,double);
+	double getPVI2(double,double,double,double);
+	double getPVI3(double,double,double,double);
+};
+
+#endif // #ifndef HEADER_INCLUDED__Image_VI_Distance_H
---
+++ saga_2/src/modules_images/modules_imagery/imagery_tools/MLB_Interface.cpp
@@ -0,0 +1,120 @@
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                    Module Library:                    //
+//                    imagery_tools                      //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//                   MLB_Interface.cpp                   //
+//                                                       //
+//                 Copyright (C) 2009 by                 //
+//                 SAGA User Group Assoc.                //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation; version 2 of the License.   //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not,       //
+// write to the Free Software Foundation, Inc.,          //
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307,   //
+// USA.                                                  //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     author@email.de                        //
+//                                                       //
+//    contact:    Author                                 //
+//                Sesame Street. 7                       //
+//                12345 Metropolis                       //
+//                Nirwana                                //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//			The Module Link Library Interface			 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+// 1. Include the appropriate SAGA-API header...
+
+#include "MLB_Interface.h"
+
+
+//---------------------------------------------------------
+// 2. Place general module library informations here...
+
+const SG_Char * Get_Info(int i)
+{
+	switch( i )
+	{
+	case MLB_INFO_Name:	default:
+		return( _TL("Imagery - Tools") );
+
+	case MLB_INFO_Author:
+		return( SG_T("SAGA User Group Assoc. (c) 2009") );
+
+	case MLB_INFO_Description:
+		return( _TL("Image processing tools.") );
+
+	case MLB_INFO_Version:
+		return( SG_T("1.0") );
+
+	case MLB_INFO_Menu_Path:
+		return( _TL("Imagery|Tools") );
+	}
+}
+
+
+//---------------------------------------------------------
+// 3. Include the headers of your modules here...
+
+#include "Image_VI_Distance.h"
+#include "Image_VI_Slope.h"
+
+
+//---------------------------------------------------------
+// 4. Allow your modules to be created here...
+
+CSG_Module *		Create_Module(int i)
+{
+	switch( i )
+	{
+	case  0:	return( new CImage_VI_Distance );
+	case  1:	return( new CImage_VI_Slope );
+	}
+
+	return( NULL );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+//{{AFX_SAGA
+
+	MLB_INTERFACE
+
+//}}AFX_SAGA
