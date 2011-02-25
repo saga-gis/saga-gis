7c92e2c4fb2492255fd6ea616d9b3900a5ce5470
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2011-02-25 10:36:35 +0000

    points thinning module added

---
+++ saga-gis/src/modules/shapes/shapes_points/points_thinning.cpp
@@ -0,0 +1,309 @@
+/**********************************************************
+ * Version $Id: points_thinning.cpp 911 2011-02-14 16:38:15Z reklov_w $
+ *********************************************************/
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                    Module Library:                    //
+//                     shapes_points                     //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//                  points_thinning.cpp                  //
+//                                                       //
+//                 Copyright (C) 2011 by                 //
+//                      Olaf Conrad                      //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation; version 2 of the License.   //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not,       //
+// write to the Free Software Foundation, Inc.,          //
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307,   //
+// USA.                                                  //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     oconrad@saga-gis.org                   //
+//                                                       //
+//    contact:    Olaf Conrad                            //
+//                Institute of Geography                 //
+//                University of Hamburg                  //
+//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#include "points_thinning.h"
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+CPoints_Thinning::CPoints_Thinning(void)
+{
+	CSG_Parameter	*pNode;
+
+	//-----------------------------------------------------
+	Set_Name		(_TL("Points Thinning"));
+
+	Set_Author		(SG_T("O.Conrad (c) 2011"));
+
+	Set_Description	(_TW(
+		""
+	));
+
+	//-----------------------------------------------------
+	pNode	= Parameters.Add_Shapes(
+		NULL	, "POINTS"		, _TL("Points"),
+		_TL(""),
+		PARAMETER_INPUT, SHAPE_TYPE_Point
+	);
+
+	Parameters.Add_Table_Field(
+		pNode	, "FIELD"		, _TL("Attribute"),
+		_TL("")
+	);
+
+	Parameters.Add_Shapes(
+		NULL	, "THINNED"		, _TL("Thinned Points"),
+		_TL(""),
+		PARAMETER_OUTPUT, SHAPE_TYPE_Point
+	);
+
+	Parameters.Add_Value(
+		NULL	, "RESOLUTION"	, _TL("Resolution"),
+		_TL(""),
+		PARAMETER_TYPE_Double, 1.0, 0.0, true
+	);
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CPoints_Thinning::On_Execute(void)
+{
+	int			Field;
+	CSG_Shapes	*pPoints;
+
+	//-----------------------------------------------------
+	pPoints			= Parameters("POINTS")		->asShapes();
+	Field			= Parameters("FIELD")		->asInt();
+	m_pPoints		= Parameters("THINNED")		->asShapes();
+	m_Resolution	= Parameters("RESOLUTION")	->asDouble();
+
+	//-----------------------------------------------------
+	if( m_Resolution <= 0.0 )
+	{
+		Error_Set(_TL("resolution has to be greater than zero"));
+
+		return( false );
+	}
+
+	if( !pPoints->is_Valid() )
+	{
+		Error_Set(_TL("invalid points layer"));
+
+		return( false );
+	}
+
+	if( pPoints->Get_Count() < 2 )
+	{
+		Error_Set(_TL("not more than one point in layer"));
+
+		return( false );
+	}
+
+	if( !Set_Search_Engine(pPoints, Field) )
+	{
+		Error_Set(_TL("failed to initialise search engine"));
+
+		return( false );
+	}
+
+	//-----------------------------------------------------
+	m_pPoints->Create(SHAPE_TYPE_Point, CSG_String::Format(SG_T("%s [%s]"), pPoints->Get_Name(), pPoints->Get_Field_Name(Field)));
+	m_pPoints->Add_Field(_TL("Count")	, SG_DATATYPE_Int);
+	m_pPoints->Add_Field(_TL("Mean")	, SG_DATATYPE_Double);
+	m_pPoints->Add_Field(_TL("Minimun")	, SG_DATATYPE_Double);
+	m_pPoints->Add_Field(_TL("Maximun")	, SG_DATATYPE_Double);
+	m_pPoints->Add_Field(_TL("StdDev")	, SG_DATATYPE_Double);
+
+	pPoints->Select();
+
+	//-----------------------------------------------------
+	Get_Points(m_Search.Get_Root_Pointer());
+
+	//-----------------------------------------------------
+	if( m_pPoints->Get_Count() == pPoints->Get_Count() )
+	{
+		Message_Add(_TL("no points removed"));
+	}
+	else
+	{
+		Message_Add(CSG_String::Format(SG_T("%d %s"), pPoints->Get_Count() - m_pPoints->Get_Count(), _TL("no points removed")));
+	}
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CPoints_Thinning::Set_Search_Engine(CSG_Shapes *pPoints, int Field)
+{
+	CSG_Rect	r(pPoints->Get_Extent());
+
+	r.Assign(
+		r.Get_XCenter() - 0.5 * m_Resolution,
+		r.Get_YCenter() - 0.5 * m_Resolution,
+		r.Get_XCenter() + 0.5 * m_Resolution,
+		r.Get_YCenter() + 0.5 * m_Resolution
+	);
+
+	while( r.Intersects(pPoints->Get_Extent()) != INTERSECTION_Contains )
+	{
+		r.Inflate(200.0);
+	}
+
+	if( !m_Search.Create(r, true) )
+	{
+		return( false );
+	}
+
+	for(int i=0; i<pPoints->Get_Count() && Set_Progress(i, pPoints->Get_Count()); i++)
+	{
+		CSG_Shape	*pPoint	= pPoints->Get_Shape(i);
+
+		m_Search.Add_Point(
+			pPoint->Get_Point(0).x,
+			pPoint->Get_Point(0).y,
+			pPoint->asDouble(Field)
+		);
+	}
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+void CPoints_Thinning::Get_Points(CSG_PRQuadTree_Item *pItem)
+{
+	if( pItem )
+	{
+		if( pItem->is_Leaf() )
+		{
+			Add_Point(pItem->asLeaf());
+		}
+		else if( pItem->Get_Size() <= m_Resolution )
+		{
+			Add_Point((CSG_PRQuadTree_Node_Statistics *)pItem);
+		}
+		else
+		{
+			for(int i=0; i<4; i++)
+			{
+				Get_Points(pItem->asNode()->Get_Child(i));
+			}
+		}
+	}
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+void CPoints_Thinning::Add_Point(CSG_PRQuadTree_Leaf *pLeaf)
+{
+	CSG_Shape	*pPoint	= m_pPoints->Add_Shape();
+
+	pPoint->Add_Point(pLeaf->Get_X(), pLeaf->Get_Y());
+
+	if( pLeaf->has_Statistics() )
+	{
+		CSG_PRQuadTree_Leaf_List	*pList	= (CSG_PRQuadTree_Leaf_List *)pLeaf;
+
+		pPoint->Set_Value(0, pList->Get_Count());	// Count
+		pPoint->Set_Value(1, pList->Get_Mean());	// Mean
+		pPoint->Set_Value(2, pList->Get_Minimum());	// Minimun
+		pPoint->Set_Value(3, pList->Get_Maximum());	// Maximun
+		pPoint->Set_Value(4, pList->Get_StdDev());	// StdDev
+	}
+	else
+	{
+		pPoint->Set_Value(0, 1);					// Count
+		pPoint->Set_Value(1, pLeaf->Get_Z());		// Mean
+		pPoint->Set_Value(2, pLeaf->Get_Z());		// Minimun
+		pPoint->Set_Value(3, pLeaf->Get_Z());		// Maximun
+		pPoint->Set_Value(4, 0.0);					// StdDev
+	}
+}
+
+//---------------------------------------------------------
+void CPoints_Thinning::Add_Point(CSG_PRQuadTree_Node_Statistics *pNode)
+{
+	CSG_Shape	*pPoint	= m_pPoints->Add_Shape();
+
+	pPoint->Add_Point(pNode->Get_X()->Get_Mean(), pNode->Get_Y()->Get_Mean());
+
+	pPoint->Set_Value(0, pNode->Get_Z()->Get_Count());		// Count
+	pPoint->Set_Value(1, pNode->Get_Z()->Get_Mean());		// Mean
+	pPoint->Set_Value(2, pNode->Get_Z()->Get_Minimum());	// Minimun
+	pPoint->Set_Value(3, pNode->Get_Z()->Get_Maximum());	// Maximun
+	pPoint->Set_Value(4, pNode->Get_Z()->Get_StdDev());		// StdDev
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
--- saga-gis/src/modules/shapes/shapes_points/points_thinning.h
+++ saga-gis/src/modules/shapes/shapes_points/points_thinning.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: points_thinning.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -9,13 +9,13 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                     shapes_points                     //
+//                     shapes_tools                      //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                   points_thinning.h                   //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2011 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -40,100 +40,63 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     volaya@ya.com                          //
+//    e-mail:     oconrad@saga-gis.org                   //
 //                                                       //
-//    contact:    Victor Olaya Ferrero                   //
-//                Madrid                                 //
-//                Spain                                  //
+//    contact:    Olaf Conrad                            //
+//                Institute of Geography                 //
+//                University of Hamburg                  //
+//                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
-
-//---------------------------------------------------------
-
 
 ///////////////////////////////////////////////////////////
 //														 //
-//			The Module Link Library Interface			 //
+//														 //
 //														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__points_thinning_H
+#define HEADER_INCLUDED__points_thinning_H
 
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
-const SG_Char * Get_Info(int i)
+//---------------------------------------------------------
+class CPoints_Thinning : public CSG_Module
 {
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Shapes - Points") );
-
-	case MLB_INFO_Author:
-		return( _TL("Various Authors") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for the manipulation of point vector data.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Shapes|Points") );
-	}
-}
+public:
+	CPoints_Thinning(void);
 
 
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
+protected:
 
-#include "Points_From_Table.h"
-#include "Points_From_Lines.h"
-#include "CountPoints.h"
-#include "CreatePointGrid.h"
-#include "DistanceMatrix.h"
-#include "FitNPointsToShape.h"
-#include "AddCoordinates.h"
-#include "remove_duplicates.h"
-#include "Clip_Points.h"
-#include "separate_by_direction.h"
-#include "add_polygon_attributes.h"
-#include "points_filter.h"
-#include "convex_hull.h"
-#include "select_points.h"
-#include "points_thinning.h"
+	virtual bool				On_Execute				(void);
 
 
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
+private:
 
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CPoints_From_Table );
-	case  1:	return( new CCountPoints );
-	case  2:	return( new CCreatePointGrid );
-	case  3:	return( new CDistanceMatrix );
-	case  4:	return( new CFitNPointsToShape );
-	case  5:	return( new CPoints_From_Lines );
-	case  6:	return( new CAddCoordinates );
-	case  7:	return( new CRemove_Duplicates );
-	case  8:	return( new CClip_Points );
-	case  9:	return( new CSeparate_by_Direction );
-	case 10:	return( new CAdd_Polygon_Attributes );
-	case 11:	return( new CPoints_Filter );
-	case 12:	return( new CConvex_Hull );
-	case 13:	return( new CSelect_Points );
-	case 14:	return( new CPoints_Thinning );
-	}
+	double						m_Resolution;
 
-	return( NULL );
-}
+	CSG_PRQuadTree				m_Search;
+
+	CSG_Shapes					*m_pPoints;
+
+
+	bool						Set_Search_Engine		(CSG_Shapes *pPoints, int Field);
+
+	void						Get_Points				(CSG_PRQuadTree_Item            *pItem);
+	void						Add_Point				(CSG_PRQuadTree_Leaf            *pLeaf);
+	void						Add_Point				(CSG_PRQuadTree_Node_Statistics *pNode);
+
+};
 
 
 ///////////////////////////////////////////////////////////
@@ -143,8 +106,4 @@ CSG_Module *		Create_Module(int i)
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
-
-	MLB_INTERFACE
-
-//}}AFX_SAGA
+#endif // #ifndef HEADER_INCLUDED__points_thinning_H
