a0407fa985c1d38389d881eac99ca78a84e81759
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2011-04-27 18:45:18 +0000

    module library name changed

--- saga-gis/src/modules/tin/tin_viewer/MLB_Interface.cpp
+++ saga-gis/src/modules/tin/tin_viewer/MLB_Interface.cpp
@@ -72,7 +72,7 @@ const SG_Char * Get_Info(int i)
 	switch( i )
 	{
 	case MLB_INFO_Name:	default:
-		return( _TL("Shapes - TIN Viewer") );
+		return( _TL("TIN - Visualisation") );
 
 	case MLB_INFO_Author:
 		return( SG_T("O.Conrad (c) 2011") );
