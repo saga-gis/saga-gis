76ccdf7303ea4ac5c4c1768c6e504829ade7ec6c
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2011-06-06 15:46:35 +0000

    saga_gui: table control: update object properties when table changed (add, delete, rename fields)

--- saga-gis/src/saga_core/saga_gui/view_table_control.cpp
+++ saga-gis/src/saga_core/saga_gui/view_table_control.cpp
@@ -607,6 +607,8 @@ void CVIEW_Table_Control::On_Field_Add(wxCommandEvent &event)
 		m_pTable->Add_Field(P("NAME")->asString(), Type, Position);
 
 		Update_Table();
+
+		g_pData->Update(m_pTable, NULL);
 	}
 }
 
@@ -645,6 +647,8 @@ void CVIEW_Table_Control::On_Field_Del(wxCommandEvent &event)
 		}
 
 		Update_Table();
+
+		g_pData->Update(m_pTable, NULL);
 	}
 }
 
@@ -729,6 +733,8 @@ void CVIEW_Table_Control::On_Field_Rename(wxCommandEvent &event)
 				SetColLabelValue(i, s.c_str());
 			}
 		}
+
+		g_pData->Update(m_pTable, NULL);
 	}
 }
 
