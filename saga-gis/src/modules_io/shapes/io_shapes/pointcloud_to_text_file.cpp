81db2dc7ae578ebfeae10a514be2f149edc5ece3
Volker Wichmann <wichmann@laserdata.at>
2011-07-21 13:18:03 +0000

    added new module 'export point cloud to text file'

---
+++ saga-gis/src/modules_io/shapes/io_shapes/pointcloud_to_text_file.cpp
@@ -0,0 +1,315 @@
+/**********************************************************
+ * Version $Id$
+ *********************************************************/
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                    Module Library:                    //
+//                       Shapes_IO                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//              pointcloud_to_text_file.cpp              //
+//                                                       //
+//                 Copyright (C) 2011 by                 //
+//                    Volker Wichmann                    //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation; version 2 of the License.   //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not,       //
+// write to the Free Software Foundation, Inc.,          //
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307,   //
+// USA.                                                  //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     wichmann@laserdata.at                  //
+//                                                       //
+//    contact:    LASERDATA GmbH                         //
+//                Management and Analysis of             //
+//                Laserscanning Data                     //
+//                Technikerstr. 21a                      //
+//                6020 Innsbruck                         //
+//                Austria                                //
+//                www.laserdata.at                       //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#include "pointcloud_to_text_file.h"
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+CPointcloud_To_Text_File::CPointcloud_To_Text_File(void)
+{
+	//-----------------------------------------------------
+	Set_Name		(_TL("Export Point Cloud to Text File"));
+
+	Set_Author		(SG_T("V. Wichmann, LASERDATA GmbH (c) 2011"));
+
+	Set_Description	(_TW(
+					"Exports a point cloud to a text file. Once the module is executed, "
+					"a pop-up dialog allows to specify the fields to be exported and their "
+					"decimal precision.\n\n"
+	));
+
+
+	//-----------------------------------------------------
+	Parameters.Add_PointCloud(
+		NULL	, "POINTS"		, _TL("Point Cloud"),
+		_TL("The point cloud to export."),
+		PARAMETER_INPUT
+	);
+
+	Parameters.Add_FilePath(
+		NULL	, "FILE"	, _TL("Text File"),
+		_TL("The file to write the point cloud to."),
+		CSG_String::Format(SG_T("%s|%s|%s|%s|%s|%s"),
+			_TL("Text Files (*.txt)")	, SG_T("*.txt"),
+			_TL("CSV Files (*.csv)")	, SG_T("*.csv"),
+			_TL("All Files")			, SG_T("*.*")
+		), NULL, true
+	);
+
+	Parameters.Add_Value(
+        NULL	, "WRITE_HEADER" , _TL("Write Header"),
+        _TL("Write column names."),
+        PARAMETER_TYPE_Bool, false
+    );
+
+    Parameters.Add_Choice(
+        NULL	, "FIELDSEP" , _TL("Field Separator"),
+        _TL("Field Separator"),
+        CSG_String::Format(SG_T("%s|%s|%s|"),
+            _TL("tabulator"),
+            _TL("space"),
+            _TL("comma")
+        ), 0
+    );
+}
+
+//---------------------------------------------------------
+CPointcloud_To_Text_File::~CPointcloud_To_Text_File(void)
+{}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CPointcloud_To_Text_File::On_Execute(void)
+{
+	CSG_PointCloud			*pPoints;
+	CSG_String				fileName;
+	CSG_File				*pTabStream = NULL;
+	bool					bWriteHeader;
+	CSG_String				fieldSep;
+
+	CSG_Parameters			P;
+	CSG_Parameter			*pNode;
+	CSG_String				s;
+	std::vector<int>		vCol, vPrecision;
+
+
+	//-----------------------------------------------------
+	pPoints			= Parameters("POINTS")		->asPointCloud();
+	fileName		= Parameters("FILE")		->asString();
+	bWriteHeader	= Parameters("WRITE_HEADER")->asBool();
+
+
+	switch (Parameters("FIELDSEP")->asInt())
+    {
+	default:
+    case 0:		fieldSep = "\t";	break;
+    case 1:		fieldSep = " ";		break;
+    case 2:		fieldSep = ",";		break;
+    }
+
+//	formatFloat.Printf(CSG_String::Format(SG_T("%%.%df"), iPrecision));
+
+
+	if( fileName.Length() == 0 )
+	{
+		SG_UI_Msg_Add_Error(_TL("Please provide an output file name!"));
+		return( false );
+	}
+
+
+	P.Set_Name(_TL("Check the fields to export"));
+
+	for(int iField=0; iField<pPoints->Get_Field_Count(); iField++)
+	{
+		s.Printf(SG_T("NODE_%03d") , iField + 1);
+		pNode	= P.Add_Node(NULL, s, CSG_String::Format(_TL("%d. %s"), iField + 1, _TL("Field")), _TL(""));
+
+		s.Printf(SG_T("FIELD_%03d"), iField);
+		P.Add_Value(pNode, s, CSG_String::Format(SG_T("%s"), pPoints->Get_Field_Name(iField)), _TL(""), PARAMETER_TYPE_Bool, false);
+
+		s.Printf(SG_T("PRECISION_%03d"), iField);
+		P.Add_Value(pNode, s, _TL("Decimal Precision"), _TL(""), PARAMETER_TYPE_Int, 2.0, 0.0, true);
+	}
+
+
+	//-----------------------------------------------------
+	if( Dlg_Parameters(&P, _TL("Field Properties")) )
+	{
+		vCol.clear();
+		vPrecision.clear();
+
+		for(int iField=0; iField<pPoints->Get_Field_Count(); iField++)
+		{
+			if( P(CSG_String::Format(SG_T("FIELD_%03d" ), iField).c_str())->asBool() )
+			{
+				vCol.push_back(iField);
+				vPrecision.push_back(P(CSG_String::Format(SG_T("PRECISION_%03d" ), iField).c_str())->asInt());
+			}
+		}
+	}
+	else
+		return( false );
+
+
+	if( vCol.size() == 0 )
+	{
+		SG_UI_Msg_Add_Error(_TL("Please provide at least one column to export!"));
+		return( false );
+	}
+
+
+	//-----------------------------------------------------
+	pTabStream = new CSG_File();
+
+	if( !pTabStream->Open(fileName, SG_FILE_W, false) )
+	{
+		SG_UI_Msg_Add_Error(CSG_String::Format(_TL("Unable to open output file %s!"), fileName.c_str()));
+		delete (pTabStream);
+		return (false);
+	}
+
+
+	if( bWriteHeader )
+	{
+		for(size_t i=0; i<vCol.size(); i++)
+		{
+			pTabStream->Printf(SG_T("%s"), pPoints->Get_Field_Name(vCol.at(i)));
+			pTabStream->Printf(i < (int)vCol.size() - 1 ? fieldSep.c_str() : SG_T("\n"));
+		}
+	}
+
+
+	for(int iPoint=0; iPoint<pPoints->Get_Count() && Set_Progress(iPoint, pPoints->Get_Count()); iPoint++)
+	{
+		for(size_t i=0; i<vCol.size(); i++)
+		{
+			switch (pPoints->Get_Field_Type(vCol.at(i)))
+			{
+			case SG_DATATYPE_Double:
+			case SG_DATATYPE_Float:
+				s = Double2String(pPoints->Get_Value(iPoint, vCol.at(i)), vPrecision.at(i));
+				pTabStream->Printf(s);
+				break;
+			default:
+				s = CSG_String::Format(SG_T("%d"), (int)pPoints->Get_Value(iPoint, vCol.at(i)));
+				pTabStream->Printf(s);
+				break;
+			}
+
+			pTabStream->Printf(i < (int)vCol.size() - 1 ? fieldSep.c_str() : SG_T("\n"));
+		}
+	}
+
+
+	pTabStream->Close();
+	delete (pTabStream);
+
+	return( true );
+}
+
+
+//---------------------------------------------------------
+CSG_String CPointcloud_To_Text_File::Double2String(double value, int precision)
+{
+	/* Originally published in pdfutility.cpp of wxPdfDocument by Ulrich Telle
+	 * modified */
+
+	CSG_String number;
+	if (precision < 0)
+		precision = 0;
+	else if (precision > 16)
+		precision = 16;
+
+	// Use absolute value locally
+	double localValue = fabs(value);
+	double localFraction = (localValue - floor(localValue)) +(5. * pow(10.0, -precision-1));
+	if (localFraction >= 1)
+	{
+		localValue += 1.0;
+		localFraction -= 1.0;
+	}
+	localFraction *= pow(10.0, precision);
+
+	if (value < 0)
+		number += CSG_String(SG_T("-"));
+
+
+	number += CSG_String::Format(SG_T("%.0f"), floor(localValue));
+
+	// generate fraction, padding with zero if necessary.
+	if (precision > 0)
+	{
+		number += CSG_String(SG_T("."));
+		CSG_String fraction = CSG_String::Format(SG_T("%.0f"), floor(localFraction));
+		if (fraction.Length() < ((size_t) precision))
+		{
+			for(int i=0; i<precision-fraction.Length(); i++)
+				number += CSG_String(SG_T('0'));//, precision-fraction.Length());
+		}
+		number += fraction;
+	}
+
+	return number;
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
--- saga-gis/src/modules_io/shapes/io_shapes/pointcloud_to_text_file.h
+++ saga-gis/src/modules_io/shapes/io_shapes/pointcloud_to_text_file.h
@@ -9,14 +9,14 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                       io_shapes                       //
+//                       Shapes_IO                       //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//               pointcloud_to_text_file.h               //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
-//                      Olaf Conrad                      //
+//                 Copyright (C) 2011 by                 //
+//                    Volker Wichmann                    //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
@@ -40,14 +40,15 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     oconrad@saga-gis.org                   //
+//    e-mail:     wichmann@laserdata.at                  //
 //                                                       //
-//    contact:    Olaf Conrad                            //
-//                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
-//                Germany                                //
+//    contact:    LASERDATA GmbH                         //
+//                Management and Analysis of             //
+//                Laserscanning Data                     //
+//                Technikerstr. 21a                      //
+//                6020 Innsbruck                         //
+//                Austria                                //
+//                www.laserdata.at                       //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
@@ -56,99 +57,55 @@
 
 ///////////////////////////////////////////////////////////
 //														 //
-//			The Module Link Library Interface			 //
+//                                                       //
 //														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__Pointcloud_To_Text_File_H
+#define HEADER_INCLUDED__Pointcloud_To_Text_File_H
 
+
+///////////////////////////////////////////////////////////
+//														 //
+//                                                       //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
-
-
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-const SG_Char * Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Import/Export - Shapes") );
-
-	case MLB_INFO_Author:
-		return( SG_T("SAGA User Group Associaton (c) 2002-10") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for the import and export of vector data.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("File|Shapes") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "gstat.h"
-#include "xyz.h"
-#include "generate.h"
-#include "surfer_bln.h"
-#include "atlas_bna.h"
-#include "wasp_map.h"
-#include "stl.h"
-#include "gpx.h"
-#include "pointcloud_from_file.h"
-#include "pointcloud_from_text_file.h"
-#include "svg.h"
-#include "pointcloud_to_text_file.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CGStat_Export );
-	case  1:	return( new CGStat_Import );
-	case  2:	return( new CXYZ_Export );
-	case  3:	return( new CXYZ_Import );
-	case  4:	return( new CGenerate_Export );
-	case  5:	return( new CSurfer_BLN_Export );
-	case  6:	return( new CSurfer_BLN_Import );
-	case  7:	return( new CAtlas_BNA_Export );
-	case  8:	return( new CAtlas_BNA_Import );
-	case  9:	return( new CWASP_MAP_Export );
-	case 10:	return( new CWASP_MAP_Import );
-	case 11:	return( new CSTL_Import );
-	case 12:	return( new CSTL_Export );
-	case 13:	return( new CGPX_Import );
-	case 14:	return( new CGPX_Export );
-	case 15:	return( new CPointCloud_From_File );
-	case 16:	return( new CPointCloud_From_Text_File );
-	case 17:	return( new CSVG_Export );
-	case 18:	return( new CPointcloud_To_Text_File );
-	}
-
-	return( NULL );
-}
+#include <vector>
 
 
 ///////////////////////////////////////////////////////////
 //														 //
-//														 //
+//                                                       //
 //														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CPointcloud_To_Text_File : public CSG_Module
+{
+public:
+	CPointcloud_To_Text_File(void);
+	virtual ~CPointcloud_To_Text_File(void);
 
-	MLB_INTERFACE
+	virtual const SG_Char *	Get_MenuPath		(void)	{	return( _TL("R:Export") );	}
 
-//}}AFX_SAGA
+
+protected:
+
+	virtual bool			On_Execute	(void);
+
+	CSG_String				Double2String(double value, int precision);
+};
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//                                                       //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__Pointcloud_To_Text_File_H
