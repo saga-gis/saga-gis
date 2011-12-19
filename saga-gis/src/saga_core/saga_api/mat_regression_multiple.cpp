cf97063afc05fca760b80ca179f4e78fb8350125
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2011-12-19 15:43:27 +0000

    saga_api, multiple regression analysis: bug fix (backward/stepwise)

--- saga-gis/src/saga_core/saga_api/mat_regression_multiple.cpp
+++ saga-gis/src/saga_core/saga_api/mat_regression_multiple.cpp
@@ -438,8 +438,8 @@ bool CSG_Regression_Multiple::_Set_Step_Info(const CSG_Matrix &X, double R2_prev
 	pRecord->Set_Value(MLR_STEP_DF		, X.Get_NRows() - m_nPredictors - 1);
 	pRecord->Set_Value(MLR_STEP_F		, R.m_pModel->Get_Record(MLR_MODEL_F  )->asDouble(1));
 	pRecord->Set_Value(MLR_STEP_SIG		, R.m_pModel->Get_Record(MLR_MODEL_SIG)->asDouble(1));
-	pRecord->Set_Value(MLR_STEP_VAR_F	, _Get_F(1, X.Get_NRows() - (m_nPredictors - 1), R.Get_R2(), R2_prev));
-	pRecord->Set_Value(MLR_STEP_VAR_SIG	, _Get_P(1, X.Get_NRows() - (m_nPredictors - 1), R.Get_R2(), R2_prev));
+	pRecord->Set_Value(MLR_STEP_VAR_F	, _Get_F(1, X.Get_NRows() - (m_nPredictors - 1), bIn ? R.Get_R2() : R2_prev, bIn ? R2_prev : R.Get_R2()));
+	pRecord->Set_Value(MLR_STEP_VAR_SIG	, _Get_P(1, X.Get_NRows() - (m_nPredictors - 1), bIn ? R.Get_R2() : R2_prev, bIn ? R2_prev : R.Get_R2()));
 	pRecord->Set_Value(MLR_STEP_DIR		, bIn ? SG_T(">>") : SG_T("<<"));
 	pRecord->Set_Value(MLR_STEP_VAR		, m_Names[1 + iVariable]);
 
@@ -540,7 +540,7 @@ int CSG_Regression_Multiple::_Get_Step_Out(CSG_Matrix &X, double P_out, double &
 
 		_Set_Step_Info(X, R2, m_Predictor[iNext], false);
 
-		R2	= R.Get_R2();
+		R2	= rNext;
 
 		m_nPredictors--;
 
@@ -584,8 +584,6 @@ bool CSG_Regression_Multiple::Calculate_Forward(const CSG_Matrix &Samples, doubl
 	{
 		return( false );
 	}
-
-//	return( _Get_Forward(Samples, P_in );
 
 	//-----------------------------------------------------
 	CSG_Matrix		X(1, Samples.Get_NY(), Samples.Get_Col(0).Get_Data());
