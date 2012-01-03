a1197cd9802a0e5a78a34e2cf7cec61ee8051f53
Manfred Egger <egger@standortkonzepte.at>
2012-01-03 10:49:17 +0000

    saga_api: gcc warning int-to-pointer-cast

--- saga-gis/src/saga_core/saga_api/parameters.h
+++ saga-gis/src/saga_core/saga_api/parameters.h
@@ -79,7 +79,7 @@
 #include "shapes.h"
 #include "tin.h"
 #include "pointcloud.h"
-
+#include "stdint.h"
 
 ///////////////////////////////////////////////////////////
 //														 //
@@ -789,7 +789,7 @@ public:
 
 	virtual bool				is_Valid				(void);
 
-	virtual bool				Set_Value				(int   Value)	{	return( Set_Value((void *)Value) );	}
+	virtual bool				Set_Value				(uintptr_t Value)	{	return( Set_Value((void *)Value) );	}
 	virtual bool				Set_Value				(void *Value);
 
 	virtual void *				asPointer				(void)			{	return( m_pDataObject );	}
