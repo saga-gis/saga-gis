b61da40471982660835756562a9075377729d20c
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2012-01-26 16:28:14 +0000

    lines to polygon conversion: optionally as one single multipolygon object

--- saga-gis/src/modules/shapes/shapes_polygons/Polygons_From_Lines.cpp
+++ saga-gis/src/modules/shapes/shapes_polygons/Polygons_From_Lines.cpp
@@ -96,6 +96,12 @@ CPolygons_From_Lines::CPolygons_From_Lines(void)
 		_TL(""),
 		PARAMETER_INPUT, SHAPE_TYPE_Line
 	);
+
+	pNode	= Parameters.Add_Value(
+		NULL	, "SINGLE"		, _TL("Create Single Multipart Polygon"),
+		_TL(""),
+		PARAMETER_TYPE_Bool, false
+	);
 }
 
 
@@ -108,29 +114,46 @@ CPolygons_From_Lines::CPolygons_From_Lines(void)
 //---------------------------------------------------------
 bool CPolygons_From_Lines::On_Execute(void)
 {
+	bool		bSingle;
+	CSG_Shape	*pLine , *pPolygon;
 	CSG_Shapes	*pLines, *pPolygons;
 
-	pPolygons	= Parameters("POLYGONS")	->asShapes();
-	pLines		= Parameters("LINES")		->asShapes();
+	pPolygons	= Parameters("POLYGONS")->asShapes();
+	pLines		= Parameters("LINES"   )->asShapes();
+	bSingle		= Parameters("SINGLE"  )->asBool  ();
 
 	if(	pLines->Get_Count() <= 0 )
 	{
 		return( false );
 	}
 
-	pPolygons->Create(SHAPE_TYPE_Polygon, pLines->Get_Name(), pLines);
+	if( !bSingle )
+	{
+		pPolygons	->Create(SHAPE_TYPE_Polygon, pLines->Get_Name(), pLines);
+	}
+	else
+	{
+		pPolygons	->Create(SHAPE_TYPE_Polygon, pLines->Get_Name());
+		pPolygons	->Add_Field(SG_T("ID"), SG_DATATYPE_Int);
+		pPolygon	= pPolygons	->Add_Shape();
+		pPolygon	->Set_Value(0, 1);
+	}
 
 	//-----------------------------------------------------
 	for(int iLine=0; iLine<pLines->Get_Count() && Set_Progress(iLine, pLines->Get_Count()); iLine++)
 	{
-		CSG_Shape	*pLine		= pLines	->Get_Shape(iLine);
-		CSG_Shape	*pPolygon	= pPolygons	->Add_Shape(pLine, SHAPE_COPY_ATTR);
+		pLine	= pLines->Get_Shape(iLine);
 
-		for(int iPart=0; iPart<pLine->Get_Part_Count(); iPart++)
+		if( !bSingle )
+		{
+			pPolygon	= pPolygons->Add_Shape(pLine, SHAPE_COPY_ATTR);
+		}
+
+		for(int iPart=0, jPart=pPolygon->Get_Part_Count(); iPart<pLine->Get_Part_Count(); iPart++, jPart++)
 		{
 			for(int iPoint=0; iPoint<pLine->Get_Point_Count(iPart); iPoint++)
 			{
-				pPolygon->Add_Point(pLine->Get_Point(iPoint, iPart), iPart);
+				pPolygon->Add_Point(pLine->Get_Point(iPoint, iPart), jPart);
 			}
 		}
 	}
