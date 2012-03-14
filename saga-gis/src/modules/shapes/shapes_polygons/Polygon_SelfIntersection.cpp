9592cc7cd784861e8b0e5d5e0d9e5c12ca591edc
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2012-03-14 10:03:01 +0000

    module added: 'self-intersection' for polygons of one layer

---
+++ saga-gis/src/modules/shapes/shapes_polygons/Polygon_SelfIntersection.cpp
@@ -0,0 +1,202 @@
+/**********************************************************
+ * Version $Id: Polygon_SelfIntersection.cpp 1230 2011-11-22 11:12:10Z oconrad $
+ *********************************************************/
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                    Module Library:                    //
+//                    shapes_polygons                    //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//              Polygon_SelfIntersection.cpp             //
+//                                                       //
+//                 Copyright (C) 2012 by                 //
+//                      Olaf Conrad                      //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation; version 2 of the License.   //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not,       //
+// write to the Free Software Foundation, Inc.,          //
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307,   //
+// USA.                                                  //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     oconrad@saga-gis.org                   //
+//                                                       //
+//    contact:    Olaf Conrad                            //
+//                Institute of Geography                 //
+//                University of Hamburg                  //
+//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#include "Polygon_SelfIntersection.h"
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+CPolygon_SelfIntersection::CPolygon_SelfIntersection(void)
+{
+	CSG_Parameter	*pNode;
+
+	//-----------------------------------------------------
+	Set_Name		(_TL("Polygon Self-Intersection"));
+
+	Set_Author		(SG_T("O.Conrad (c) 2012"));
+
+	Set_Description	(_TW(
+		"Self-intersection of one layer's polygons.\n"
+		"Uses the free and open source software library <b>Clipper</b> created by Angus Johnson.\n"
+		"<a target=\"_blank\" href=\"http://www.angusj.com/delphi/clipper.php\">Clipper Homepage</a>\n"
+		"<a target=\"_blank\" href=\"http://sourceforge.net/projects/polyclipping/\">Clipper at SourceForge</a>\n"
+	));
+
+	//-----------------------------------------------------
+	pNode	= Parameters.Add_Shapes(
+		NULL	, "POLYGONS"	, _TL("Polygons"),
+		_TL(""),
+		PARAMETER_INPUT, SHAPE_TYPE_Polygon
+	);
+
+	Parameters.Add_Table_Field(
+		pNode	, "ID"			, _TL("Identifier"),
+		_TL(""),
+		true
+	);
+
+	Parameters.Add_Shapes(
+		NULL	, "INTERSECT"	, _TL("Intersection"),
+		_TL(""),
+		PARAMETER_OUTPUT, SHAPE_TYPE_Polygon
+	);
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CPolygon_SelfIntersection::On_Execute(void)
+{
+	int			ID;
+	CSG_Shapes	*pPolygons;
+
+	pPolygons		= Parameters("POLYGONS" )->asShapes();
+	m_pIntersect	= Parameters("INTERSECT")->asShapes();
+	ID				= Parameters("ID")->asInt();	if( ID >= pPolygons->Get_Field_Count() )	{	ID	= -1;	}
+
+	m_pIntersect->Create(SHAPE_TYPE_Polygon, CSG_String::Format(SG_T("%s [%s]"), pPolygons->Get_Name(), _TL("self-intersection")), pPolygons);
+	m_pIntersect->Add_Field("ID", SG_DATATYPE_String);
+
+	for(int i=0; i<pPolygons->Get_Count() && Set_Progress(i, pPolygons->Get_Count()); i++)
+	{
+		Add_Polygon(pPolygons->Get_Shape(i), ID);
+	}
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+void CPolygon_SelfIntersection::Add_Polygon(CSG_Shape *pPolygon, int ID)
+{
+	CSG_String	sID;
+
+	if( ID >= 0 )
+	{	sID	= pPolygon->asString(ID);	}
+	else
+	{	sID	= SG_Get_String(pPolygon->Get_Index() + 1, 0);	}
+
+	ID	= m_pIntersect->Get_Field_Count() - 1;
+
+	//-----------------------------------------------------
+	if( !m_pIntersect->Select(pPolygon->Get_Extent()) )
+	{
+		pPolygon	= m_pIntersect->Add_Shape(pPolygon);
+		pPolygon	->Set_Value(ID, sID);
+
+		return;
+	}
+
+	//-----------------------------------------------------
+	CSG_Shapes	Intersect(m_pIntersect->Get_Type(), SG_T(""), m_pIntersect);
+
+	int	nIntersects	= m_pIntersect->Get_Count();
+
+	pPolygon	= m_pIntersect->Add_Shape(pPolygon);
+	pPolygon	->Set_Value(ID, sID);
+
+	for(int i=0; i<nIntersects && pPolygon->is_Valid(); i++)
+	{
+		if( pPolygon != m_pIntersect->Get_Shape(i) && pPolygon->Intersects(m_pIntersect->Get_Shape(i)) )
+		{
+			CSG_Shape	*pOriginal	= Intersect.Add_Shape(m_pIntersect->Get_Shape(i));
+			CSG_Shape	*pIntersect	= Intersect.Add_Shape();
+
+			if( SG_Polygon_Intersection(pOriginal, pPolygon, pIntersect) )
+			{
+				pIntersect	= m_pIntersect->Add_Shape(pIntersect);
+				pIntersect	->Set_Value(ID, CSG_String::Format(SG_T("%s|%s"), pPolygon->asString(ID), pOriginal->asString(ID)));
+
+				SG_Polygon_Difference(m_pIntersect->Get_Shape(i), pPolygon);
+				SG_Polygon_Difference(pPolygon, pOriginal);
+			}
+
+			Intersect.Del_Shapes();
+		}
+	}
+
+	m_pIntersect->Select();
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
--- saga-gis/src/modules/shapes/shapes_polygons/Polygon_SelfIntersection.h
+++ saga-gis/src/modules/shapes/shapes_polygons/Polygon_SelfIntersection.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: Polygon_SelfIntersection.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -13,9 +13,9 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//               Polygon_SelfIntersection.h              //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2012 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -44,9 +44,7 @@
 //                                                       //
 //    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
@@ -56,83 +54,51 @@
 
 ///////////////////////////////////////////////////////////
 //														 //
-//			The Module Link Library Interface			 //
+//                                                       //
 //														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__Polygon_SelfIntersection_H
+#define HEADER_INCLUDED__Polygon_SelfIntersection_H
 
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
-CSG_String Get_Info(int i)
+//---------------------------------------------------------
+class CPolygon_SelfIntersection : public CSG_Module  
 {
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Shapes - Polygons") );
-
-	case MLB_INFO_Author:
-		return( SG_T("O. Conrad, V. Olaya (c) 2002-5") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for polygons.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Shapes|Polygons") );
-	}
-}
+public:
+	CPolygon_SelfIntersection(void);
 
 
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
+protected:
 
-#include "Polygon_Intersection.h"
-#include "Polygon_Centroids.h"
-#include "Polygon_Geometrics.h"
-#include "Polygons_From_Lines.h"
-#include "Polygon_StatisticsFromPoints.h"
-#include "Polygon_Union.h"
-#include "polygon_to_points.h"
-#include "shape_index.h"
-#include "polygon_line_intersection.h"
-#include "polygon_to_edges_nodes.h"
-#include "polygon_split_parts.h"
-#include "Polygon_Clip.h"
-#include "Polygon_SelfIntersection.h"
+	virtual bool			On_Execute			(void);
 
 
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
+private:
 
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CPolygon_Intersection );
-	case  1:	return( new CPolygon_Centroids );
-	case  2:	return( new CPolygon_Geometrics );
-	case  3:	return( new CPolygons_From_Lines );
-	case  4:	return( new CPolygonStatisticsFromPoints );
-	case  5:	return( new CPolygon_Dissolve );
-	case  6:	return( new CPolygon_To_Points );
-	case  7:	return( new CShape_Index );
-	case  8:	return( new CPolygon_Line_Intersection );
-	case  9:	return( new CPolygon_to_Edges_Nodes );
-	case 10:	return( new CPolygon_Split_Parts );
-	case 11:	return( new CPolygon_Clip );
-	case 12:	return( new CPolygon_SelfIntersection );
-	}
+	CSG_Shapes				*m_pIntersect;
 
-	return( NULL );
-}
+
+	void					Add_Polygon			(CSG_Shape *pPolygon, int ID);
+
+};
 
 
 ///////////////////////////////////////////////////////////
@@ -142,8 +108,4 @@ CSG_Module *		Create_Module(int i)
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
-
-	MLB_INTERFACE
-
-//}}AFX_SAGA
+#endif // #ifndef HEADER_INCLUDED__Polygon_SelfIntersection_H
