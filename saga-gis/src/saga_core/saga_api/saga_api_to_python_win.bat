45ba41415a7f152067172772e80dbd53898d3b35
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2012-11-07 12:35:07 +0000

    saga_api, msw: python compilation updated

--- saga-gis/src/saga_core/saga_api/saga_api_to_python_win.bat
+++ saga-gis/src/saga_core/saga_api/saga_api_to_python_win.bat
@@ -1,24 +1,26 @@
 @echo off
 
 rem #########################################
-set WXWIN=H:\libs\libs\wxMSW-2.8.11
-set SAGA=H:\saga\saga_svn_wx28\saga-gis
+set WXWIN=H:\libs\libs\wxWidgets-2.9.4
+set SAGA=H:\saga\saga_2\trunk\saga-gis
 rem set SWIG=C:\Program Files\swigwin-1.3.25
-set PYTHONPATH=C:\Programme (x86)\PYTHON26
+set PYTHONPATH=H:\libs\_libs\Python\Python-2.6
 rem #########################################
 
 rem #########################################
+echo ________________________________________
 echo SWIG compilation...
 
-"%SWIG%\swig" -c++ -python -includeall -D_SAGA_PYTHON saga_api.h
+"%SWIG%\swig" -c++ -python -includeall -I%SAGA%/src/saga_core/saga_api -D_SAGA_PYTHON -D_SAGA_UNICODE saga_api.h
 
 echo SWIG compilation finished.
 rem #########################################
 
 rem #########################################
+echo ________________________________________
 echo Python compilation...
 
-saga_api_to_python_win.py install
+rem saga_api_to_python_win.py install
 "%PYTHONPATH%\python.exe" saga_api_to_python_win.py install
 
 copy saga_api.py "%PYTHONPATH%\Lib\site-packages\saga_api.py"
