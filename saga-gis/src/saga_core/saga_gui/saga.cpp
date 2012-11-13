5b78bec4471715f75fa5cd6ec3cce7713ec20e0b
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2012-11-13 08:51:00 +0000

    saga_gui, bug fix: drag'n'drop files to icon leads to crash

--- saga-gis/src/saga_core/saga_gui/saga.cpp
+++ saga-gis/src/saga_core/saga_gui/saga.cpp
@@ -238,20 +238,20 @@ bool CSAGA::OnInit(void)
 	SetTopWindow(new CSAGA_Frame());
 
 	//-----------------------------------------------------
+	if( pLogo && (iLogo == 1 || argc > 1) )
+	{
+		pLogo->Destroy();
+
+		wxYield();
+	}
+
+	//-----------------------------------------------------
 	if( argc > 1 )
 	{
 		for(int i=1; i<argc; i++)
 		{
 			g_pWKSP->Open(argv[i]);
 		}
-	}
-
-	//-----------------------------------------------------
-	if( pLogo && iLogo == 1 )
-	{
-		pLogo->Destroy();
-
-		wxYield();
 	}
 
 	//-----------------------------------------------------
