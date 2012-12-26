150a2ffa2748684a917e00eca2a4b1c126d58f4d
Volker Wichmann <wichmann@laserdata.at>
2012-12-26 15:27:55 +0000

    added print_version.py in ../srcipting/python/helpers

---
+++ saga-gis/src/scripting/python/helpers/print_version.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import saga_api, sys, os
+
+##########################################
+
+if __name__ == '__main__':
+    print
+    print '--------------------------------------------------------------------------------'
+    print '# ' + saga_api.SAGA_API_Get_Version()
+    print '# Python - Version: ' + sys.version
+    print '--------------------------------------------------------------------------------'
+    print
