9fe7d23c4ffe5fb4da26378c0455af839472f7a5
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2013-03-05 16:07:00 +0000

    ta_morphometry, terrain classification added: top hat based ridge/valley detection, fuzzy landform classification. refers to http://sourceforge.net/p/saga-gis/feature-requests/38/

--- saga-gis/src/modules_terrain_analysis/terrain_analysis/ta_morphometry/fuzzy_landform_elements.h
+++ saga-gis/src/modules_terrain_analysis/terrain_analysis/ta_morphometry/fuzzy_landform_elements.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: fuzzy_landform_elements.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -13,9 +13,9 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                fuzzy_landform_elements.h              //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2013 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -44,119 +44,71 @@
 //                                                       //
 //    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#ifndef HEADER_INCLUDED__fuzzy_landform_elements_H
+#define HEADER_INCLUDED__fuzzy_landform_elements_H
+
 
 ///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Terrain Analysis - Morphometry") );
-
-	case MLB_INFO_Author:
-		return( SG_T("Various Authors") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for (grid based) digital terrain analysis.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Terrain Analysis|Morphometry") );
-	}
-}
-
-
-//---------------------------------------------------------
-#include "Morphometry.h"
-#include "Convergence.h"
-#include "Convergence_Radius.h"
-#include "SurfaceSpecificPoints.h"
-#include "Curvature_Classification.h"
-#include "Hypsometry.h"
-#include "RealArea.h"
-#include "ProtectionIndex.h"
-#include "mrvbf.h"
-#include "distance_gradient.h"
-#include "mass_balance_index.h"
-#include "air_flow_height.h"
-#include "anisotropic_heating.h"
-#include "land_surface_temperature.h"
-#include "relative_heights.h"
-#include "wind_effect.h"
-#include "ruggedness.h"
-#include "tpi.h"
-#include "tc_iwahashi_pike.h"
-#include "param_scale.h"
-#include "top_hat.h"
-#include "fuzzy_landform_elements.h"
-
-//---------------------------------------------------------
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case 0:		return( new CMorphometry );
-	case 1:		return( new CConvergence );
-	case 2:		return( new CConvergence_Radius );
-	case 3:		return( new CSurfaceSpecificPoints );
-	case 4:		return( new CCurvature_Classification );
-	case 5:		return( new CHypsometry );
-	case 6:		return( new CRealArea );
-	case 7:		return( new CProtectionIndex );
-	case 8:		return( new CMRVBF );
-	case 9:		return( new CDistance_Gradient );
-	case 10:	return( new CMass_Balance_Index );
-	case 11:	return( new CAir_Flow_Height );
-	case 12:	return( new CAnisotropic_Heating );
-	case 13:	return( new CLand_Surface_Temperature );
-	case 14:	return( new CRelative_Heights );
-	case 15:	return( new CWind_Effect );
-	case 16:	return( new CRuggedness_TRI );
-	case 17:	return( new CRuggedness_VRM );
-	case 18:	return( new CTPI );
-	case 19:	return( new CTPI_Classification );
-	case 20:	return( new CTC_Texture );
-	case 21:	return( new CTC_Convexity );
-	case 22:	return( new CTC_Classification );
-	case 23:	return( new CParam_Scale );
-	case 24:	return( new CTop_Hat );
-	case 25:	return( new CFuzzy_Landform_Elements );
-	}
-
-	return( NULL );
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CFuzzy_Landform_Elements : public CSG_Module_Grid
+{
+public:
+	CFuzzy_Landform_Elements(void);
 
-	MLB_INTERFACE
+	virtual CSG_String	Get_MenuPath			(void)	{	return( SG_T("Classification") );	}
 
-//}}AFX_SAGA
+
+protected:
+
+	virtual bool		On_Execute				(void);
+
+
+private:
+
+	bool				m_bSlopeToDeg;
+
+	double				m_loSlope, m_hiSlope, m_loCurve, m_hiCurve;
+
+
+	bool				Get_Memberships			(double Input[], double Membership[], int &Element, double &MaxMem, double &Entropy, double &CI);
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__fuzzy_landform_elements_H
--- saga-gis/src/modules_terrain_analysis/terrain_analysis/ta_morphometry/top_hat.h
+++ saga-gis/src/modules_terrain_analysis/terrain_analysis/ta_morphometry/top_hat.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: top_hat.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -13,9 +13,9 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                      top_hat.h                        //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2013 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -44,119 +44,70 @@
 //                                                       //
 //    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#ifndef HEADER_INCLUDED__top_hat_H
+#define HEADER_INCLUDED__top_hat_H
+
 
 ///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Terrain Analysis - Morphometry") );
-
-	case MLB_INFO_Author:
-		return( SG_T("Various Authors") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for (grid based) digital terrain analysis.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Terrain Analysis|Morphometry") );
-	}
-}
-
-
-//---------------------------------------------------------
-#include "Morphometry.h"
-#include "Convergence.h"
-#include "Convergence_Radius.h"
-#include "SurfaceSpecificPoints.h"
-#include "Curvature_Classification.h"
-#include "Hypsometry.h"
-#include "RealArea.h"
-#include "ProtectionIndex.h"
-#include "mrvbf.h"
-#include "distance_gradient.h"
-#include "mass_balance_index.h"
-#include "air_flow_height.h"
-#include "anisotropic_heating.h"
-#include "land_surface_temperature.h"
-#include "relative_heights.h"
-#include "wind_effect.h"
-#include "ruggedness.h"
-#include "tpi.h"
-#include "tc_iwahashi_pike.h"
-#include "param_scale.h"
-#include "top_hat.h"
-#include "fuzzy_landform_elements.h"
-
-//---------------------------------------------------------
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case 0:		return( new CMorphometry );
-	case 1:		return( new CConvergence );
-	case 2:		return( new CConvergence_Radius );
-	case 3:		return( new CSurfaceSpecificPoints );
-	case 4:		return( new CCurvature_Classification );
-	case 5:		return( new CHypsometry );
-	case 6:		return( new CRealArea );
-	case 7:		return( new CProtectionIndex );
-	case 8:		return( new CMRVBF );
-	case 9:		return( new CDistance_Gradient );
-	case 10:	return( new CMass_Balance_Index );
-	case 11:	return( new CAir_Flow_Height );
-	case 12:	return( new CAnisotropic_Heating );
-	case 13:	return( new CLand_Surface_Temperature );
-	case 14:	return( new CRelative_Heights );
-	case 15:	return( new CWind_Effect );
-	case 16:	return( new CRuggedness_TRI );
-	case 17:	return( new CRuggedness_VRM );
-	case 18:	return( new CTPI );
-	case 19:	return( new CTPI_Classification );
-	case 20:	return( new CTC_Texture );
-	case 21:	return( new CTC_Convexity );
-	case 22:	return( new CTC_Classification );
-	case 23:	return( new CParam_Scale );
-	case 24:	return( new CTop_Hat );
-	case 25:	return( new CFuzzy_Landform_Elements );
-	}
-
-	return( NULL );
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CTop_Hat : public CSG_Module_Grid
+{
+public:
+	CTop_Hat(void);
 
-	MLB_INTERFACE
+	virtual CSG_String	Get_MenuPath			(void)	{	return( SG_T("Classification") );	}
 
-//}}AFX_SAGA
+
+protected:
+
+	virtual bool		On_Execute				(void);
+
+
+private:
+
+	bool				Get_Focal_Statistics	(int x, int y, const CSG_Grid &g, const CSG_Grid_Cell_Addressor &r, CSG_Simple_Statistics &s);
+
+	bool				Get_Focal_Minimum		(int x, int y, const CSG_Grid &g, const CSG_Grid_Cell_Addressor &r, double &Minimum);
+	bool				Get_Focal_Maximum		(int x, int y, const CSG_Grid &g, const CSG_Grid_Cell_Addressor &r, double &Maximum);
+	bool				Get_Focal_Extremes		(int x, int y, const CSG_Grid &g, const CSG_Grid_Cell_Addressor &r, double &Minimum, double &Maximum);
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__top_hat_H
