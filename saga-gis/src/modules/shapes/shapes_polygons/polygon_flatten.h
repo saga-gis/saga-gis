e065330405b8e7064a5f38dd6717f2e94962a1ce
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2014-03-31 10:35:00 +0000

    shapes_polygons: flatten polygon layer tool added

--- saga-gis/src/modules/shapes/shapes_polygons/polygon_flatten.h
+++ saga-gis/src/modules/shapes/shapes_polygons/polygon_flatten.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: polygon_flatten.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -9,13 +9,13 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                    shapes_polygons                    //
+//                    shapes_polygon                     //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                   polygon_flatten.h                   //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2014 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -44,9 +44,7 @@
 //                                                       //
 //    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
@@ -55,110 +53,56 @@
 
 
 ///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__polygon_flatten_H
+#define HEADER_INCLUDED__polygon_flatten_H
 
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Shapes - Polygons") );
-
-	case MLB_INFO_Author:
-		return( SG_T("O. Conrad, V. Olaya, V. Wichmann (c) 2002-14") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for polygons.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Shapes|Polygons") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "Polygon_Intersection.h"
-#include "Polygon_Centroids.h"
-#include "Polygon_Geometrics.h"
-#include "Polygons_From_Lines.h"
-#include "Polygon_StatisticsFromPoints.h"
-#include "Polygon_Union.h"
-#include "polygon_to_points.h"
-#include "shape_index.h"
-#include "polygon_line_intersection.h"
-#include "polygon_to_edges_nodes.h"
-#include "polygon_split_parts.h"
-#include "Polygon_Clip.h"
-#include "Polygon_SelfIntersection.h"
-#include "add_point_attributes.h"
-#include "polygon_flatten.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  1:	return( new CPolygon_Centroids );
-	case  2:	return( new CPolygon_Geometrics );
-	case  3:	return( new CPolygons_From_Lines );
-	case  4:	return( new CPolygonStatisticsFromPoints );
-	case  5:	return( new CPolygon_Dissolve );
-	case  6:	return( new CPolygon_To_Points );
-	case  7:	return( new CShape_Index );
-	case  8:	return( new CPolygon_Line_Intersection );
-	case  9:	return( new CPolygon_to_Edges_Nodes );
-	case 10:	return( new CPolygon_Split_Parts );
-	case 11:	return( new CPolygon_Clip );
-	case 12:	return( new CPolygon_SelfIntersection );
-
-	case 14:	return( new CPolygon_Intersection );
-	case 15:	return( new CPolygon_Difference );
-	case 16:	return( new CPolygon_SymDifference );
-	case 17:	return( new CPolygon_Union );
-	case 18:	return( new CPolygon_Update );
-	case 19:	return( new CPolygon_Identity );
-	case 20:	return( new CAdd_Point_Attributes );
-
-	case 21:	return( new CPolygon_Flatten );
-
-	//-----------------------------------------------------
-	case 22:	return( NULL );
-	default:	return( MLB_INTERFACE_SKIP_MODULE );
-	}
-
-	return( NULL );
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CPolygon_Flatten : public CSG_Module
+{
+public: ////// public members and functions: //////////////
 
-	MLB_INTERFACE
+	CPolygon_Flatten(void);
 
-//}}AFX_SAGA
+
+protected: /// protected members and functions: ///////////
+
+	virtual bool		On_Execute				(void);
+
+
+private: ///// private members and functions: /////////////
+
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__polygon_flatten_H
