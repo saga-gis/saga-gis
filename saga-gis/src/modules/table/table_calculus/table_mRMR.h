7c9ed6571f33d1f10ff3b8a6125b7111e0a8af3e
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2014-06-13 12:29:42 +0000

    table_calculus: minimum Redundancy Maximum Relevance (mRMR) feature selection tool added

--- saga-gis/src/modules/table/table_calculus/table_mRMR.h
+++ saga-gis/src/modules/table/table_calculus/table_mRMR.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: mrmr.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -9,14 +9,14 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                    Table_Calculus                     //
+//                    table_calculus                     //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                      table_mRMR.h                     //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
-//                    Andre Ringeler                     //
+//                 Copyright (C) 2014 by                 //
+//                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
@@ -40,13 +40,11 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     aringel@gwdg.de                        //
+//    e-mail:     oconrad@saga-gis.org                   //
 //                                                       //
-//    contact:    Andre Ringeler                         //
+//    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
@@ -55,93 +53,58 @@
 
 
 ///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__table_mRMR_H
+#define HEADER_INCLUDED__table_mRMR_H
 
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Table - Calculus") );
-
-	case MLB_INFO_Author:
-		return( _TL("SAGA User Group Associaton (c) 2002") );
-
-	case MLB_INFO_Description:
-		return( _TL("") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Table|Calculus") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "Fit.h"
-#include "Table_Calculator.h"
-#include "table_running_average.h"
-#include "table_cluster_analysis.h"
-#include "table_pca.h"
-#include "table_fill_record_gaps.h"
-#include "table_field_analyzer.h"
-#include "table_mRMR.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CFit );
-
-	case  1:	return( new CTable_Calculator );
-	case  2:	return( new CTable_Calculator_Shapes );
-
-	case  5:	return( new CTable_Running_Average );
-	case  6:	return( new CTable_Cluster_Analysis );
-	case  7:	return( new CTable_PCA );
-
-	case  8:	return( new CTable_Fill_Record_Gaps );
-
-	case 11:	return( new CTable_Field_Extreme );
-
-	case 12:	return( new CTable_mRMR );
-
-	//-----------------------------------------------------
-	case 19:	return( NULL );
-	default:	return( MLB_INTERFACE_SKIP_MODULE );
-	}
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CTable_mRMR : public CSG_Module
+{
+public: ////// public members and functions: //////////////
 
-	MLB_INTERFACE
+	CTable_mRMR(void);
 
-//}}AFX_SAGA
+
+protected: /// protected members and functions: ///////////
+
+	virtual int			On_Parameters_Enable	(CSG_Parameters *pParameters, CSG_Parameter *pParameter);
+
+	virtual bool		On_Execute				(void);
+
+
+private: ///// private members and functions: /////////////
+
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__table_mRMR_H
