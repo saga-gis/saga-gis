acd29ad20efe561ea3082d69f91175e958563811
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2014-07-14 11:49:08 +0000

    shapes_grid, local grid minima/maxima: bug fix, no data cells. refers to bugs:#195 http://sourceforge.net/p/saga-gis/bugs/195

--- saga-gis/src/modules/shapes/shapes_grid/grid_local_extremes_to_points.cpp
+++ saga-gis/src/modules/shapes/shapes_grid/grid_local_extremes_to_points.cpp
@@ -73,7 +73,7 @@ CGrid_Local_Extremes_to_Points::CGrid_Local_Extremes_to_Points(void)
 {
 	Set_Name		(_TL("Local Minima and Maxima"));
 
-	Set_Author		(SG_T("(c) 2009 by O.Conrad"));
+	Set_Author		(SG_T("O.Conrad (c) 2009"));
 
 	Set_Description	(_TW(
 		"Extracts local grid value minima and maxima of to vector points."
@@ -108,70 +108,79 @@ CGrid_Local_Extremes_to_Points::CGrid_Local_Extremes_to_Points(void)
 //---------------------------------------------------------
 bool CGrid_Local_Extremes_to_Points::On_Execute(void)
 {
-	bool		bMinimum, bMaximum;
-	int			x, y, i, ix, iy;
-	double		z, iz;
-	CSG_Grid	*pGrid;
-	TSG_Point	p;
-	CSG_Shape	*pPoint;
-	CSG_Shapes	*pMinima, *pMaxima;
+	CSG_Grid	*pGrid	= Parameters("GRID")->asGrid();
 
-	pGrid		= Parameters("GRID")	->asGrid();
-	pMinima		= Parameters("MINIMA")	->asShapes();
-	pMaxima		= Parameters("MAXIMA")	->asShapes();
+	//-----------------------------------------------------
+	CSG_Shapes	*pMinima	= Parameters("MINIMA")->asShapes();
 
 	pMinima->Create(SHAPE_TYPE_Point, CSG_String::Format(SG_T("%s [%s]"), pGrid->Get_Name(), _TL("Local Minima")));
-	pMinima->Add_Field(SG_T("GRID_X")	, SG_DATATYPE_Int);
-	pMinima->Add_Field(SG_T("GRID_Y")	, SG_DATATYPE_Int);
-	pMinima->Add_Field(SG_T("X")		, SG_DATATYPE_Double);
-	pMinima->Add_Field(SG_T("Y")		, SG_DATATYPE_Double);
-	pMinima->Add_Field(SG_T("Z")		, SG_DATATYPE_Double);
+	pMinima->Add_Field(SG_T("GRID_X"), SG_DATATYPE_Int);
+	pMinima->Add_Field(SG_T("GRID_Y"), SG_DATATYPE_Int);
+	pMinima->Add_Field(SG_T("X"     ), SG_DATATYPE_Double);
+	pMinima->Add_Field(SG_T("Y"     ), SG_DATATYPE_Double);
+	pMinima->Add_Field(SG_T("Z"     ), SG_DATATYPE_Double);
+
+	//-----------------------------------------------------
+	CSG_Shapes	*pMaxima	= Parameters("MAXIMA")->asShapes();
 
 	pMaxima->Create(SHAPE_TYPE_Point, CSG_String::Format(SG_T("%s [%s]"), pGrid->Get_Name(), _TL("Local Maxima")));
-	pMaxima->Add_Field(SG_T("GRID_X")	, SG_DATATYPE_Int);
-	pMaxima->Add_Field(SG_T("GRID_Y")	, SG_DATATYPE_Int);
-	pMaxima->Add_Field(SG_T("X")		, SG_DATATYPE_Double);
-	pMaxima->Add_Field(SG_T("Y")		, SG_DATATYPE_Double);
-	pMaxima->Add_Field(SG_T("Z")		, SG_DATATYPE_Double);
+	pMaxima->Add_Field(SG_T("GRID_X"), SG_DATATYPE_Int);
+	pMaxima->Add_Field(SG_T("GRID_Y"), SG_DATATYPE_Int);
+	pMaxima->Add_Field(SG_T("X"     ), SG_DATATYPE_Double);
+	pMaxima->Add_Field(SG_T("Y"     ), SG_DATATYPE_Double);
+	pMaxima->Add_Field(SG_T("Z"     ), SG_DATATYPE_Double);
 
-	for(y=0; y<Get_NY() && Set_Progress(y); y++)
+	//-----------------------------------------------------
+	for(int y=0; y<Get_NY() && Set_Progress(y); y++)
 	{
-		for(x=0; x<Get_NX(); x++)
+		for(int x=0; x<Get_NX(); x++)
 		{
-			z	= pGrid->asDouble(x, y);
-
-			for(i=0, bMinimum=true, bMaximum=true; i<8 && (bMinimum || bMaximum); i++)
+			if( !pGrid->is_NoData(x, y) )
 			{
-				if( !Get_System()->Get_Neighbor_Pos(i, x, y, ix, iy) || pGrid->is_NoData(ix, iy) )
+				double	z	= pGrid->asDouble(x, y);
+
+				bool	bMinimum	= true;
+				bool	bMaximum	= true;
+
+				for(int i=0; i<8 && (bMinimum || bMaximum); i++)
 				{
-					bMinimum	= bMaximum	= false;
+					int	ix	= Get_xTo(i, x);
+					int	iy	= Get_yTo(i, y);
+
+					if( !pGrid->is_InGrid(ix, iy) )
+					{
+						bMinimum	= bMaximum	= false;
+
+					}
+					else
+					{
+						double	iz	= pGrid->asDouble(ix, iy);
+
+						if( iz <= z )
+						{
+							bMinimum	= false;
+						}
+						else if( iz >= z )
+						{
+							bMaximum	= false;
+						}
+					}
 				}
-				else
+
+				//-----------------------------------------
+				CSG_Shape	*pPoint	= bMinimum ? pMinima->Add_Shape() : bMaximum ? pMaxima->Add_Shape() : NULL;
+
+				if( pPoint )
 				{
-					iz	= pGrid->asDouble(ix, iy);
+					TSG_Point	p	= Get_System()->Get_Grid_to_World(x, y);
 
-					if( iz <= z )
-					{
-						bMinimum	= false;
-					}
-					else if( iz >= z )
-					{
-						bMaximum	= false;
-					}
+					pPoint->Set_Point(p, 0);
+					pPoint->Set_Value(0, x);
+					pPoint->Set_Value(1, y);
+					pPoint->Set_Value(2, p.x);
+					pPoint->Set_Value(3, p.y);
+					pPoint->Set_Value(4, z);
 				}
-			}
-
-			pPoint	= bMinimum ? pMinima->Add_Shape() : (bMaximum ? pMaxima->Add_Shape() : NULL);
-
-			if( pPoint )
-			{
-				p		= Get_System()->Get_Grid_to_World(x, y);
-				pPoint->Set_Point(p, 0);
-				pPoint->Set_Value(0, x);
-				pPoint->Set_Value(1, y);
-				pPoint->Set_Value(2, p.x);
-				pPoint->Set_Value(3, p.y);
-				pPoint->Set_Value(4, z);
 			}
 		}
 	}
