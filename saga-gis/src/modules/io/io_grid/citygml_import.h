d101f8518fc6c6df1639a3ce8786d1b220005f0c
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2014-08-15 08:31:54 +0000

    io_grid, tool added: import building sketches from citygml derivate

--- saga-gis/src/modules/io/io_grid/citygml_import.h
+++ saga-gis/src/modules/io/io_grid/citygml_import.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id: MLB_Interface.cpp 1921 2014-01-09 10:24:11Z oconrad $
+ * Version $Id: citygml_import.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -9,13 +9,13 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                        Grid_IO                        //
+//                        io_grid                        //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                    citygml_import.h                   //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2014 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -44,9 +44,7 @@
 //                                                       //
 //    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
@@ -55,97 +53,65 @@
 
 
 ///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__citygml_import_H
+#define HEADER_INCLUDED__citygml_import_H
 
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Import/Export - Grids") );
-
-	case MLB_INFO_Author:
-		return( _TL("SAGA User Group Associaton (c) 2002") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for the import and export of gridded data.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("File|Grid") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "esri_arcinfo.h"
-#include "surfer.h"
-#include "raw.h"
-#include "xyz.h"
-#include "usgs_srtm.h"
-#include "mola.h"
-#include "srtm30.h"
-#include "bmp_export.h"
-#include "erdas_lan.h"
-#include "grid_table.h"
-#include "wrf.h"
-#include "citygml_import.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case 0:		return( new CESRI_ArcInfo_Export );
-	case 1:		return( new CESRI_ArcInfo_Import );
-	case 2:		return( new CSurfer_Export );
-	case 3:		return( new CSurfer_Import );
-	case 4:		return( new CRaw_Import );
-	case 5:		return( new CXYZ_Export );
-	case 6:		return( new CXYZ_Import );
-	case 7:		return( new CUSGS_SRTM_Import );
-	case 8:		return( new CMOLA_Import );
-	case 9:		return( new CSRTM30_Import );
-	case 10:	return( new CBMP_Export );
-	case 11:	return( new CErdas_LAN_Import );
-	case 12:	return( new CGrid_Table_Import );
-	case 13:	return( new CWRF_Import );
-	case 14:	return( new CWRF_Export );
-	case 15:	return( new CCityGML_Import );
-
-	default:	return( NULL );
-	}
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CCityGML_Import : public CSG_Module
+{
+public: ////// public members and functions: //////////////
 
-	MLB_INTERFACE
+	CCityGML_Import(void);
 
-//}}AFX_SAGA
+	virtual CSG_String	Get_MenuPath			(void)	{	return( _TL("R:Import") );	}
+
+
+protected: /// protected members and functions: ///////////
+
+	virtual bool		On_Execute				(void);
+
+
+private: ///// private members and functions: /////////////
+
+	bool				Get_Buildings			(const CSG_String &File, CSG_Shapes *pPolygons, bool bParts);
+	bool				Get_Buildings			(const CSG_String &File, CSG_Shapes *pPolygons);
+
+	bool				Add_Buildings			(CSG_Shapes *pBuildings, CSG_Shapes *pAdd);
+
+	bool				Has_BuildingParts		(const CSG_MetaData &GML);
+	bool				Add_BuildingParts		(const CSG_MetaData &GML, CSG_MetaData &GML_Parts);
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__citygml_import_H
