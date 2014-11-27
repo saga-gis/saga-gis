a2f7b254e8407c556c90399655d35180a80eb7b0
Volker Wichmann <wichmann@laserdata.at>
2014-11-27 11:27:59 +0000

    table_calculus: add new tool 'Field Statistics' (allows to calculate simple statistics of table fields)

---
+++ saga-gis/src/modules/table/table_calculus/table_field_statistics.cpp
@@ -0,0 +1,185 @@
+/**********************************************************
+ * Version $Id$
+ *********************************************************/
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                    Module Library:                    //
+//                    table_calculus                     //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//               table_field_statistics.cpp              //
+//                                                       //
+//                 Copyright (C) 2014 by                 //
+//                    Volker Wichmann                    //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation; version 2 of the License.   //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not,       //
+// write to the Free Software Foundation, Inc.,          //
+// 51 Franklin Street, 5th Floor, Boston, MA 02110-1301, //
+// USA.                                                  //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     wichmann@laserdata.at                  //
+//                                                       //
+//    contact:    Volker Wichmann                        //
+//                LASERDATA GmbH                         //
+//                Management and analysis of             //
+//                laserscanning data                     //
+//                Innsbruck, Austria                     //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#include "table_field_statistics.h"
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+CTable_Field_Statistics::CTable_Field_Statistics(void)
+{
+	CSG_Parameter	*pNode;
+
+	//-----------------------------------------------------
+	Set_Name		(_TL("Field Statistics"));
+
+	Set_Author		(SG_T("V. Wichmann (c) 2014"));
+
+	Set_Description	(_TW(
+		"The modules allows one to calculate statistics (n, min, max, range, sum, "
+		"mean, variance and standard deviation) for attribute fields of tables, "
+		"shapefiles or point clouds.\n\n"
+	));
+
+	//-----------------------------------------------------
+	pNode	= Parameters.Add_Table(
+		NULL	, "TABLE"		, _TL("Table"),
+		_TL("The input table."),
+		PARAMETER_INPUT
+	);
+
+	Parameters.Add_Table_Fields(
+		pNode	, "FIELDS"		, _TL("Attributes"),
+		_TL("The (numeric) fields to calculate the statistics for.")
+	);
+
+	Parameters.Add_Table(
+		NULL	, "STATISTICS"	, _TL("Statistics"),
+		_TL("The calculated statistics."),
+		PARAMETER_OUTPUT
+	);
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CTable_Field_Statistics::On_Execute(void)
+{
+	CSG_Table	*pTab_in, *pTab_out;
+	int			nFeatures, *Features;
+
+	//-----------------------------------------------------
+	pTab_in		= Parameters("TABLE")->asTable();
+	pTab_out	= Parameters("STATISTICS")->asTable();
+	
+	Features	= (int *)Parameters("FIELDS")->asPointer();
+	nFeatures	=        Parameters("FIELDS")->asInt    ();
+
+	//-----------------------------------------------------
+	if( !Features || nFeatures <= 0 )
+	{
+		Error_Set(_TL("No attribute fields selected!"));
+
+		return( false );
+	}
+
+	//-----------------------------------------------------
+	pTab_out->Destroy();
+	pTab_out->Set_Name(CSG_String::Format(_TL("%s_stats"), pTab_in->Get_Name()));
+	
+	pTab_out->Add_Field(_TL("Field")	, SG_DATATYPE_String);
+	pTab_out->Add_Field(_TL("n")		, SG_DATATYPE_Long);
+	pTab_out->Add_Field(_TL("min")		, SG_DATATYPE_Double);
+	pTab_out->Add_Field(_TL("max")		, SG_DATATYPE_Double);
+	pTab_out->Add_Field(_TL("range")	, SG_DATATYPE_Double);
+	pTab_out->Add_Field(_TL("sum")		, SG_DATATYPE_Double);
+	pTab_out->Add_Field(_TL("mean")		, SG_DATATYPE_Double);
+	pTab_out->Add_Field(_TL("variance")	, SG_DATATYPE_Double);
+	pTab_out->Add_Field(_TL("stddev")	, SG_DATATYPE_Double);
+
+	//-----------------------------------------------------
+	for(int iFeature=0; iFeature<nFeatures; iFeature++)
+	{
+		if( SG_Data_Type_is_Numeric(pTab_in->Get_Field_Type(iFeature)) )
+		{
+			CSG_Table_Record *pRecord = pTab_out->Add_Record();
+
+			pRecord->Set_Value(0, pTab_in->Get_Field_Name(iFeature));
+			pRecord->Set_Value(1, pTab_in->Get_N(iFeature));
+			pRecord->Set_Value(2, pTab_in->Get_Minimum(iFeature));
+			pRecord->Set_Value(3, pTab_in->Get_Maximum(iFeature));
+			pRecord->Set_Value(4, pTab_in->Get_Range(iFeature));
+			pRecord->Set_Value(5, pTab_in->Get_Sum(iFeature));
+			pRecord->Set_Value(6, pTab_in->Get_Mean(iFeature));
+			pRecord->Set_Value(7, pTab_in->Get_Variance(iFeature));
+			pRecord->Set_Value(8, pTab_in->Get_StdDev(iFeature));
+		}
+		else
+		{
+			SG_UI_Msg_Add(CSG_String::Format(_TL("WARNING: skipping non-numeric field '%s'!"), pTab_in->Get_Field_Name(iFeature)), true);
+		}
+	}
+
+	//-----------------------------------------------------
+	DataObject_Update(pTab_out);
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
--- saga-gis/src/modules/table/table_calculus/table_field_statistics.h
+++ saga-gis/src/modules/table/table_calculus/table_field_statistics.h
@@ -9,14 +9,14 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                    Table_Calculus                     //
+//                    table_calculus                     //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//                table_field_statistics.h               //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
-//                    Andre Ringeler                     //
+//                 Copyright (C) 2014 by                 //
+//                    Volker Wichmann                    //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
@@ -40,115 +40,59 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     aringel@gwdg.de                        //
+//    e-mail:     wichmann@laserdata.at                  //
 //                                                       //
-//    contact:    Andre Ringeler                         //
-//                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
-//                Germany                                //
+//    contact:    Volker Wichmann                        //
+//                LASERDATA GmbH                         //
+//                Management and analysis of             //
+//                laserscanning data                     //
+//                Innsbruck, Austria                     //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                                                       //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-
-
-///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
-///////////////////////////////////////////////////////////
+#ifndef HEADER_INCLUDED__table_field_statistics_H
+#define HEADER_INCLUDED__table_field_statistics_H
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
-
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Calculus") );
-
-	case MLB_INFO_Category:
-		return( _TL("Table") );
-
-	case MLB_INFO_Author:
-		return( _TL("SAGA User Group Associaton (c) 2002-2014") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for table based analyses and calculations.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Table|Calculus") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "Fit.h"
-#include "Table_Calculator.h"
-#include "table_running_average.h"
-#include "table_cluster_analysis.h"
-#include "table_pca.h"
-#include "table_fill_record_gaps.h"
-#include "table_field_analyzer.h"
-#include "table_mRMR.h"
-#include "table_field_statistics.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CFit );
-
-	case  1:	return( new CTable_Calculator );
-	case  2:	return( new CTable_Calculator_Shapes );
-
-	case  5:	return( new CTable_Running_Average );
-	case  6:	return( new CTable_Cluster_Analysis(false) );
-	case 14:	return( new CTable_Cluster_Analysis(true) );
-	case  7:	return( new CTable_PCA );
-
-	case  8:	return( new CTable_Fill_Record_Gaps );
-
-	case 11:	return( new CTable_Field_Extreme );
-
-	case 12:	return( new CTable_mRMR );
-
-	case 15:	return( new CTable_Field_Statistics() );
-
-	//-----------------------------------------------------
-	case 19:	return( NULL );
-	default:	return( MLB_INTERFACE_SKIP_MODULE );
-	}
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //
+//                                                       //
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CTable_Field_Statistics : public CSG_Module
+{
+public:
+	CTable_Field_Statistics(void);
 
-	MLB_INTERFACE
 
-//}}AFX_SAGA
+protected:
+
+	virtual bool			On_Execute				(void);
+
+
+private:
+
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                                                       //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__table_field_statistics_H
