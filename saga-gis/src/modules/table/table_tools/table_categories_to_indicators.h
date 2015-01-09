e02a702a9cdd7fe7899402ae5fb57f5e9baa54f3
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2015-01-09 15:13:32 +0000

    table_tools, tool added: add indicators for categorical attributes

--- saga-gis/src/modules/table/table_tools/table_categories_to_indicators.h
+++ saga-gis/src/modules/table/table_tools/table_categories_to_indicators.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id$
+ * Version $Id: table_categories_to_indicators.h 911 2011-02-14 16:38:15Z reklov_w $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -9,14 +9,14 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                      Table_Tools                      //
+//                     table_tools                       //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//            table_categories_to_indicators.h           //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
-//              SAGA User Group Associaton               //
+//                 Copyright (C) 2015 by                 //
+//                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
@@ -40,119 +40,66 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     aringel@gwdg.de                        //
+//    e-mail:     oconrad@saga-gis.org                   //
 //                                                       //
-//    contact:    SAGA User Group Associaton             //
+//    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 
-
 ///////////////////////////////////////////////////////////
-//														 //
-//			The Module Link Library Interface			 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__table_categories_to_indicators_H
+#define HEADER_INCLUDED__table_categories_to_indicators_H
 
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general module library informations here...
-
-CSG_String Get_Info(int i)
-{
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Tools") );
-
-	case MLB_INFO_Category:
-		return( _TL("Table") );
-
-	case MLB_INFO_Author:
-		return( _TL("SAGA User Group Associaton (c) 2002-2013") );
-
-	case MLB_INFO_Description:
-		return( _TL("Tools for the creation and manipulation of tables.") );
-
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case MLB_INFO_Menu_Path:
-		return( _TL("Table|Tools") );
-	}
-}
-
-
-//---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "Table_Create_Empty.h"
-#include "Table_Rotate.h"
-#include "Table_Enumerate.h"
-#include "Join_Tables.h"
-#include "table_change_date_format.h"
-#include "table_change_field_type.h"
-#include "table_change_color_format.h"
-#include "table_text_replacer.h"
-#include "table_field_deletion.h"
-#include "table_selection.h"
-#include "table_categories_to_indicators.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CTable_Create_Empty );
-	case  1:	return( new CTable_Rotate );
-	case  2:	return( new CTable_Enumerate );
-	case  3:	return( new CJoin_Tables );
-	case  4:	return( new CJoin_Tables_Shapes );
-	case  5:	return( new CTable_Change_Date_Format );
-	case  6:	return( new CTable_Change_Time_Format );
-	case  7:	return( new CTable_Change_Field_Type );
-	case  8:	return( new CTable_Append_Rows );
-	case  9:	return( new CTable_Change_Color_Format );
-	case 10:	return( new CTable_Text_Replacer );
-	case 11:	return( new CTable_Field_Deletion );
-
-	case 15:	return( new CSelection_Copy );
-	case 16:	return( new CSelection_Delete );
-	case 17:	return( new CSelection_Invert );
-	case 18:	return( new CSelect_Numeric );
-	case 19:	return( new CSelect_String );
-
-	case 20:	return( new CTable_Categories_to_Indicators );
-
-	case 25:	return( NULL );
-	default:	return( MLB_INTERFACE_SKIP_MODULE );
-	}
-}
-
-
 ///////////////////////////////////////////////////////////
-//														 //
-//														 //
-//														 //
+//                                                       //												
+//                                                       //												
+//                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
+class CTable_Categories_to_Indicators : public CSG_Module
+{
+public:
+	CTable_Categories_to_Indicators(void);
 
-	MLB_INTERFACE
 
-//}}AFX_SAGA
+protected:
+
+	virtual int			On_Parameters_Enable	(CSG_Parameters *pParameters, CSG_Parameter *pParameter);
+
+	virtual bool		On_Execute				(void);
+
+
+};
+
+
+///////////////////////////////////////////////////////////
+//                                                       //												
+//                                                       //												
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__table_categories_to_indicators_H
