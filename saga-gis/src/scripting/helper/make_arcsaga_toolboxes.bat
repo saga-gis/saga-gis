3e4af5e29ad7b1ca46b573e62d08ce3659113578
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2015-12-09 18:25:21 +0000

    make saga release helpers, arcsaga toolboxes

---
+++ saga-gis/src/scripting/helper/make_arcsaga_toolboxes.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+
+REM ___________________________________
+SET SAGA_ROOT=D:\saga\saga-code\trunk\saga-gis
+
+REM ___________________________________
+SET OUTDIR=ArcSAGA Toolboxes
+
+IF EXIST "%OUTDIR%" RMDIR "%OUTDIR%" /S/Q
+MKDIR "%OUTDIR%"
+
+COPY "%SAGA_ROOT%\src\dev_tools\ArcSAGA.py" "%OUTDIR%"
+COPY "%SAGA_ROOT%\src\dev_tools\ArcSAGA_Grid.lyr" "%OUTDIR%\grid.lyr"
+
+"%SAGA_ROOT%\bin\saga_vc_win32\saga_cmd.exe" dev_tools 5 -BOX_NAMING=1 -ARC_VERSION=1 -DIRECTORY="%OUTDIR%"
+
+RMDIR /S/Q "%SAGA_ROOT%\bin\saga_vc_win32\%OUTDIR%\"
+XCOPY /C/Q/Y "%OUTDIR%" "%SAGA_ROOT%\bin\saga_vc_win32\%OUTDIR%\"
+RMDIR /S/Q "%SAGA_ROOT%\bin\saga_vc_x64\%OUTDIR%\"
+XCOPY /C/Q/Y "%OUTDIR%" "%SAGA_ROOT%\bin\saga_vc_x64\%OUTDIR%\"
+
+RMDIR "%OUTDIR%" /S/Q
+
+REM ___________________________________
+SET OUTDIR=ArcSAGA Toolboxes 10.1
+
+IF EXIST "%OUTDIR%" RMDIR "%OUTDIR%" /S/Q
+MKDIR "%OUTDIR%"
+
+COPY "%SAGA_ROOT%\src\dev_tools\ArcSAGA.py" "%OUTDIR%"
+COPY "%SAGA_ROOT%\src\dev_tools\ArcSAGA_Grid.lyr" "%OUTDIR%\grid.lyr"
+
+"%SAGA_ROOT%\bin\saga_vc_win32\saga_cmd.exe" dev_tools 5 -BOX_NAMING=1 -ARC_VERSION=1 -DIRECTORY="%OUTDIR%"
+
+RMDIR /S/Q "%SAGA_ROOT%\bin\saga_vc_win32\%OUTDIR%\"
+XCOPY /C/Q/Y "%OUTDIR%" "%SAGA_ROOT%\bin\saga_vc_win32\%OUTDIR%\"
+RMDIR /S/Q "%SAGA_ROOT%\bin\saga_vc_x64\%OUTDIR%\"
+XCOPY /C/Q/Y "%OUTDIR%" "%SAGA_ROOT%\bin\saga_vc_x64\%OUTDIR%\"
+
+RMDIR "%OUTDIR%" /S/Q
+
+REM ___________________________________
+REM PAUSE
