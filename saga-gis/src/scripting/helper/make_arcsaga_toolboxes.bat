356639438e07bf6225eae716e8c5da35794d7787
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2015-12-16 12:08:25 +0000

    make saga release helpers, arcsaga/translation: updates

--- saga-gis/src/scripting/helper/make_arcsaga_toolboxes.bat
+++ saga-gis/src/scripting/helper/make_arcsaga_toolboxes.bat
@@ -30,7 +30,7 @@ MKDIR "%OUTDIR%"
 COPY "%SAGA_ROOT%\src\dev_tools\ArcSAGA.py" "%OUTDIR%"
 COPY "%SAGA_ROOT%\src\dev_tools\ArcSAGA_Grid.lyr" "%OUTDIR%\grid.lyr"
 
-"%SAGA_ROOT%\bin\saga_vc_win32\saga_cmd.exe" dev_tools 5 -BOX_NAMING=1 -ARC_VERSION=1 -DIRECTORY="%OUTDIR%"
+"%SAGA_ROOT%\bin\saga_vc_win32\saga_cmd.exe" dev_tools 5 -BOX_NAMING=1 -ARC_VERSION=0 -DIRECTORY="%OUTDIR%"
 
 RMDIR /S/Q "%SAGA_ROOT%\bin\saga_vc_win32\%OUTDIR%\"
 XCOPY /C/Q/Y "%OUTDIR%" "%SAGA_ROOT%\bin\saga_vc_win32\%OUTDIR%\"
--- saga-gis/src/scripting/helper/make_translation_files.bat
+++ saga-gis/src/scripting/helper/make_translation_files.bat
@@ -1,17 +1,29 @@
 @ECHO OFF
 
 REM ___________________________________
+SET SVNEXE=svn
+
 SET SAGA_ROOT=D:\saga\saga-code\trunk\saga-gis
 SET SAGA_CMD="%SAGA_ROOT%\bin\saga_vc_win32\saga_cmd.exe" -f=s
+SET SAGA_SRC=saga_src
 
 SET RESPATH=%SAGA_ROOT%\src\saga_core\saga_gui\res
 
 REM ___________________________________
-%SAGA_CMD% dev_tools 0 -TARGET="%RESPATH%\saga.lng.txt" -DIRECTORY="%SAGA_ROOT%\src"
+REM SVN Source Code Repository
+%SVNEXE% checkout svn://svn.code.sf.net/p/saga-gis/code-0/trunk %SAGA_SRC% -q --non-interactive
+PUSHD %SAGA_SRC%
+RMDIR /S/Q .svn
+POPD
+
+REM ___________________________________
+%SAGA_CMD% dev_tools 0 -TARGET="%RESPATH%\saga.lng.txt" -DIRECTORY="%SAGA_SRC%"
 
 %SAGA_CMD% dev_tools 1 -MASTER="%RESPATH%\saga.lng.txt" -IMPORT="%RESPATH%\saga.ger.txt" -UNION="%RESPATH%\saga.ger.txt" -METHOD=1
 
 DEL "%RESPATH%\*.mtab"
+
+RMDIR /S/Q %SAGA_SRC%
 
 REM ___________________________________
 PAUSE
