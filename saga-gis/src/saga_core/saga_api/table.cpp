8e69af9e2c5c5d8c5d9a3d8ffec82fba3f183c41
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2015-12-21 08:54:37 +0000

    saga_api, table.cpp: fixes gcc-4.8.5 compiler issue, reported by rainer hurling in '[saga-gis-developer] r2690 does not build on FreeBSD'

--- saga-gis/src/saga_core/saga_api/table.cpp
+++ saga-gis/src/saga_core/saga_api/table.cpp
@@ -175,7 +175,7 @@ bool CSG_Table::Create(const CSG_String &File_Name, TSG_Table_File_Type Format)
 	SG_UI_Msg_Add(CSG_String::Format("%s: %s...", _TL("Load table"), File_Name.c_str()), true);
 
 	//-----------------------------------------------------
-	bool	bResult	= File_Name.BeforeFirst(':').Cmp("PGSQL") && SG_File_Exists(File_Name) && Load(File_Name, (int)Format, NULL);
+	bool	bResult	= File_Name.BeforeFirst(':').Cmp("PGSQL") && SG_File_Exists(File_Name) && Load(File_Name, (int)Format, '\0');
 
 	if( bResult )
 	{
