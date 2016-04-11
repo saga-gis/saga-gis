32e35137eaf13de0e1ac42bf8b86c0420278257e
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2016-04-11 16:14:53 +0000

    cluster_isodata.cpp, switch to gpl v3: refers to [bugs:#226] License for cluster_isodata should be GPL3 (or GPL3+)

--- saga-gis/src/modules/imagery/imagery_classification/cluster_isodata.cpp
+++ saga-gis/src/modules/imagery/imagery_classification/cluster_isodata.cpp
@@ -21,7 +21,8 @@
 // Geoscientific Analyses'. SAGA is free software; you   //
 // can redistribute it and/or modify it under the terms  //
 // of the GNU General Public License as published by the //
-// Free Software Foundation; version 2 of the License.   //
+// Free Software Foundation, either version 3 of the     // 
+// License, or (at your option) any later version.       //
 //                                                       //
 // SAGA is distributed in the hope that it will be       //
 // useful, but WITHOUT ANY WARRANTY; without even the    //
@@ -31,9 +32,7 @@
 //                                                       //
 // You should have received a copy of the GNU General    //
 // Public License along with this program; if not,       //
-// write to the Free Software Foundation, Inc.,          //
-// 59 Temple Place - Suite 330, Boston, MA 02111-1307,   //
-// USA.                                                  //
+// see <http://www.gnu.org/licenses/>.                   //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
