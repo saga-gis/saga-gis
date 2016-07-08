f205f1cf73a1a759e0018ac380818dfc0ac97245
oconrad <conrad@geowiss.uni-hamburg.de>
2016-07-08 15:07:48 +0200

    table_calculus: record statistics tool added

--- saga-gis/src/modules/table/table_calculus/table_record_statistics.h
+++ saga-gis/src/modules/table/table_calculus/table_record_statistics.h
@@ -1,6 +1,3 @@
-/**********************************************************
- * Version $Id$
- *********************************************************/
 
 ///////////////////////////////////////////////////////////
 //                                                       //
@@ -9,14 +6,14 @@
 //      System for Automated Geoscientific Analyses      //
 //                                                       //
 //                    Module Library:                    //
-//                    Table_Calculus                     //
+//                    table_calculus                     //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   MLB_Interface.cpp                   //
+//               table_record_statistics.h               //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
-//                    Andre Ringeler                     //
+//                 Copyright (C) 2016 by                 //
+//                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
@@ -40,107 +37,75 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//    e-mail:     aringel@gwdg.de                        //
+//    e-mail:     oconrad@saga-gis.org                   //
 //                                                       //
-//    contact:    Andre Ringeler                         //
+//    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 
-
 ///////////////////////////////////////////////////////////
 //														 //
-//			The Module Link Library Interface			 //
+//														 //
 //														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#include <saga_api/saga_api.h>
 
-#include "MLB_Interface.h"
 
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 2. Place general module library informations here...
-
-CSG_String Get_Info(int i)
+class CTable_Record_Statistics_Base : public CSG_Module  
 {
-	switch( i )
-	{
-	case MLB_INFO_Name:	default:
-		return( _TL("Calculus") );
+public:
+	CTable_Record_Statistics_Base(void);
 
-	case MLB_INFO_Category:
-		return( _TL("Table") );
 
-	case MLB_INFO_Author:
-		return( _TL("SAGA User Group Associaton (c) 2002-2014") );
+protected:
 
-	case MLB_INFO_Description:
-		return( _TL("Tools for table based analyses and calculations.") );
+	int						On_Parameters_Enable	(CSG_Parameters *pParameters, CSG_Parameter *pParameter);
 
-	case MLB_INFO_Version:
-		return( SG_T("1.0") );
+	virtual bool			On_Execute				(void);
 
-	case MLB_INFO_Menu_Path:
-		return( _TL("Table|Calculus") );
-	}
-}
+};
 
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 3. Include the headers of your modules here...
-
-#include "Fit.h"
-#include "Table_Calculator.h"
-#include "table_running_average.h"
-#include "table_cluster_analysis.h"
-#include "table_pca.h"
-#include "table_fill_record_gaps.h"
-#include "table_field_analyzer.h"
-#include "table_mRMR.h"
-#include "table_field_statistics.h"
-#include "table_record_statistics.h"
-
-
-//---------------------------------------------------------
-// 4. Allow your modules to be created here...
-
-CSG_Module *		Create_Module(int i)
+class CTable_Record_Statistics : public CTable_Record_Statistics_Base  
 {
-	switch( i )
-	{
-	case  0:	return( new CFit );
+public:
+	CTable_Record_Statistics(void);
 
-	case  1:	return( new CTable_Calculator );
-	case  2:	return( new CTable_Calculator_Shapes );
+};
 
-	case  5:	return( new CTable_Running_Average );
-	case  6:	return( new CTable_Cluster_Analysis(false) );
-	case 14:	return( new CTable_Cluster_Analysis(true) );
-	case  7:	return( new CTable_PCA );
 
-	case  8:	return( new CTable_Fill_Record_Gaps );
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
 
-	case 11:	return( new CTable_Field_Extreme );
+//---------------------------------------------------------
+class CTable_Record_Statistics_Shapes : public CTable_Record_Statistics_Base  
+{
+public:
+	CTable_Record_Statistics_Shapes(void);
 
-	case 12:	return( new CTable_mRMR );
+	virtual CSG_String		Get_MenuPath		(void)	{	return( _TL("A:Shapes|Table") );	}
 
-	case 15:	return( new CTable_Field_Statistics() );
-	case 16:	return( new CTable_Record_Statistics() );
-	case 17:	return( new CTable_Record_Statistics_Shapes() );
-
-	//-----------------------------------------------------
-	case 19:	return( NULL );
-	default:	return( MLB_INTERFACE_SKIP_MODULE );
-	}
-}
+};
 
 
 ///////////////////////////////////////////////////////////
@@ -150,8 +115,3 @@ CSG_Module *		Create_Module(int i)
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
-
-	MLB_INTERFACE
-
-//}}AFX_SAGA
