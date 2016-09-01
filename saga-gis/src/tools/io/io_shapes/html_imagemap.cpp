8abaf18dc59c46a59017cbd8c56c3e8dfd8a920e
oconrad <conrad@geowiss.uni-hamburg.de>
2016-09-01 14:13:58 +0200

    io_shapes, tool added: export html image map

---
+++ saga-gis/src/tools/io/io_shapes/html_imagemap.cpp
@@ -0,0 +1,275 @@
+/**********************************************************
+ * Version $Id: html_imagemap.cpp 1921 2014-01-09 10:24:11Z oconrad $
+ *********************************************************/
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                     Tool Library                      //
+//                       io_shapes                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//                   html_imagemap.cpp                   //
+//                                                       //
+//                 Copyright (C) 2016 by                 //
+//                      Olaf Conrad                      //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation; version 2 of the License.   //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not,       //
+// write to the Free Software Foundation, Inc.,          //
+// 51 Franklin Street, 5th Floor, Boston, MA 02110-1301, //
+// USA.                                                  //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     oconrad@saga-gis.org                   //
+//                                                       //
+//    contact:    Olaf Conrad                            //
+//                Institute of Geography                 //
+//                University of Hamburg                  //
+//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#include "html_imagemap.h"
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+CHTML_ImageMap::CHTML_ImageMap(void)
+{
+	Set_Name		(_TL("Export HTML Image Map"));
+
+	Set_Author		("P.Lamp, O.Conrad (c) 2016");
+
+	Set_Description	(_TW(
+		"Tool to create an HTML ImageMap with polygon dataset and image. "
+		"The tool requires a polygon dataset and an georeferenced image file. "
+		"It outputs an html file for possible further editing. "
+		"The image name in the mapfile html is hard coded to be map.png.\n"
+		"\n"
+		"<b>How to build the link:</b>"
+		"<ul>"
+		"<li><b>Link</b> Select link identfier from the attribute table of the polygon data layer.\n"
+		"<li><b>Prefix</b> Adds a prefix to the link identifier. If just for icon with alternative text and, add <i>'#'</i>).\n"
+		"<li><b>Suffix</b> Adds a suffix of the link identifier. Likewise <i>'.txt, .html, .doc, .png'</i>.\n"
+		"</ul>The link will be built with the pattern <i>'Prefix + Identifier + Suffix'</i>.\n"
+		"\n"
+		"<b>Take care!:</b>"
+		"<ul>"
+		"<li>Image and Polygon dataset have to share the same projection."
+		"<li>Special characters in link (like &ouml;, &szlig, oder &raquo;) are not automatically replaced to html entities. Replace them after creation in editor if needed."
+		"</ul>\n"
+		"\n"
+		"<b>Example for GUI usage:</b>"
+		"<ol>"
+		"<li>Load your polygon dataset and open it in a map."
+		"<li>Create an image of this map using the map's menu command <i>'Save Map to Workspace'</i> and save it to an image file using the file name <i>'map.png'</i>."
+		"<li>Run this tool and save the output to the same directory as the image."
+		"<li>Open the html file in a browser for preview."
+		"<li>Further editing of the html file is suggested."
+		"</ol>"
+	));
+
+	//-----------------------------------------------------
+	CSG_Parameter	*pNode	= Parameters.Add_Shapes(
+		NULL	, "POLYGONS"		, _TL("Polygons"),
+		_TL(""),
+		PARAMETER_INPUT, SHAPE_TYPE_Polygon
+	);
+
+	Parameters.Add_Table_Field(
+		pNode	, "LINK"			, _TL("Link"),
+		_TL(""),
+		true
+	);
+
+	Parameters.Add_Table_Field(
+		pNode	, "TITLE"			, _TL("Title"),
+		_TL(""),
+		true
+	);
+
+	Parameters.Add_Grid_System(
+		NULL	, "IMAGE"			, _TL("Image"),
+		_TL("grid system of georeferenced image")
+	);
+
+	Parameters.Add_FilePath(
+		NULL	, "FILE"			, _TL("File"),
+		_TL(""),
+		CSG_String::Format("%s|%s|%s|%s",
+			_TL("HTML File (*.html)"), SG_T("*.html"),
+			_TL("All Files")         , SG_T("*.*")
+		), NULL, true
+ 	);
+
+	Parameters.Add_String(
+		NULL	, "LINK_PREFIX"		, _TL("Link Prefix"),
+		_TL(""),
+		"http://www.saga-gis.org/"
+	);
+
+	Parameters.Add_String(
+		NULL	, "LINK_SUFFIX"		, _TL("Link Suffix"),
+		_TL(""),
+		"index.html"
+	);
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CHTML_ImageMap::On_Execute(void)
+{
+	CSG_Shapes	*pPolygons	= Parameters("POLYGONS")->asShapes();
+
+	m_Image	= *Parameters("IMAGE")->asGrid_System();
+
+	if( !m_Image.Get_Extent().Intersects(pPolygons->Get_Extent()) )
+	{
+		Error_Set(_TL("image and polygons do not overlap"));
+
+		return( false );
+	}
+
+	//-----------------------------------------------------
+	CSG_MetaData	HTML, *pEntry;
+
+	HTML.Set_Name("body");
+
+	pEntry	= HTML.Add_Child("img");
+
+	pEntry->Add_Property("src"   , "map.png");
+	pEntry->Add_Property("width" , m_Image.Get_NX());
+	pEntry->Add_Property("height", m_Image.Get_NY());
+	pEntry->Add_Property("alt"   , "map");
+	pEntry->Add_Property("usemap", "#image_map");
+
+	pEntry	= HTML.Add_Child("map");
+	pEntry->Add_Property("name", "image_map");
+
+	//-----------------------------------------------------
+	int	Link	= Parameters("LINK" )->asInt();
+	int	Title	= Parameters("TITLE")->asInt();
+
+	CSG_String	Prefix	= Parameters("LINK_PREFIX")->asString();
+	CSG_String	Suffix	= Parameters("LINK_SUFFIX")->asString();
+
+	for(int iPolygon=0; iPolygon<pPolygons->Get_Count() && Set_Progress(iPolygon, pPolygons->Get_Count()); iPolygon++)
+	{
+		CSG_Shape_Polygon	*pPolygon	= (CSG_Shape_Polygon *)pPolygons->Get_Shape(iPolygon);
+
+		for(int iPart=0; iPart<pPolygon->Get_Part_Count(); iPart++)
+		{
+			CSG_String	Coords;
+
+			if( !pPolygon->is_Lake(iPart) && Get_Polygon(Coords, pPolygon->Get_Part(iPart)) )
+			{
+				CSG_MetaData	*pArea	= pEntry->Add_Child("area");
+
+				pArea->Add_Property("shape", "poly");
+				pArea->Add_Property("coords", Coords);
+
+				pArea->Add_Property("href", Prefix + pPolygon->asString(Link) + Suffix);
+
+				CSG_String	s;
+
+				if( Title < 0 )
+				{
+					s	= CSG_String::Format("%d. %s, %d. %s", 1 + iPolygon, _TL("Polygon"), 1 + iPart, _TL("Part"));
+				}
+				else
+				{
+					s	= pPolygon->asString(Title);
+				}
+
+				pArea->Add_Property("title", s);
+				pArea->Add_Property("alt"  , s);
+			}
+		}
+	}
+
+	//-----------------------------------------------------
+	if( !HTML.Save(Parameters("FILE")->asString(), SG_T("html")) )
+	{
+		Error_Fmt("%s [%s]", _TL("failed to save file"), Parameters("FILE")->asString());
+
+		return( false );
+	}
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CHTML_ImageMap::Get_Polygon(CSG_String &Coords, CSG_Shape_Part *pPolygon)
+{
+	double	Scale	= m_Image.Get_NX() / m_Image.Get_XRange(true);
+
+	for(int iPoint=0; iPoint<pPolygon->Get_Count(); iPoint++)
+	{
+		TSG_Point	p	= pPolygon->Get_Point(iPoint);
+
+		if( iPoint > 0 )
+		{
+			Coords	+= ",";
+		}
+
+		Coords	+= CSG_String::Format("%d,%d",
+			(int)(0.5 + (p.x - m_Image.Get_XMin(true)) * Scale),
+			(int)(0.5 + (m_Image.Get_YMax(true) - p.y) * Scale)
+		);
+	}
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
--- saga-gis/src/tools/io/io_shapes/html_imagemap.h
+++ saga-gis/src/tools/io/io_shapes/html_imagemap.h
@@ -1,5 +1,5 @@
 /**********************************************************
- * Version $Id: TLB_Interface.cpp 1921 2014-01-09 10:24:11Z oconrad $
+ * Version $Id: html_imagemap.h 1921 2014-01-09 10:24:11Z oconrad $
  *********************************************************/
 
 ///////////////////////////////////////////////////////////
@@ -13,9 +13,9 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   TLB_Interface.cpp                   //
+//                    html_imagemap.h                    //
 //                                                       //
-//                 Copyright (C) 2003 by                 //
+//                 Copyright (C) 2016 by                 //
 //                      Olaf Conrad                      //
 //                                                       //
 //-------------------------------------------------------//
@@ -44,9 +44,7 @@
 //                                                       //
 //    contact:    Olaf Conrad                            //
 //                Institute of Geography                 //
-//                University of Goettingen               //
-//                Goldschmidtstr. 5                      //
-//                37077 Goettingen                       //
+//                University of Hamburg                  //
 //                Germany                                //
 //                                                       //
 ///////////////////////////////////////////////////////////
@@ -56,100 +54,53 @@
 
 ///////////////////////////////////////////////////////////
 //														 //
-//           The Tool Link Library Interface             //
+//														 //
 //														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__html_imagemap_H
+#define HEADER_INCLUDED__html_imagemap_H
 
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
 
-//---------------------------------------------------------
-// 2. Place general tool library informations here...
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
-CSG_String Get_Info(int i)
+//---------------------------------------------------------
+class CHTML_ImageMap : public CSG_Tool
 {
-	switch( i )
-	{
-	case TLB_INFO_Name:	default:
-		return( _TL("Shapes") );
+public:
+	CHTML_ImageMap(void);
 
-	case TLB_INFO_Category:
-		return( _TL("Import/Export") );
-
-	case TLB_INFO_Author:
-		return( SG_T("SAGA User Group Associaton (c) 2002-10") );
-
-	case TLB_INFO_Description:
-		return( _TL("Tools for the import and export of vector data.") );
-
-	case TLB_INFO_Version:
-		return( SG_T("1.0") );
-
-	case TLB_INFO_Menu_Path:
-		return( _TL("File|Shapes") );
-	}
-}
+	virtual CSG_String			Get_MenuPath		(void)	{	return( _TL("Export") );	}
 
 
-//---------------------------------------------------------
-// 3. Include the headers of your tools here...
+protected:
 
-#include "gstat.h"
-#include "xyz.h"
-#include "generate.h"
-#include "surfer_bln.h"
-#include "atlas_bna.h"
-#include "wasp_map.h"
-#include "stl.h"
-#include "gpx.h"
-#include "pointcloud_from_file.h"
-#include "pointcloud_from_text_file.h"
-#include "svg.h"
-#include "pointcloud_to_text_file.h"
-#include "wktb.h"
-#include "citygml_import.h"
-#include "html_imagemap.h"
+	virtual bool				On_Execute			(void);
 
 
-//---------------------------------------------------------
-// 4. Allow your tools to be created here...
+private:
 
-CSG_Tool *		Create_Tool(int i)
-{
-	switch( i )
-	{
-	case  0:	return( new CGStat_Export );
-	case  1:	return( new CGStat_Import );
-	case  2:	return( new CXYZ_Export );
-	case  3:	return( new CXYZ_Import );
-	case  4:	return( new CGenerate_Export );
-	case  5:	return( new CSurfer_BLN_Export );
-	case  6:	return( new CSurfer_BLN_Import );
-	case  7:	return( new CAtlas_BNA_Export );
-	case  8:	return( new CAtlas_BNA_Import );
-	case  9:	return( new CWASP_MAP_Export );
-	case 10:	return( new CWASP_MAP_Import );
-	case 11:	return( new CSTL_Import );
-	case 12:	return( new CSTL_Export );
-	case 13:	return( new CGPX_Import );
-	case 14:	return( new CGPX_Export );
-	case 15:	return( new CPointCloud_From_File );
-	case 16:	return( new CPointCloud_From_Text_File );
-	case 17:	return( new CSVG_Export );
-	case 18:	return( new CPointcloud_To_Text_File );
-	case 19:	return( new CWKT_Import );
-	case 20:	return( new CWKT_Export );
-	case 21:	return( new CCityGML_Import );
-	case 22:	return( new CHTML_ImageMap );
+	CSG_Grid_System				m_Image;
 
-	//-----------------------------------------------------
-	case 23:	return( NULL );
-	default:	return( TLB_INTERFACE_SKIP_TOOL );
-	}
-}
+
+	bool						Get_Polygon			(CSG_String &Coords, CSG_Shape_Part *pPolygon);
+
+};
 
 
 ///////////////////////////////////////////////////////////
@@ -159,8 +110,4 @@ CSG_Tool *		Create_Tool(int i)
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
-
-	TLB_INTERFACE
-
-//}}AFX_SAGA
+#endif // #ifndef HEADER_INCLUDED__html_imagemap_H
