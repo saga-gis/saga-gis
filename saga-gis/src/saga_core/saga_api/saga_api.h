78839c43f7e62202a49f7dcbe599e2a90f09ecee
oconrad <conrad@geowiss.uni-hamburg.de>
2016-10-13 14:59:18 +0200

    saga version for coming release set to 3.0.0 (instead of 2.4.0) because of backwards incompatible API changes

--- saga-gis/src/saga_core/saga_api/saga_api.h
+++ saga-gis/src/saga_core/saga_api/saga_api.h
@@ -126,10 +126,10 @@
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-#define SAGA_MAJOR_VERSION		2
-#define SAGA_MINOR_VERSION		4
+#define SAGA_MAJOR_VERSION		3
+#define SAGA_MINOR_VERSION		0
 #define SAGA_RELEASE_NUMBER		0
-#define SAGA_VERSION			SG_T("2.4.0")
+#define SAGA_VERSION			SG_T("3.0.0")
 
 
 ///////////////////////////////////////////////////////////
