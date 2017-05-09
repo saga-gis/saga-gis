dd3769bb88b763de56d3fca8f08fba5981396d3a
oconrad <conrad@geowiss.uni-hamburg.de>
2017-05-09 12:45:31 +0200

    saga_api, CSG_Simple_Statistics::Get_Gini: slight modification

--- saga-gis/src/saga_core/saga_api/mat_tools.cpp
+++ saga-gis/src/saga_core/saga_api/mat_tools.cpp
@@ -349,6 +349,8 @@ bool CSG_Simple_Statistics::Create(const CSG_Simple_Statistics &Statistics)
 	m_Kurtosis		= Statistics.m_Kurtosis;
 	m_Skewness		= Statistics.m_Skewness;
 
+	m_Gini			= Statistics.m_Gini;
+
 	m_bSorted		= Statistics.m_bSorted;
 	m_Values		.Create(Statistics.m_Values);
 
@@ -411,6 +413,8 @@ void CSG_Simple_Statistics::Invalidate(void)
 
 	m_Kurtosis		= 0.0;
 	m_Skewness		= 0.0;
+
+	m_Gini			= -1.0;
 
 	m_bSorted		= false;
 	m_Values		.Destroy();
@@ -569,6 +573,37 @@ double CSG_Simple_Statistics::Get_Quantile(double Quantile)
 	}
 
 	return( m_Mean );
+}
+
+//---------------------------------------------------------
+/**
+  * The Gini coefficient is a measure of statistical dispersion
+  * intended to represent the income or wealth distribution of 
+  * a nation's residents, and is the most commonly used measure
+  * of inequality.
+*/
+double CSG_Simple_Statistics::Get_Gini(void)
+{
+	if( m_Gini < 0.0 && m_Values.Get_Size() > 1 )
+	{
+		if( !m_bSorted )
+		{
+			qsort(m_Values.Get_Array(), m_Values.Get_Size(), sizeof(double), SG_Compare_Double);
+
+			m_bSorted	= true;
+		}
+
+		m_Gini	= 0.0;
+
+		for(int i=0; i<Get_Count(); i++)
+		{
+			m_Gini	+= (i + 1.0) * Get_Value(i);
+		}
+
+		m_Gini	= 2.0 * m_Gini / (Get_Count() * Get_Sum()) - (Get_Count() + 1.0) / Get_Count();
+	}
+
+	return( m_Gini );
 }
 
 
@@ -798,35 +833,6 @@ int CSG_Category_Statistics::Get_Category(const CSG_String &Value)	const
 	}
 
 	return( -1 );
-}
-
-double CSG_Simple_Statistics::Get_Gini()
-{
-	if( m_Values.Get_Size() > 1 )
-	{
-		if( !m_bSorted )
-		{
-			qsort(m_Values.Get_Array(), m_Values.Get_Size(), sizeof(double), SG_Compare_Double);
-
-			m_bSorted	= true;
-		}
-		double sum = this->Get_Sum();
-		double cumsum = 0;
-		int n = m_Values.Get_Size();
-
-		for (int i =0; i< n; i++)
-		{
-			cumsum += (i+1) * Get_Value(i);
-		}
-
-		double gini = 2 * cumsum / (n*sum) - (n + 1) / n;
-
-		return( gini);
-	}
-	else
-	{
-		return( -1 );
-	}
 }
 
 
