fdc2f2cb5925af2048d325ebf692fca1d3577551
oconrad <conrad@geowiss.uni-hamburg.de>
2017-10-24 16:46:23 +0200

    statistics_points/GSPoints_Pattern_Analysis.cpp: weighting option added

--- saga-gis/src/tools/statistics/statistics_points/GSPoints_Pattern_Analysis.cpp
+++ saga-gis/src/tools/statistics/statistics_points/GSPoints_Pattern_Analysis.cpp
@@ -70,44 +70,48 @@
 //---------------------------------------------------------
 CGSPoints_Pattern_Analysis::CGSPoints_Pattern_Analysis(void)
 {
-	CSG_Parameter	*pNode;
-
 	//-----------------------------------------------------
 	Set_Name		(_TL("Spatial Point Pattern Analysis"));
 
-	Set_Author		(SG_T("O.Conrad (c) 2010"));
+	Set_Author		("O.Conrad (c) 2010");
 
 	Set_Description(
 		_TL("Basic measures for spatial point patterns.")
 	);
 
 	//-----------------------------------------------------
-	Parameters.Add_Shapes(
-		NULL	, "POINTS"		, _TL("Points"),
+	Parameters.Add_Shapes("",
+		"POINTS"	, _TL("Points"),
 		_TL(""),
 		PARAMETER_INPUT, SHAPE_TYPE_Point
 	);
 
-	Parameters.Add_Shapes(
-		NULL	, "CENTRE"		, _TL("Mean Centre"),
+	Parameters.Add_Table_Field("POINTS",
+		"WEIGHT"	, _TL("Weight"),
+		_TL(""),
+		true
+	);
+
+	Parameters.Add_Shapes("",
+		"CENTRE"	, _TL("Mean Centre"),
 		_TL(""),
 		PARAMETER_OUTPUT, SHAPE_TYPE_Point
 	);
 
-	pNode	= Parameters.Add_Shapes(
-		NULL	, "STDDIST"		, _TL("Standard Distance"),
+	Parameters.Add_Shapes("",
+		"STDDIST"	, _TL("Standard Distance"),
 		_TL(""),
 		PARAMETER_OUTPUT, SHAPE_TYPE_Polygon
 	);
 
-	Parameters.Add_Value(
-		pNode	, "STEP"		, _TL("Vertex Distance [Degree]"),
+	Parameters.Add_Double("STDDIST",
+		"STEP"		, _TL("Vertex Distance [Degree]"),
 		_TL(""),
-		PARAMETER_TYPE_Double, 5.0, 0.1, true, 20.0, true
+		5.0, 0.1, true, 20.0, true
 	);
 
-	Parameters.Add_Shapes(
-		NULL	, "BBOX"		, _TL("Bounding Box"),
+	Parameters.Add_Shapes("",
+		"BBOX"		, _TL("Bounding Box"),
 		_TL(""),
 		PARAMETER_OUTPUT, SHAPE_TYPE_Polygon
 	);
@@ -116,21 +120,12 @@ CGSPoints_Pattern_Analysis::CGSPoints_Pattern_Analysis(void)
 
 ///////////////////////////////////////////////////////////
 //														 //
-//														 //
-//														 //
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
 bool CGSPoints_Pattern_Analysis::On_Execute(void)
 {
-	int						iPoint;
-	double					StdDist;
-	CSG_Shape				*pShape;
-	CSG_Shapes				*pPoints, *pShapes;
-	CSG_Simple_Statistics	X, Y, D;
-
-	//-----------------------------------------------------
-	pPoints		= Parameters("POINTS")	->asShapes();
+	CSG_Shapes	*pPoints	= Parameters("POINTS")->asShapes();
 
 	if( pPoints->Get_Count() <= 1 )
 	{
@@ -139,13 +134,19 @@ bool CGSPoints_Pattern_Analysis::On_Execute(void)
 		return( false );
 	}
 
+	int	iPoint, Weight	= Parameters("WEIGHT")->asInt();
+
+	CSG_Simple_Statistics	X, Y, D;
+
 	//-----------------------------------------------------
 	for(iPoint=0; iPoint<pPoints->Get_Count() && Set_Progress(iPoint, pPoints->Get_Count()); iPoint++)
 	{
 		TSG_Point	p	= pPoints->Get_Shape(iPoint)->Get_Point(0);
 
-		X.Add_Value(p.x);
-		Y.Add_Value(p.y);
+		double	w	= Weight < 0 ? 1.0 : pPoints->Get_Shape(iPoint)->asDouble(Weight);
+
+		X.Add_Value(p.x, w);
+		Y.Add_Value(p.y, w);
 	}
 
 	if( X.Get_Range() == 0.0 && Y.Get_Range() == 0.0 )
@@ -156,74 +157,85 @@ bool CGSPoints_Pattern_Analysis::On_Execute(void)
 	}
 
 	//-----------------------------------------------------
-	StdDist	= 0.0;
+	double	StdDist	= 0.0;
 
 	for(iPoint=0; iPoint<pPoints->Get_Count() && Set_Progress(iPoint, pPoints->Get_Count()); iPoint++)
 	{
 		TSG_Point	p	= pPoints->Get_Shape(iPoint)->Get_Point(0);
 
-		D.Add_Value(SG_Get_Distance(X.Get_Mean(), Y.Get_Mean(), p.x, p.y));
+		double	w	= Weight < 0 ? 1.0 : pPoints->Get_Shape(iPoint)->asDouble(Weight);
 
-		StdDist	+= SG_Get_Square(p.x - X.Get_Mean()) + SG_Get_Square(p.y - Y.Get_Mean());
+		D.Add_Value(SG_Get_Distance(X.Get_Mean(), Y.Get_Mean(), p.x, p.y), w);
+
+		StdDist	+= w * (SG_Get_Square(p.x - X.Get_Mean()) + SG_Get_Square(p.y - Y.Get_Mean()));
 	}
 
-	StdDist	= sqrt(StdDist / D.Get_Count());
+	if( D.Get_Weights() == 0.0 )
+	{
+		Error_Set(_TL("number of valid points or sum of weights equals zero"));
+
+		return( false );
+	}
+
+	StdDist	= sqrt(StdDist / D.Get_Weights());
 
 	//-----------------------------------------------------
-	pShapes	= Parameters("CENTRE")	->asShapes();
+	CSG_Shapes	*pShapes;	CSG_Shape	*pShape;
 
-	pShapes	->Create(SHAPE_TYPE_Point	, CSG_String::Format(SG_T("%s [%s]"), pPoints->Get_Name(), _TL("Centre")));
-	pShapes	->Add_Field(SG_T("X_CENTRE"), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("Y_CENTRE"), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("N_POINTS"), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("STDDIST" ), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("MEANDIST"), SG_DATATYPE_Double);
+	pShapes	= Parameters("CENTRE")->asShapes();
+
+	pShapes	->Create(SHAPE_TYPE_Point, CSG_String::Format("%s [%s]", pPoints->Get_Name(), _TL("Centre")));
+	pShapes	->Add_Field("X_CENTRE", SG_DATATYPE_Double);
+	pShapes	->Add_Field("Y_CENTRE", SG_DATATYPE_Double);
+	pShapes	->Add_Field("N_POINTS", SG_DATATYPE_Double);
+	pShapes	->Add_Field("STDDIST" , SG_DATATYPE_Double);
+	pShapes	->Add_Field("MEANDIST", SG_DATATYPE_Double);
 
 	pShape	= pShapes->Add_Shape();
 
-	pShape	->Set_Value(0, X.Get_Mean());
-	pShape	->Set_Value(1, Y.Get_Mean());
-	pShape	->Set_Value(2, D.Get_Count());
-	pShape	->Set_Value(3, StdDist);
+	pShape	->Set_Value(0, X.Get_Mean  ());
+	pShape	->Set_Value(1, Y.Get_Mean  ());
+	pShape	->Set_Value(2, D.Get_Count ());
+	pShape	->Set_Value(3, StdDist       );
 	pShape	->Set_Value(4, D.Get_StdDev());
 
 	pShape	->Add_Point(X.Get_Mean(), Y.Get_Mean());
 
 	//-----------------------------------------------------
-	pShapes	= Parameters("STDDIST")	->asShapes();
+	pShapes	= Parameters("STDDIST")->asShapes();
 
-	pShapes	->Create(SHAPE_TYPE_Polygon	, CSG_String::Format(SG_T("%s [%s]"), pPoints->Get_Name(), _TL("Standard Distance")));
-	pShapes	->Add_Field(SG_T("X_CENTRE"), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("Y_CENTRE"), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("N_POINTS"), SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("STDDIST") , SG_DATATYPE_Double);
+	pShapes	->Create(SHAPE_TYPE_Polygon, CSG_String::Format("%s [%s]", pPoints->Get_Name(), _TL("Standard Distance")));
+	pShapes	->Add_Field("X_CENTRE", SG_DATATYPE_Double);
+	pShapes	->Add_Field("Y_CENTRE", SG_DATATYPE_Double);
+	pShapes	->Add_Field("N_POINTS", SG_DATATYPE_Double);
+	pShapes	->Add_Field("STDDIST" , SG_DATATYPE_Double);
 
 	pShape	= pShapes->Add_Shape();
 
-	pShape	->Set_Value(0, X.Get_Mean());
-	pShape	->Set_Value(1, Y.Get_Mean());
+	pShape	->Set_Value(0, X.Get_Mean ());
+	pShape	->Set_Value(1, Y.Get_Mean ());
 	pShape	->Set_Value(2, X.Get_Count());
-	pShape	->Set_Value(3, StdDist);
+	pShape	->Set_Value(3, StdDist      );
 
 	double	dTheta	= Parameters("STEP")->asDouble() * M_DEG_TO_RAD;
 
 	for(double Theta=0.0; Theta<=M_PI_360; Theta+=dTheta)
 	{
-		pShape	->Add_Point(
+		pShape->Add_Point(
 			X.Get_Mean() + StdDist * cos(Theta),
 			Y.Get_Mean() + StdDist * sin(Theta)
 		);
 	}
 
 	//-----------------------------------------------------
-	pShapes	= Parameters("BBOX")	->asShapes();
+	pShapes	= Parameters("BBOX")->asShapes();
 
-	pShapes	->Create(SHAPE_TYPE_Polygon	, CSG_String::Format(SG_T("%s [%s]"), pPoints->Get_Name(), _TL("Bounding Box")));
+	pShapes	->Create(SHAPE_TYPE_Polygon, CSG_String::Format("%s [%s]", pPoints->Get_Name(), _TL("Bounding Box")));
 
-	pShapes	->Add_Field(SG_T("XMIN")	, SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("XMAX")	, SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("YMIN")	, SG_DATATYPE_Double);
-	pShapes	->Add_Field(SG_T("YMAX")	, SG_DATATYPE_Double);
+	pShapes	->Add_Field("XMIN", SG_DATATYPE_Double);
+	pShapes	->Add_Field("XMAX", SG_DATATYPE_Double);
+	pShapes	->Add_Field("YMIN", SG_DATATYPE_Double);
+	pShapes	->Add_Field("YMAX", SG_DATATYPE_Double);
 
 	pShape	= pShapes->Add_Shape();
 
@@ -237,6 +249,7 @@ bool CGSPoints_Pattern_Analysis::On_Execute(void)
 	pShape	->Add_Point(X.Get_Maximum(), Y.Get_Maximum());
 	pShape	->Add_Point(X.Get_Maximum(), Y.Get_Minimum());
 
+	//-----------------------------------------------------
 	return( true );
 }
 
