15b9ecca4143aef49eed9425fcc21d2da58fb010
oconrad <conrad@geowiss.uni-hamburg.de>
2017-12-21 11:55:02 +0100

    SAGA version updated to next aspired version 6.3.0

--- saga-gis/src/saga_core/saga_api/saga_api.h
+++ saga-gis/src/saga_core/saga_api/saga_api.h
@@ -127,9 +127,9 @@
 
 //---------------------------------------------------------
 #define SAGA_MAJOR_VERSION		6
-#define SAGA_MINOR_VERSION		2
+#define SAGA_MINOR_VERSION		3
 #define SAGA_RELEASE_NUMBER		0
-#define SAGA_VERSION			SG_T("6.2.0")
+#define SAGA_VERSION			SG_T("6.3.0")
 
 
 ///////////////////////////////////////////////////////////
