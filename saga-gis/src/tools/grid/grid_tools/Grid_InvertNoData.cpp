f7f0f5c2e3ee60e4b7fd425f0b2c8c87e076477b
oconrad <conrad@geowiss.uni-hamburg.de>
2018-10-23 13:13:27 +0200

    grid_tools, invert data/no-data: reworked, options added

--- saga-gis/src/tools/grid/grid_tools/Grid_InvertNoData.cpp
+++ saga-gis/src/tools/grid/grid_tools/Grid_InvertNoData.cpp
@@ -1,6 +1,3 @@
-/**********************************************************
- * Version $Id$
- *********************************************************/
 /*******************************************************************************
     InvertNoData.cpp
     Copyright (C) Victor Olaya
@@ -20,49 +17,113 @@
     Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301, USA
 *******************************************************************************/ 
 
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "Grid_InvertNoData.h"
 
-CInvertNoData::CInvertNoData(void){
 
-	Parameters.Set_Name(_TL("Invert Data/No-Data"));
-	Parameters.Set_Description(_TW("(c) 2005 Victor Olaya."));
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
-	Parameters.Add_Grid(NULL, 
-						"INPUT",
-						_TL("Grid"), 						
-						_TL(""), 
-						PARAMETER_INPUT);
+//---------------------------------------------------------
+CInvertNoData::CInvertNoData(void)
+{
+	Set_Name		(_TL("Invert Data/No-Data"));
+
+	Set_Author		("Victor Olaya (c) 2005");
+
+	Set_Description	(_TW(
+		"Converts valid data cells to no-data cells and no-data cells to "
+		"the user specified value. Mostly suitable when dealing with masks."
+	));
+
+	Parameters.Add_Grid("",
+		"INPUT" , _TL("Grid"),
+		_TL(""),
+		PARAMETER_INPUT
+	);
 	
-	Parameters.Add_Grid(NULL, 
-						"OUTPUT",
-						_TL("Result"), 						
-						_TL(""), 
-						PARAMETER_OUTPUT);
-}//constructor
+	Parameters.Add_Grid("", 
+		"OUTPUT", _TL("Result"),
+		_TL(""),
+		PARAMETER_OUTPUT_OPTIONAL
+	);
+
+	Parameters.Add_Double("",
+		"VALUE" , _TL("Data Value"),
+		_TL(""),
+		1.
+	);
+}
 
 
-CInvertNoData::~CInvertNoData(void)
-{}
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
 
-bool CInvertNoData::On_Execute(void){
-	
-	int x,y;
-	
-	CSG_Grid* pInput = Parameters("INPUT")->asGrid(); 
-	CSG_Grid* pOutput = Parameters("OUTPUT")->asGrid(); 
-	pOutput->Set_NoData_Value_Range(-9999,0);
-				
-	for(y=0; y<Get_NY() && Set_Progress(y); y++){		
-		for(x=0; x<Get_NX(); x++){
-			if (pInput->is_NoData(x,y)){
-				pOutput->Set_Value(x, y, 1.0);
-			}//if
-			else{
-				pOutput->Set_Value(x, y, -9999.0);
-			}//else
-		}//if
-	}//for
+//---------------------------------------------------------
+bool CInvertNoData::On_Execute(void)
+{
+	CSG_Grid	*pGrid	= Parameters("OUTPUT")->asGrid();
 
-	return true;
+	if( pGrid && pGrid != Parameters("INPUT")->asGrid() )
+	{
+		pGrid->Create(*Parameters("INPUT")->asGrid());
 
-}//method
+		pGrid->Set_Name("%s [%s]", Parameters("INPUT")->asGrid()->Get_Name(), _TL("inverse no-data"));
+	}
+	else
+	{
+		pGrid	= Parameters("INPUT")->asGrid();
+	}
+
+	//-----------------------------------------------------
+	double	Value	= Parameters("VALUE")->asDouble();
+
+	if( SG_IS_BETWEEN(pGrid->Get_NoData_Value(), Value, pGrid->Get_NoData_hiValue()) )
+	{
+		Message_Fmt("%s: %s", _TL("Warning"), _TL("targeted data value is a no-data value"));
+	}
+
+	//-----------------------------------------------------
+	for(int y=0; y<Get_NY() && Set_Progress(y); y++)
+	{
+		#pragma omp parallel for
+		for(int x=0; x<Get_NX(); x++)
+		{
+			if( pGrid->is_NoData(x, y) )
+			{
+				pGrid->Set_Value(x, y, Value);
+			}
+			else
+			{
+				pGrid->Set_NoData(x, y);
+			}
+		}
+	}
+
+	//-----------------------------------------------------
+	if( pGrid != Parameters("OUTPUT")->asGrid() )
+	{
+		DataObject_Update(pGrid);
+	}
+
+	return( true );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
