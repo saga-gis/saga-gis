2370daf58da34902e16ae57b4d7724d0df1143d1
oconrad <conrad@geowiss.uni-hamburg.de>
2019-01-30 13:23:04 +0100

    shapes_polygons/Polygon_Geometrics: centroid and extent options added

--- saga-gis/src/tools/shapes/shapes_polygons/Polygon_Geometrics.cpp
+++ saga-gis/src/tools/shapes/shapes_polygons/Polygon_Geometrics.cpp
@@ -42,49 +42,66 @@ CPolygon_Geometrics::CPolygon_Geometrics(void)
 {
 	Set_Name		(_TL("Polygon Properties"));
 
-	Set_Author		(_TL("V.Olaya (c) 2004, O.Conrad (c) 2011"));
+	Set_Author		("V.Olaya (c) 2004, O.Conrad (c) 2011");
 
 	Set_Description	(_TW(
-		"General and geometric properties of polygons."
+		"Add general and geometric properties of polygons to its atttributes."
 	));
 
-	Parameters.Add_Shapes(NULL,
+	Parameters.Add_Shapes("",
 		"POLYGONS"	, _TL("Polygons"),
 		_TL(""),
 		PARAMETER_INPUT, SHAPE_TYPE_Polygon
 	);
 
-	Parameters.Add_Shapes(NULL,
+	Parameters.Add_Shapes("",
 		"OUTPUT"	, _TL("Polygons with Property Attributes"),
 		_TL("If not set property attributes will be added to the orignal layer."),
 		PARAMETER_OUTPUT_OPTIONAL, SHAPE_TYPE_Polygon
 	);
 
-	Parameters.Add_Bool(NULL,
+	Parameters.Add_Table_Fields("POLYGONS",
+		"FIELDS"	, _TL("Copy Attributes"),
+		_TL("Select one or more attributes to be copied to the target layer.")
+	);
+
+	Parameters.Add_Bool("",
 		"BPARTS"	, _TL("Number of Parts"),
 		_TL(""),
 		false
 	);
 
-	Parameters.Add_Bool(NULL,
+	Parameters.Add_Bool("",
 		"BPOINTS"	, _TL("Number of Vertices"),
 		_TL(""),
 		false
 	);
 
-	Parameters.Add_Bool(NULL,
+	Parameters.Add_Bool("",
+		"BEXTENT"	, _TL("Extent"),
+		_TL(""),
+		false
+	);
+
+	Parameters.Add_Bool("",
+		"BCENTER"	, _TL("Centroid"),
+		_TL(""),
+		false
+	);
+
+	Parameters.Add_Bool("",
 		"BLENGTH"	, _TL("Perimeter"),
 		_TL(""),
 		true
 	);
 
-	Parameters.Add_Bool(NULL,
+	Parameters.Add_Bool("",
 		"BAREA"		, _TL("Area"),
 		_TL(""),
 		true
 	);
 
-	Parameters.Add_Double(NULL,
+	Parameters.Add_Double("",
 		"SCALING"	, _TL("Scaling"),
 		_TL("Scaling factor for perimeter and area (squared). meter to feet = 1 / 0.3048 = 3.2808"),
 		1.0, 0.0, true
@@ -97,15 +114,33 @@ CPolygon_Geometrics::CPolygon_Geometrics(void)
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
+int CPolygon_Geometrics::On_Parameters_Enable(CSG_Parameters *pParameters, CSG_Parameter *pParameter)
+{
+	if( pParameter->Cmp_Identifier("OUTPUT") )
+	{
+		pParameters->Set_Enabled("FIELDS", pParameter->asPointer() && pParameter->asPointer() != (*pParameters)("POLYGONS")->asPointer());
+	}
+
+	return( CSG_Tool::On_Parameters_Enable(pParameters, pParameter) );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 bool CPolygon_Geometrics::On_Execute(void)
 {
 	//-------------------------------------------------
-	int	bParts	= Parameters("BPARTS" )->asBool() ? 0 : -1;
-	int	bPoints	= Parameters("BPOINTS")->asBool() ? 0 : -1;
-	int	bLength	= Parameters("BLENGTH")->asBool() ? 0 : -1;
-	int	bArea	= Parameters("BAREA"  )->asBool() ? 0 : -1;
+	int	fParts	= Parameters("BPARTS" )->asBool() ? 0 : -1;
+	int	fPoints	= Parameters("BPOINTS")->asBool() ? 0 : -1;
+	int	fCenter	= Parameters("BCENTER")->asBool() ? 0 : -1;
+	int	fExtent	= Parameters("BEXTENT")->asBool() ? 0 : -1;
+	int	fLength	= Parameters("BLENGTH")->asBool() ? 0 : -1;
+	int	fArea	= Parameters("BAREA"  )->asBool() ? 0 : -1;
 
-	if( bParts && bPoints && bLength && bArea )
+	if( fParts && fPoints && fCenter && fExtent && fLength && fArea )
 	{
 		Error_Set(_TL("no properties selected"));
 
@@ -117,22 +152,52 @@ bool CPolygon_Geometrics::On_Execute(void)
 
 	if(	!pPolygons->is_Valid() || pPolygons->Get_Count() <= 0 )
 	{
-		Error_Set(_TL("invalid layer"));
+		Error_Set(_TL("invalid or empty polygons layer"));
 
 		return( false );
 	}
 
+	//-------------------------------------------------
 	if( Parameters("OUTPUT")->asShapes() && Parameters("OUTPUT")->asShapes() != pPolygons )
 	{
-		pPolygons	= Parameters("OUTPUT")->asShapes();
-		pPolygons->Create(*Parameters("POLYGONS")->asShapes());
+		CSG_Shapes *pCopies	= Parameters("OUTPUT")->asShapes();
+
+		pCopies->Create(SHAPE_TYPE_Polygon, pPolygons->Get_Name(), NULL, pPolygons->Get_Vertex_Type());
+
+		CSG_Parameter_Table_Fields	*pFields	= Parameters("FIELDS")->asTableFields();
+
+		for(int Field=0; Field<pFields->Get_Count(); Field++)
+		{
+			pCopies->Add_Field(
+				pPolygons->Get_Field_Name(pFields->Get_Index(Field)),
+				pPolygons->Get_Field_Type(pFields->Get_Index(Field))
+			);
+		}
+
+		for(int i=0; i<pPolygons->Get_Count() && Set_Progress(i, pPolygons->Get_Count()); i++)
+		{
+			CSG_Shape *pPolygon = pPolygons->Get_Shape(i); CSG_Shape *pCopy = pCopies->Add_Shape(pPolygon, SHAPE_COPY_GEOM);
+
+			for(int Field=0; Field<pFields->Get_Count(); Field++)
+			{
+				*pCopy->Get_Value(Field)	= *pPolygon->Get_Value(pFields->Get_Index(Field));
+			}
+		}
+
+		pPolygons	= pCopies;
 	}
 
 	//-------------------------------------------------
-	if( !bParts  )	{	bParts	= pPolygons->Get_Field_Count();	pPolygons->Add_Field("NPARTS"   , SG_DATATYPE_Int   );	}
-	if( !bPoints )	{	bPoints	= pPolygons->Get_Field_Count();	pPolygons->Add_Field("NPOINTS"  , SG_DATATYPE_Int   );	}
-	if( !bLength )	{	bLength	= pPolygons->Get_Field_Count();	pPolygons->Add_Field("PERIMETER", SG_DATATYPE_Double);	}
-	if( !bArea   )	{	bArea	= pPolygons->Get_Field_Count();	pPolygons->Add_Field("AREA"     , SG_DATATYPE_Double);	}
+	if( !fParts  ) { fParts	 = pPolygons->Get_Field_Count(); pPolygons->Add_Field("NPARTS"   , SG_DATATYPE_Int   ); }
+	if( !fPoints ) { fPoints = pPolygons->Get_Field_Count(); pPolygons->Add_Field("NPOINTS"  , SG_DATATYPE_Int   ); }
+	if( !fCenter ) { fCenter = pPolygons->Get_Field_Count(); pPolygons->Add_Field("CENTER_X" , SG_DATATYPE_Double);
+	                                                         pPolygons->Add_Field("CENTER_Y" , SG_DATATYPE_Double); }
+	if( !fExtent ) { fExtent = pPolygons->Get_Field_Count(); pPolygons->Add_Field("MIN_X"    , SG_DATATYPE_Double);
+	                                                         pPolygons->Add_Field("MAX_X"    , SG_DATATYPE_Double);
+															 pPolygons->Add_Field("MIN_Y"    , SG_DATATYPE_Double);
+															 pPolygons->Add_Field("MAX_Y"    , SG_DATATYPE_Double); }
+	if( !fLength ) { fLength = pPolygons->Get_Field_Count(); pPolygons->Add_Field("PERIMETER", SG_DATATYPE_Double); }
+	if( !fArea   ) { fArea   = pPolygons->Get_Field_Count(); pPolygons->Add_Field("AREA"     , SG_DATATYPE_Double); }
 
 	//-------------------------------------------------
 	double	Scaling	= Parameters("SCALING")->asDouble();
@@ -142,10 +207,26 @@ bool CPolygon_Geometrics::On_Execute(void)
 	{
 		CSG_Shape_Polygon	*pPolygon	= (CSG_Shape_Polygon *)pPolygons->Get_Shape(i);
 
-		if( bParts  >= 0 )	pPolygon->Set_Value(bParts , pPolygon->Get_Part_Count());
-		if( bPoints >= 0 )	pPolygon->Set_Value(bPoints, ((CSG_Shape *)pPolygon)->Get_Point_Count());
-		if( bLength >= 0 )	pPolygon->Set_Value(bLength, pPolygon->Get_Perimeter() * Scaling);
-		if( bArea   >= 0 )	pPolygon->Set_Value(bArea  , pPolygon->Get_Area() * Scaling*Scaling);
+		if( fParts  >= 0 ) { pPolygon->Set_Value(fParts , pPolygon->Get_Part_Count ()                  ); }
+		if( fPoints >= 0 ) { pPolygon->Set_Value(fPoints, pPolygon->Get_Point_Count()                  ); }
+		if( fLength >= 0 ) { pPolygon->Set_Value(fLength, pPolygon->Get_Perimeter  () * Scaling        ); }
+		if( fArea   >= 0 ) { pPolygon->Set_Value(fArea  , pPolygon->Get_Area       () * Scaling*Scaling); }
+
+		if( fCenter >= 0 )
+		{
+			TSG_Point	p	= pPolygon->Get_Centroid();
+
+			pPolygon->Set_Value(fCenter + 0, p.x);
+			pPolygon->Set_Value(fCenter + 1, p.y);
+		}
+
+		if( fExtent >= 0 )
+		{
+			pPolygon->Set_Value(fExtent + 0, pPolygon->Get_Extent().Get_XMin());
+			pPolygon->Set_Value(fExtent + 1, pPolygon->Get_Extent().Get_XMax());
+			pPolygon->Set_Value(fExtent + 2, pPolygon->Get_Extent().Get_YMin());
+			pPolygon->Set_Value(fExtent + 3, pPolygon->Get_Extent().Get_YMax());
+		}
 	}
 
 	//-------------------------------------------------
--- saga-gis/src/tools/shapes/shapes_polygons/add_point_attributes.cpp
+++ saga-gis/src/tools/shapes/shapes_polygons/add_point_attributes.cpp
@@ -70,7 +70,7 @@ CAdd_Point_Attributes::CAdd_Point_Attributes(void)
 {
 	Set_Name		(_TL("Add Point Attributes to Polygons"));
 
-	Set_Author		(SG_T("V. Wichmann (c) 2014"));
+	Set_Author		("V. Wichmann (c) 2014");
 
 	Set_Description	(_TW(
 		"Spatial join for polygons. Retrieves for each polygon the selected "
@@ -152,7 +152,7 @@ bool CAdd_Point_Attributes::On_Execute(void)
 
 		for(int iField=0; iField<pPoints->Get_Field_Count(); iField++)
 		{
-			sFields += CSG_String::Format(SG_T("%d,"), iField);
+			sFields += CSG_String::Format("%d,", iField);
 		}
 
 		pFields->Set_Value(sFields);
@@ -185,18 +185,18 @@ bool CAdd_Point_Attributes::On_Execute(void)
 
 	if( bAddLocInfo )
 	{
-		pOutput->Add_Field(SG_T("X"), SG_DATATYPE_Float);
+		pOutput->Add_Field("X", SG_DATATYPE_Float);
 		iXField = pOutput->Get_Field_Count() - 1;
 
-		pOutput->Add_Field(SG_T("Y"), SG_DATATYPE_Float);
+		pOutput->Add_Field("Y", SG_DATATYPE_Float);
 
 		if( pPoints->Get_Vertex_Type() != SG_VERTEX_TYPE_XY )
 		{
-			pOutput->Add_Field(SG_T("Z"), SG_DATATYPE_Float);
+			pOutput->Add_Field("Z", SG_DATATYPE_Float);
 
 			if( pPoints->Get_Vertex_Type() == SG_VERTEX_TYPE_XYZM )
 			{
-				pOutput->Add_Field(SG_T("M"), SG_DATATYPE_Float);
+				pOutput->Add_Field("M", SG_DATATYPE_Float);
 			}
 		}
 	}
