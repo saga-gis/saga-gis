02fe2b728dd18bc2e157b03a323320b6865fd5c5
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2021-06-25 18:58:04 +0200

    cmake build system: slight adjustments

--- saga-gis/cmake/modules/FindSVM.cmake
+++ saga-gis/cmake/modules/FindSVM.cmake
@@ -11,6 +11,6 @@ find_path   (SVM_INCLUDE NAMES libsvm/svm.h DOC "libsvm include directory")
 find_library(SVM_LIBRARY NAMES svm          DOC "libsvm library")
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(svm REQUIRED_VARS SVM_LIBRARY SVM_INCLUDE)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SVM REQUIRED_VARS SVM_LIBRARY SVM_INCLUDE)
 
 mark_as_advanced(SVM_INCLUDE SVM_LIBRARY)
