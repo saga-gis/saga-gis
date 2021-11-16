d56baf332383acc814d8b61927fc8f22e639c7a7
Justus Spitzmüller <jspitzmueller@scilands.de>
2021-11-16 18:28:05 +0100

    Added the GPL Licence Header

--- saga-gis/src/tools/shapes/shapes_lines/cut_lines.cpp
+++ saga-gis/src/tools/shapes/shapes_lines/cut_lines.cpp
@@ -1,10 +1,58 @@
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                     Tool Library                      //
+//                     shapes_lines                      //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//                     cut_lines.cpp                     //
+//                                                       //
+//                 Copyright (C) 2021 by                 //
+//                   Justus Spitzmüller                  //
+//                     scilands GmbH                     //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation, either version 2 of the     //
+// License, or (at your option) any later version.       //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not, see   //
+// <http://www.gnu.org/licenses/>.                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     jspitzmueller@scilands.de              //
+//                                                       //
+//    contact:    Justus Spitzmüller                     //
+//                scilands GmbH                          //
+//                Goethe-Allee 11                        //
+//                37073 Göttingen                        //
+//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
 #include "cut_lines.h"
 
 CCut_Lines::CCut_Lines(void)
 {
 	Set_Name(_TL("Cut Lines"));
 
-	Set_Author(_TL("\u00a9 scilands GmbH 2021, Justus Spitzmüller"));
+	Set_Author(_TL("Justus Spitzmüller \u00a9 scilands GmbH 2021"));
 
 	Set_Description (_TW(
 		"Tool cuts lines in a regular distance and assigns shape- and segment-numbers along with an ongoing identifier."
--- saga-gis/src/tools/shapes/shapes_lines/cut_lines.h
+++ saga-gis/src/tools/shapes/shapes_lines/cut_lines.h
@@ -1,3 +1,50 @@
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                     Tool Library                      //
+//                     shapes_lines                      //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//                      cut_lines.h                      //
+//                                                       //
+//                 Copyright (C) 2021 by                 //
+//                   Justus Spitzmüller                  //
+//                     scilands GmbH                     //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation, either version 2 of the     //
+// License, or (at your option) any later version.       //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not, see   //
+// <http://www.gnu.org/licenses/>.                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     jspitzmueller@scilands.de              //
+//                                                       //
+//    contact:    Justus Spitzmüller                     //
+//                scilands GmbH                          //
+//                Goethe-Allee 11                        //
+//                37073 Göttingen                        //
+//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
 #pragma once
 
 #include <saga_api/saga_api.h>
