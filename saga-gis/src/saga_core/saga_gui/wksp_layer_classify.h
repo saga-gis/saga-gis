d4cd7c08819de1f9a0b6384a2e9137e896602f41
Olaf Conrad <olaf.conrad@uni-hamburg.de>
2022-02-11 18:40:21 +0100

    saga_gui, wksp_layer_legend: graduated colors legend

--- saga-gis/src/saga_core/saga_gui/wksp_layer_classify.h
+++ saga-gis/src/saga_core/saga_gui/wksp_layer_classify.h
@@ -247,7 +247,7 @@ public: ///////////////////////////////////////////////////
 	//-----------------------------------------------------
 	bool						Set_Class_Count			(int Count);
 
-	int							Get_Class_Count			(void)
+	int							Get_Class_Count			(void)	const
 	{
 		switch( m_Mode )
 		{
@@ -255,14 +255,14 @@ public: ///////////////////////////////////////////////////
 			return( 1 );
 
 		case CLASSIFY_LUT      :
-			return( m_pLUT->Get_Record_Count() );
+			return( m_pLUT->Get_Count() );
 
 		case CLASSIFY_GRADUATED:
 		case CLASSIFY_SHADE    :
 		case CLASSIFY_OVERLAY  :
 			return( m_Count );
 
-		case CLASSIFY_DISCRETE   :
+		case CLASSIFY_DISCRETE :
 			return( m_pColors->Get_Count() );
 		}
 	}
@@ -279,7 +279,7 @@ public: ///////////////////////////////////////////////////
 			return( _LUT_Get_Class(Value) );
 
 		case CLASSIFY_GRADUATED:
-		case CLASSIFY_DISCRETE   :
+		case CLASSIFY_DISCRETE :
 		case CLASSIFY_SHADE    :
 		case CLASSIFY_OVERLAY  :
 			return( _METRIC_Get_Class(Value) );
@@ -308,7 +308,10 @@ public: ///////////////////////////////////////////////////
 	///////////////////////////////////////////////////////
 
 	//-----------------------------------------------------
-	bool						Get_Class_Color			(int iClass, int &Color)
+	CSG_Colors					Get_Class_Colors		(void)	const;
+
+	//-----------------------------------------------------
+	bool						Get_Class_Color			(int iClass, int &Color)	const
 	{
 		switch( m_Mode )
 		{
@@ -317,7 +320,7 @@ public: ///////////////////////////////////////////////////
 			break;
 
 		case CLASSIFY_LUT      :
-			if( iClass < 0 || iClass >= m_pLUT->Get_Record_Count() )
+			if( iClass < 0 || iClass >= m_pLUT->Get_Count() )
 			{
 				Color	= m_UNI_Color;
 
@@ -328,7 +331,7 @@ public: ///////////////////////////////////////////////////
 			break;
 
 
-		case CLASSIFY_DISCRETE   :
+		case CLASSIFY_DISCRETE :
 			Color	= m_pColors->Get_Color(iClass < 0 ? 0 : iClass >= m_pColors->Get_Count() ? m_pColors->Get_Count() - 1 : iClass);
 			break;
 
@@ -343,7 +346,7 @@ public: ///////////////////////////////////////////////////
 	}
 
 	//-----------------------------------------------------
-	int							Get_Class_Color			(int iClass)
+	int							Get_Class_Color			(int iClass)	const
 	{
 		int		Color;
 
@@ -351,7 +354,7 @@ public: ///////////////////////////////////////////////////
 	}
 
 	//-----------------------------------------------------
-	bool						Get_Class_Color_byValue	(double Value, int &Color)
+	bool						Get_Class_Color_byValue	(double Value, int &Color)	const
 	{
 		switch( m_Mode )
 		{
@@ -365,7 +368,7 @@ public: ///////////////////////////////////////////////////
 				return( Get_Class_Color(_LUT_Get_Class(Value), Color) );
 			}
 
-		case CLASSIFY_DISCRETE   :
+		case CLASSIFY_DISCRETE :
 			{
 				return( Get_Class_Color(_METRIC_Get_Class(Value), Color) );
 			}
@@ -470,17 +473,17 @@ public: ///////////////////////////////////////////////////
 
 	//-----------------------------------------------------
 	void						Set_Metric				(int Mode, double LogFactor, double zMin, double zMax);
-	int							Get_Metric_Mode			(void)		{	return( m_zMode );		}
-	CSG_Colors *				Get_Metric_Colors		(void)		{	return( m_pColors );	}
+	int							Get_Metric_Mode			(void)	const	{	return( m_zMode           );	}
+	CSG_Colors *				Get_Metric_Colors		(void)	const	{	return( m_pColors         );	}
 	double						Get_Metric_Minimum		(void)	const	{	return( m_zMin            );	}
 	double						Get_Metric_Maximum		(void)	const	{	return( m_zMin + m_zRange );	}
 	double						Get_Metric_Range		(void)	const	{	return(          m_zRange );	}
-	double						Get_Metric_LogFactor	(void)	const	{	return( m_zLogRange );	}
+	double						Get_Metric_LogFactor	(void)	const	{	return( m_zLogRange       );	}
 
 	void						Metric2EqualElements	(void);
 
 	//-----------------------------------------------------
-	double						Get_MetricToRelative	(double Value)
+	double						Get_MetricToRelative	(double Value)	const
 	{
 		if( m_zRange > 0.0 )
 		{
@@ -503,7 +506,7 @@ public: ///////////////////////////////////////////////////
 	}
 
 	//-----------------------------------------------------
-	double						Get_RelativeToMetric	(double Value)
+	double						Get_RelativeToMetric	(double Value)	const
 	{
 		switch( m_zMode )
 		{
@@ -548,13 +551,13 @@ protected: ////////////////////////////////////////////////
 
 
 	//-----------------------------------------------------
-	int							_LUT_Cmp_Class			(double Value, int iClass);
-	int							_LUT_Get_Class			(double Value);
+	int							_LUT_Cmp_Class			(double            Value, int iClass)	const;
+	int							_LUT_Get_Class			(double            Value            )	const;
 
-	int							_LUT_Cmp_Class			(const CSG_String &Value, int iClass);
-	int							_LUT_Get_Class			(const CSG_String &Value);
+	int							_LUT_Cmp_Class			(const CSG_String &Value, int iClass)	const;
+	int							_LUT_Get_Class			(const CSG_String &Value            )	const;
 
-	int							_METRIC_Get_Class		(double Value)
+	int							_METRIC_Get_Class		(double Value)	const
 	{
 		if( Value < m_zMin )
 		{
