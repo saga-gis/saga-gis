24d90fbd0e41af0d7f1c4ab40a8498d27ebb8e55
reklov_w <wichmann@laserdata.at>
2022-03-11 10:04:54 +0100

    Profile from Points: complete rework of the tool fixing bugs (location of first point was not sampled) and adding new features (possibility to sample additional grids, output of sampling locations, and distance calculations along the line segment(s) instead of calculating grid cell center distances)

--- saga-gis/src/tools/terrain_analysis/ta_profiles/Grid_ProfileFromPoints.cpp
+++ saga-gis/src/tools/terrain_analysis/ta_profiles/Grid_ProfileFromPoints.cpp
@@ -1,163 +1,283 @@
-/**********************************************************
- * Version $Id: Grid_ProfileFromPoints.cpp 911 2011-02-14 16:38:15Z reklov_w $
- *********************************************************/
-/*******************************************************************************
-    ProfileFromPoints.cpp
-    Copyright (C) Victor Olaya
-    
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                     Tool Library                      //
+//                     ta_profiles                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//              Grid_ProfileFromPoints.cpp               //
+//                                                       //
+//              Copyright (C) 2004 - 2022 by             //
+//            Victor Olaya and Volker Wichmann           //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation, either version 2 of the     //
+// License, or (at your option) any later version.       //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not, see   //
+// <http://www.gnu.org/licenses/>.                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     wichmann@laserdata                     //
+//                                                       //
+//    contact:    Volker Wichmann                        //
+//                LASERDATA GmbH                         //
+//                Management and analysis of             //
+//                laserscanning data                     //
+//                Innsbruck, Austria                     //
+//                                                       //
+///////////////////////////////////////////////////////////
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+//---------------------------------------------------------
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301, USA
-*******************************************************************************/ 
 
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include "Grid_ProfileFromPoints.h"
 
-CProfileFromPoints::CProfileFromPoints(void){
 
-	CSG_Parameter	*pNode_0, *pNode_1;
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
-	Parameters.Set_Name(_TL("Profile from points"));
-	Parameters.Set_Description(_TW("(c) 2004 by Victor Olaya. Creates a profile from coordinates stored in a table"));
+//---------------------------------------------------------
+CProfileFromPoints::CProfileFromPoints(void)
+{
+	Set_Name        (_TL("Profile from Points"));
 
-	Parameters.Add_Grid(NULL, 
-						"GRID",
-						_TL("Grid"), 						
-						_TL(""), 
-						PARAMETER_INPUT);
+    Set_Author      (SG_T("V.Olaya, V. Wichmann (c) 2004-2022"));
 
-	pNode_0 = Parameters.Add_Table(NULL,
-								"TABLE", 
-								_TL("Input"),
-								_TL(""),	
-								PARAMETER_INPUT);
+	Set_Description (_TW("The tool allows one to query a profile from an input grid "
+                         "(usually a DEM) for point coordinates stored in a table "
+                         "or shapefile. The profile is traced from one point to "
+                         "the next, sampling the grid values along each line segment. "
+                         "Optionally, additional grids can be queried "
+                         "whose values are added to the profile table.\n\n"));
+
+    Parameters.Add_Grid("", 
+        "GRID"  , _TL("Grid"), 						
+		_TL("The input grid to query."), 
+		PARAMETER_INPUT
+    );
+
+    Parameters.Add_Grid_List("",
+        "VALUES"    , _TL("Values"),
+        _TL("Additional grids whose values shall be saved to the output table."),
+        PARAMETER_INPUT_OPTIONAL
+    );
+
+	Parameters.Add_Table("",
+        "TABLE" ,   _TL("Input Table"),
+        _TL("The input table with the point coordinates to query."),	
+        PARAMETER_INPUT
+    );
 	
-	pNode_1	= Parameters.Add_Table_Field(pNode_0,
-									"X",
-									_TL("X"),
-									_TL(""));
+	Parameters.Add_Table_Field("TABLE",
+        "X" , _TL("X"),
+		_TL("The attribute field with the x-coordinate.")
+    );
 
-	pNode_1	= Parameters.Add_Table_Field(pNode_0,
-									"Y",
-									_TL("Y"),
-									_TL(""));
+    Parameters.Add_Table_Field("TABLE",
+        "Y" , _TL("Y"),
+        _TL("The attribute field with the y-coordinate.")
+    );
 
-	Parameters.Add_Table(NULL, 
-						"RESULT", 
-						_TL("Result"), 
-						_TL(""), 
-						PARAMETER_OUTPUT);
+	Parameters.Add_Table("", 
+        "RESULT"    , _TL("Result"), 
+        _TL("The output table with the queried profile values."), 
+		PARAMETER_OUTPUT
+    );
+}
 
-}//constructor
 
-
+//---------------------------------------------------------
 CProfileFromPoints::~CProfileFromPoints(void)
 {}
 
-bool CProfileFromPoints::On_Execute(void){
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CProfileFromPoints::On_Execute(void)
+{
+    CSG_Table   *pTable;
+    int         iXField, iYField;
+
+	m_pGrid     = Parameters("GRID")->asGrid();
+    m_pValues   = Parameters("VALUES")->asGridList();
+	pTable      = Parameters("TABLE")->asTable();
+	m_pProfile  = Parameters("RESULT")->asTable();
+	iXField     = Parameters("X")->asInt();
+	iYField     = Parameters("Y")->asInt();	
 	
-	CSG_Table* pTable;	
-	CSG_Table* pProfileTable;
-	CSG_Table_Record* pRecord;
-	CSG_Grid* pGrid;	
-	int iXField, iYField;	
-	int i;
-	int x1,x2,y1,y2;
-	float fPartialDist;
-	float fDist = 0;
+
+    //---------------------------------------------------------
+    m_pProfile->Destroy();
+    m_pProfile->Set_Name(CSG_String::Format(SG_T("%s [%s]"), m_pGrid->Get_Name(), _TL("Profile")));
+
+    m_pProfile->Add_Field("ID"       , SG_DATATYPE_Int);
+    m_pProfile->Add_Field("DIST"     , SG_DATATYPE_Double);
+    m_pProfile->Add_Field("DIST_SURF", SG_DATATYPE_Double);
+    m_pProfile->Add_Field("X"        , SG_DATATYPE_Double);
+    m_pProfile->Add_Field("Y"        , SG_DATATYPE_Double);
+    m_pProfile->Add_Field("Z"        , SG_DATATYPE_Double);
+
+    for(int i=0; i<m_pValues->Get_Grid_Count(); i++)
+    {
+        m_pProfile->Add_Field(m_pValues->Get_Grid(i)->Get_Name(), SG_DATATYPE_Double);
+    }
 
 
-	pGrid = Parameters("GRID")->asGrid();
-	pTable = Parameters("TABLE")->asTable();
-	pProfileTable = Parameters("RESULT")->asTable();
-	iXField = Parameters("X")->asInt();
-	iYField = Parameters("Y")->asInt();	
-	
-	pProfileTable->Create((CSG_Table*)NULL);
-	pProfileTable->Set_Name(_TL("Profile"));
-	pProfileTable->Add_Field(_TL("Distance"), SG_DATATYPE_Double);
-	pProfileTable->Add_Field("Z", SG_DATATYPE_Double);
+    //---------------------------------------------------------
+	for(int i=1; i<pTable->Get_Record_Count(); i++)
+    {
+        TSG_Point   A, B;
 
-	for (i = 0; i < pTable->Get_Record_Count()-1; i++){
-		
-		x1=(int)(0.5 + (pTable->Get_Record(i  )->asDouble(iXField) - pGrid->Get_XMin()) / pGrid->Get_Cellsize());
-		x2=(int)(0.5 + (pTable->Get_Record(i+1)->asDouble(iXField) - pGrid->Get_XMin()) / pGrid->Get_Cellsize());
-		y1=(int)(0.5 + (pTable->Get_Record(i  )->asDouble(iYField) - pGrid->Get_YMin()) / pGrid->Get_Cellsize());			
-		y2=(int)(0.5 + (pTable->Get_Record(i+1)->asDouble(iYField) - pGrid->Get_YMin()) / pGrid->Get_Cellsize());			
+        A.x = pTable->Get_Record(i - 1)->asDouble(iXField);
+        A.y = pTable->Get_Record(i - 1)->asDouble(iYField);
+        B.x = pTable->Get_Record(i    )->asDouble(iXField);
+        B.y = pTable->Get_Record(i    )->asDouble(iYField);
 
-        int x = x1, y = y1, D = 0, HX = x2 - x1, HY = y2 - y1,
-                c, M, xInc = 1, yInc = 1, iLastX = x1, iLastY = y1;
-
-        if (HX < 0) {
-            xInc = -1;
-            HX = -HX;
-        }//if
-        if (HY < 0) {
-            yInc = -1;
-            HY = -HY;
-        }//if
-        if (HY <= HX) {
-            c = 2 * HX;
-            M = 2 * HY;
-            for (;;) {                
-                fPartialDist = (float)(M_GET_LENGTH(x-iLastX, y-iLastY) * pGrid->Get_Cellsize());
-                if (pGrid->is_InGrid(x,y) && fPartialDist){
-					fDist+=fPartialDist;
-                	pRecord = pProfileTable->Add_Record();
-					pRecord->Set_Value(0, fDist);
-					pRecord->Set_Value(1, pGrid->asFloat(x,y));
-                }//if
-                iLastX = x;
-                iLastY = y;
-                if (x == x2) {
-                    break;
-                }// if
-                x += xInc;
-                D += M;
-                if (D > HX) {
-                    y += yInc;
-                    D -= c;
-                }// if
-            }// for
-        }// if
-        else {
-            c = 2 * HY;
-            M = 2 * HX;
-            for (;;) {
-                fPartialDist = (float)(M_GET_LENGTH(x-iLastX, y-iLastY) * pGrid->Get_Cellsize());
-                if (pGrid->is_InGrid(x,y) && fPartialDist){
-					fDist+=fPartialDist;
-                	pRecord = pProfileTable->Add_Record();
-					pRecord->Set_Value(0, fDist);
-					pRecord->Set_Value(1, pGrid->asFloat(x,y));
-                }//if
-                iLastX = x;
-                iLastY = y;
-                if (y == y2) {
-                    break;
-                }// if
-                y += yInc;
-                D += M;
-                if (D > HY) {
-                    x += xInc;
-                    D -= c;
-                }// if
-            }// for
-        }// else        
-     
-	}//for
-
-	return true;
-
-}// method
+        Set_Profile(A, B, i == (pTable->Get_Record_Count() - 1));
+	}
 
 
+    //---------------------------------------------------------
+	return( true );
+}
+
+
+//---------------------------------------------------------
+bool CProfileFromPoints::Set_Profile(const TSG_Point &A, const TSG_Point &B, bool bLastPoint)
+{
+    double      dx, dy, d, n;
+    TSG_Point   p;
+
+    //-----------------------------------------------------
+    dx  = fabs(B.x - A.x);
+    dy  = fabs(B.y - A.y);
+
+    if( dx > 0.0 || dy > 0.0 )
+    {
+        if( dx > dy )
+        {
+            dx  /= Get_Cellsize();
+            n    = dx;
+            dy  /= dx;
+            dx   = Get_Cellsize();
+        }
+        else
+        {
+            dy  /= Get_Cellsize();
+            n    = dy;
+            dx  /= dy;
+            dy   = Get_Cellsize();
+        }
+
+        if( B.x < A.x )
+        {
+            dx  = -dx;
+        }
+
+        if( B.y < A.y )
+        {
+            dy  = -dy;
+        }
+
+        //-------------------------------------------------
+        for(d=0.0, p.x=A.x, p.y=A.y; d<=n; d++, p.x+=dx, p.y+=dy)
+        {
+            Add_Point(p);
+        }
+
+        if( bLastPoint && SG_Get_Distance(p, B) > M_ALMOST_ZERO )
+        {
+            Add_Point(B);
+        }
+    }
+
+    //-----------------------------------------------------
+    return( true );
+}
+
+
+//---------------------------------------------------------
+bool CProfileFromPoints::Add_Point(const TSG_Point &Point)
+{
+    int			        x, y, i;
+    double		        z, Distance, Distance_2;
+    CSG_Table_Record    *pPoint, *pLast;
+
+    if( Get_System().Get_World_to_Grid(x, y, Point) && m_pGrid->is_InGrid(x, y) )
+    {
+        z	= m_pGrid->asDouble(x, y);
+
+        if( m_pProfile->Get_Count() == 0 )
+        {
+            Distance	= 0.0;
+            Distance_2	= 0.0;
+        }
+        else
+        {
+            pLast		= m_pProfile->Get_Record(m_pProfile->Get_Count() - 1);
+            Distance	= SG_Get_Distance(Point.x, Point.y, pLast->asDouble(F_X), pLast->asDouble(F_Y));
+
+            if( Distance == 0.0 )
+            {
+                return( false );
+            }
+
+            Distance_2	= pLast->asDouble(F_Z) - z;
+            Distance_2	= sqrt(Distance*Distance + Distance_2*Distance_2);
+
+            Distance	+= pLast->asDouble(F_DIST);
+            Distance_2	+= pLast->asDouble(F_DIST_SURF);
+        }
+
+        pPoint	= m_pProfile->Add_Record();
+
+        pPoint->Set_Value(F_ID       , m_pProfile->Get_Count());
+        pPoint->Set_Value(F_DIST     , Distance);
+        pPoint->Set_Value(F_DIST_SURF, Distance_2);
+        pPoint->Set_Value(F_X        , Point.x);
+        pPoint->Set_Value(F_Y        , Point.y);
+        pPoint->Set_Value(F_Z        , z);
+
+        for(i=0; i<m_pValues->Get_Grid_Count(); i++)
+        {
+            pPoint->Set_Value(F_VALUES + i, m_pValues->Get_Grid(i)->asDouble(x, y));
+        }
+
+        return( true );
+    }
+
+    return( false );
+}
--- saga-gis/src/tools/terrain_analysis/ta_profiles/Grid_ProfileFromPoints.h
+++ saga-gis/src/tools/terrain_analysis/ta_profiles/Grid_ProfileFromPoints.h
@@ -1,38 +1,110 @@
-/**********************************************************
- * Version $Id: Grid_ProfileFromPoints.h 911 2011-02-14 16:38:15Z reklov_w $
- *********************************************************/
-/*******************************************************************************
-    ProfileFromPoints.cpp
-    Copyright (C) Victor Olaya
-    
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                     Tool Library                      //
+//                     ta_profiles                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//               Grid_ProfileFromPoints.h                //
+//                                                       //
+//              Copyright (C) 2004 - 2022 by             //
+//            Victor Olaya and Volker Wichmann           //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation, either version 2 of the     //
+// License, or (at your option) any later version.       //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not, see   //
+// <http://www.gnu.org/licenses/>.                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     wichmann@laserdata                     //
+//                                                       //
+//    contact:    Volker Wichmann                        //
+//                LASERDATA GmbH                         //
+//                Management and analysis of             //
+//                laserscanning data                     //
+//                Innsbruck, Austria                     //
+//                                                       //
+///////////////////////////////////////////////////////////
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+//---------------------------------------------------------
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301, USA
-*******************************************************************************/ 
 
-#if _MSC_VER > 1000
-#pragma once
-#endif // _MSC_VER > 1000
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
+//---------------------------------------------------------
+#ifndef HEADER_INCLUDED__Grid_Profile_From_Points_H
+#define HEADER_INCLUDED__Grid_Profile_From_Points_H
+
+//---------------------------------------------------------
 #include "MLB_Interface.h"
 
-class CProfileFromPoints : public CSG_Tool_Grid {
 
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+class CProfileFromPoints : public CSG_Tool_Grid
+{
 public:
 	CProfileFromPoints(void);
 	virtual ~CProfileFromPoints(void);
 
 protected:
-	virtual bool On_Execute(void);
+	virtual bool    On_Execute(void);
 
+private:
+
+    enum
+    {
+        F_ID	= 0,
+        F_DIST,
+        F_DIST_SURF,
+        F_X,
+        F_Y,
+        F_Z,
+        F_VALUES
+    };
+
+    CSG_Grid                *m_pGrid;
+    CSG_Parameter_Grid_List *m_pValues;
+    CSG_Table               *m_pProfile;
+
+    bool    Set_Profile(const TSG_Point &A, const TSG_Point &B, bool bLastPoint);
+    bool    Add_Point(const TSG_Point &Point);
 };
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#endif // #ifndef HEADER_INCLUDED__Grid_Profile_From_Points_H
