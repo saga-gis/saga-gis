9065c759893bfe286aea874495856dd97bf4711f
reklov_w <wichmann@laserdata.at>
2022-04-22 17:10:35 +0200

    saga_api, io_grid: enable LZW compression when grids are written as GeoTIFF file

--- saga-gis/src/saga_core/saga_api/grid_io.cpp
+++ saga-gis/src/saga_core/saga_api/grid_io.cpp
@@ -174,8 +174,9 @@ bool CSG_Grid::Save(const CSG_String &FileName, int Format)
 
 	case GRID_FILE_FORMAT_GeoTIFF:
 		SG_RUN_TOOL(bResult, "io_gdal", 2,	// Export GeoTIFF
-			    SG_TOOL_PARAMLIST_ADD("GRIDS", this)
-			&&	SG_TOOL_PARAMETER_SET("FILE" , FileName)
+			    SG_TOOL_PARAMLIST_ADD("GRIDS"   , this)
+			&&	SG_TOOL_PARAMETER_SET("FILE"    , FileName)
+            &&	SG_TOOL_PARAMETER_SET("OPTIONS" , SG_T("COMPRESS=LZW"))
 		);
 		break;
 	}
