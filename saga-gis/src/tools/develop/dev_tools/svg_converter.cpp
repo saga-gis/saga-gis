742fa54787f55d05e626b596ed3679c229faf170
oconrad <olaf.conrad@uni-hamburg.de>
2022-07-18 16:12:33 +0200

    dev_tools, tool added: svg <-> header conversion

---
+++ saga-gis/src/tools/develop/dev_tools/svg_converter.cpp
@@ -0,0 +1,185 @@
+
+///////////////////////////////////////////////////////////
+//                                                       //
+//                         SAGA                          //
+//                                                       //
+//      System for Automated Geoscientific Analyses      //
+//                                                       //
+//                     Tool Library                      //
+//                      dev_tools                        //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//                  svg_converter.cpp                    //
+//                                                       //
+//                 Copyright (C) 2022 by                 //
+//                     Olaf Conrad                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+// This file is part of 'SAGA - System for Automated     //
+// Geoscientific Analyses'. SAGA is free software; you   //
+// can redistribute it and/or modify it under the terms  //
+// of the GNU General Public License as published by the //
+// Free Software Foundation, either version 2 of the     //
+// License, or (at your option) any later version.       //
+//                                                       //
+// SAGA is distributed in the hope that it will be       //
+// useful, but WITHOUT ANY WARRANTY; without even the    //
+// implied warranty of MERCHANTABILITY or FITNESS FOR A  //
+// PARTICULAR PURPOSE. See the GNU General Public        //
+// License for more details.                             //
+//                                                       //
+// You should have received a copy of the GNU General    //
+// Public License along with this program; if not, see   //
+// <http://www.gnu.org/licenses/>.                       //
+//                                                       //
+//-------------------------------------------------------//
+//                                                       //
+//    e-mail:     oconrad@saga-gis.org                   //
+//                                                       //
+//    contact:    Olaf Conrad                            //
+//                Institute for Geography                //
+//                University of Hamburg                  //
+//                Germany                                //
+//                                                       //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+#include "svg_converter.h"
+
+//---------------------------------------------------------
+#include <wx/dir.h>
+#include <wx/filename.h>
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+CSVG_Converter::CSVG_Converter(void)
+{
+	Set_Name		("SVG Converter");
+
+	Set_Author		("O. Conrad (c) 2022");
+
+	Set_Description	(
+		""
+	);
+
+	//-----------------------------------------------------
+	CSG_String Headers;
+
+	#ifdef _SAGA_MSW
+	if( SG_Get_Environment("SAGA", &Headers) )
+	{
+		Headers += "\\src\\saga_core\\saga_gui\\res\\svg";
+	}
+	#endif
+
+	Parameters.Add_Choice  ("", "DIRECTION" , "Direction"       , "", "SVG to Header|Header to SVG");
+
+	Parameters.Add_FilePath("", "SVG_FILES" , "SVG Files"       , "", NULL,    NULL, false, false, true);
+	Parameters.Add_FilePath("", "HDR_OUTPUT", "Output Directory", "", NULL, Headers,  true,  true      );
+
+	Parameters.Add_FilePath("", "HDR_FILES" , "Header Files"    , "", NULL, Headers, false, false, true);
+	Parameters.Add_FilePath("", "SVG_OUTPUT", "Output Directory", "", NULL,    NULL,  true,  true      );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+int CSVG_Converter::On_Parameters_Enable(CSG_Parameters *pParameters, CSG_Parameter *pParameter)
+{
+	if( pParameter->Cmp_Identifier("DIRECTION") )
+	{
+		pParameters->Set_Enabled("SVG_FILES" , pParameter->asInt() == 0);
+		pParameters->Set_Enabled("HDR_OUTPUT", pParameter->asInt() == 0);
+
+		pParameters->Set_Enabled("HDR_FILES" , pParameter->asInt() == 1);
+		pParameters->Set_Enabled("SVG_OUTPUT", pParameter->asInt() == 1);
+	}
+
+	return( CSG_Tool::On_Parameters_Enable(pParameters, pParameter) );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+bool CSVG_Converter::On_Execute(void)
+{
+	#ifdef _SAGA_MSW
+	const SG_Char Separator = '\\';
+	#else
+	const SG_Char Separator = '/';
+	#endif
+
+	CSG_Strings Files; CSG_String OutputFile;
+
+	if( Parameters("DIRECTION")->asInt() == 0 )
+	{
+		Parameters("SVG_FILES")->asFilePath()->Get_FilePaths(Files);
+		OutputFile.Printf("%s%c%%s.svg.h", Parameters("HDR_OUTPUT")->asString(), Separator);
+	}
+	else
+	{
+		Parameters("HDR_FILES")->asFilePath()->Get_FilePaths(Files);
+		OutputFile.Printf("%s%c%%s"      , Parameters("SVG_OUTPUT")->asString(), Separator);
+	}
+
+	//-----------------------------------------------------
+	int nConverted = 0;
+
+	for(int i=0; i<Files.Get_Count(); i++)
+	{
+		CSG_String Content, Name = SG_File_Get_Name(Files[i], false);
+
+		Process_Set_Text("file: " + Name);
+
+		CSG_File Stream;
+
+		if( Stream.Open(Files[i], SG_FILE_R) && Stream.Read(Content, Stream.Length()) )
+		{
+			if( Parameters("DIRECTION")->asInt() == 0 )
+			{
+				Content.Replace("\n", ""    );
+				Content.Replace("\"", "\\\"");
+				Content.Prepend(CSG_String::Format("static const char svg_%s[] = \"", Name.c_str()));
+				Content.Append ("\";");
+			}
+			else
+			{
+				Content = Content.AfterFirst('\"').BeforeLast('\"');
+				Content.Replace("\\\"", "\"");
+			}
+
+			if( Stream.Open(CSG_String::Format(OutputFile.c_str(), Name.c_str()), SG_FILE_W) )
+			{
+				Stream.Write(Content);
+
+				nConverted++;
+			}
+		}
+	}
+
+	//-----------------------------------------------------
+	return( nConverted > 0 );
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
--- saga-gis/src/tools/develop/dev_tools/svg_converter.h
+++ saga-gis/src/tools/develop/dev_tools/svg_converter.h
@@ -10,9 +10,9 @@
 //                                                       //
 //-------------------------------------------------------//
 //                                                       //
-//                   TLB_Interface.cpp                   //
+//                    svg_converter.h                    //
 //                                                       //
-//                 Copyright (C) 2010 by                 //
+//                 Copyright (C) 2022 by                 //
 //                     Olaf Conrad                       //
 //                                                       //
 //-------------------------------------------------------//
@@ -46,73 +46,47 @@
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-// 1. Include the appropriate SAGA-API header...
+#ifndef HEADER_INCLUDED__svg_converter_H
+#define HEADER_INCLUDED__svg_converter_H
 
+
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
 #include <saga_api/saga_api.h>
 
 
-//---------------------------------------------------------
-// 2. Place general tool library informations here...
+///////////////////////////////////////////////////////////
+//														 //
+//														 //
+//														 //
+///////////////////////////////////////////////////////////
 
-CSG_String Get_Info(int i)
+//---------------------------------------------------------
+class CSVG_Converter : public CSG_Tool
 {
-	switch( i )
-	{
-	case TLB_INFO_Name:	default:
-		return( _TL("Development Tools") );
-
-	case TLB_INFO_Category:
-		return( "SAGA Development" );
-
-	case TLB_INFO_Author:
-		return( "O. Conrad (c) 2010" );
-
-	case TLB_INFO_Description:
-		return( _TL("Tools and helpers for the SAGA development.") );
-
-	case TLB_INFO_Version:
-		return( "1.0" );
-
-	case TLB_INFO_Menu_Path:
-		return( "SAGA Development|Development Tools" );
-	}
-}
+public:
+	CSVG_Converter(void);
 
 
-//---------------------------------------------------------
-// 3. Include the headers of your tools here...
+protected:
 
-#include "tl_extract.h"
-#include "tl_union.h"
-#include "tl_merge.h"
-#include "tool_counter.h"
-#include "arctoolbox.h"
-#include "tool_py_updater.h"
-#include "qgistoolbox.h"
-#include "svg_converter.h"
+	virtual int		On_Parameters_Enable	(CSG_Parameters *pParameters, CSG_Parameter *pParameter);
+
+	virtual bool	On_Execute				(void);
 
 
-//---------------------------------------------------------
-// 4. Allow your tools to be created here...
+private:
 
-CSG_Tool *		Create_Tool(int i)
-{
-	switch( i )
-	{
-	case  0: return( new CTL_Extract );
-	case  1: return( new CTL_Union );
-	case  2: return( new CTL_Merge );
-	case  3: return( new CTool_Counter );
-	case  4: return( new CTool_Menus );
-	case  5: return( new CArcToolBox );
-	case  6: return( new CTool_Py_Updater );
-	case  7: return( new CQGIS_ToolBox );
-	case  8: return( new CSVG_Converter );
+	int				Read_Directory	(const SG_Char *Directory, CSG_Table &Elements);
+	int				Read_File		(const SG_Char *File     , CSG_Table &Elements);
+	int				Read_Text		(const SG_Char *String   , CSG_String &Text);
 
-	case  9: return( NULL );
-	default: return( TLB_INTERFACE_SKIP_TOOL );
-	}
-}
+};
 
 
 ///////////////////////////////////////////////////////////
@@ -122,8 +96,4 @@ CSG_Tool *		Create_Tool(int i)
 ///////////////////////////////////////////////////////////
 
 //---------------------------------------------------------
-//{{AFX_SAGA
-
-	TLB_INTERFACE
-
-//}}AFX_SAGA
+#endif // #ifndef HEADER_INCLUDED__svg_converter_H
