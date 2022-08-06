3558aa2a985a05668f0426487887a5c8c9906ef8
reklov_w <wichmann@laserdata.at>
2022-08-06 15:40:12 +0200

    saga_gui: further svg icons added and enabled

--- saga-gis/src/saga_core/saga_gui/res_images.cpp
+++ saga-gis/src/saga_core/saga_gui/res_images.cpp
@@ -512,20 +512,20 @@ wxCursor	IMG_Get_Cursor(int ID_IMG)
 //#include "./res/svg/tb_map_layout_show.svg.h"
 //#include "./res/svg/tb_map_legend_horizontal.svg.h"
 //#include "./res/svg/tb_map_legend_vertical.svg.h"
-//#include "./res/svg/tb_map_mode_distance.svg.h"
+#include "./res/svg/tb_map_mode_distance.svg.h"
 #include "./res/svg/tb_map_mode_pan.svg.h"
-//#include "./res/svg/tb_map_mode_select.svg.h"
+#include "./res/svg/tb_map_mode_select.svg.h"
 #include "./res/svg/tb_map_mode_zoom.svg.h"
-//#include "./res/svg/tb_map_scalebar.svg.h"
-//#include "./res/svg/tb_map_north_arrow.svg.h"
-//#include "./res/svg/tb_map_synchronize.svg.h"
+#include "./res/svg/tb_map_scalebar.svg.h"
+#include "./res/svg/tb_map_north_arrow.svg.h"
+#include "./res/svg/tb_map_synchronize.svg.h"
 #include "./res/svg/tb_map_zoom_active.svg.h"
 #include "./res/svg/tb_map_pan_active.svg.h"
 #include "./res/svg/tb_map_zoom_full.svg.h"
 #include "./res/svg/tb_map_zoom_last.svg.h"
 #include "./res/svg/tb_map_zoom_next.svg.h"
 #include "./res/svg/tb_map_zoom_selection.svg.h"
-//#include "./res/svg/tb_map_crosshair.svg.h"
+#include "./res/svg/tb_map_crosshair.svg.h"
 #include "./res/svg/tb_new_project.svg.h"
 #include "./res/svg/tb_open.svg.h"
 //#include "./res/svg/tb_open_grid.svg.h"
@@ -663,20 +663,20 @@ const char * _Get_SVG(int ID_IMG)
 //	case ID_IMG_TB_MAP_LAYOUT_SHOW       : return( svg_tb_map_layout_show );
 //	case ID_IMG_TB_MAP_LEGEND_HORIZONTAL : return( svg_tb_map_legend_horizontal );
 //	case ID_IMG_TB_MAP_LEGEND_VERTICAL   : return( svg_tb_map_legend_vertical );
-//	case ID_IMG_TB_MAP_MODE_DISTANCE     : return( svg_tb_map_mode_distance );
+	case ID_IMG_TB_MAP_MODE_DISTANCE     : return( svg_tb_map_mode_distance );
 	case ID_IMG_TB_MAP_MODE_PAN          : return( svg_tb_map_mode_pan );
-//	case ID_IMG_TB_MAP_MODE_SELECT       : return( svg_tb_map_mode_select );
+	case ID_IMG_TB_MAP_MODE_SELECT       : return( svg_tb_map_mode_select );
 	case ID_IMG_TB_MAP_MODE_ZOOM         : return( svg_tb_map_mode_zoom );
-//	case ID_IMG_TB_MAP_SCALEBAR          : return( svg_tb_map_scalebar );
-//	case ID_IMG_TB_MAP_NORTH_ARROW       : return( svg_tb_map_north_arrow );
-//	case ID_IMG_TB_MAP_SYNCHRONIZE       : return( svg_tb_map_synchronize );
+	case ID_IMG_TB_MAP_SCALEBAR          : return( svg_tb_map_scalebar );
+	case ID_IMG_TB_MAP_NORTH_ARROW       : return( svg_tb_map_north_arrow );
+	case ID_IMG_TB_MAP_SYNCHRONIZE       : return( svg_tb_map_synchronize );
 	case ID_IMG_TB_MAP_ZOOM_ACTIVE       : return( svg_tb_map_zoom_active );
 	case ID_IMG_TB_MAP_PAN_ACTIVE        : return( svg_tb_map_pan_active );
 	case ID_IMG_TB_MAP_ZOOM_FULL         : return( svg_tb_map_zoom_full );
 	case ID_IMG_TB_MAP_ZOOM_LAST         : return( svg_tb_map_zoom_last );
 	case ID_IMG_TB_MAP_ZOOM_NEXT         : return( svg_tb_map_zoom_next );
 	case ID_IMG_TB_MAP_ZOOM_SELECTION    : return( svg_tb_map_zoom_selection );
-//	case ID_IMG_TB_MAP_CROSSHAIR         : return( svg_tb_map_crosshair );
+	case ID_IMG_TB_MAP_CROSSHAIR         : return( svg_tb_map_crosshair );
 	case ID_IMG_TB_NEW_PROJECT           : return( svg_tb_new_project );
 	case ID_IMG_TB_OPEN                  : return( svg_tb_open );
 //	case ID_IMG_TB_OPEN_GRID             : return( svg_tb_open_grid );
