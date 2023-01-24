d5b7c2ff928dffaad300a0f772e764f06687ea8b
oconrad <olaf.conrad@uni-hamburg.de>
2023-01-24 17:37:23 +0100

    accessories/helper/make_python_api_all.bat(make_saga_release.bat): updates for inclusion of latest stable python release (3.11)

--- saga-gis/src/accessories/helper/make_python_api_all.bat
+++ saga-gis/src/accessories/helper/make_python_api_all.bat
@@ -22,12 +22,14 @@ CMD /C CALL make_python_api.bat 3.7  win32 true false F:\develop\libs\Python\_wi
 CMD /C CALL make_python_api.bat 3.8  win32 true false F:\develop\libs\Python\_win32\Python-3.8
 CMD /C CALL make_python_api.bat 3.9  win32 true false F:\develop\libs\Python\_win32\Python-3.9
 CMD /C CALL make_python_api.bat 3.10 win32 true false F:\develop\libs\Python\_win32\Python-3.10
+CMD /C CALL make_python_api.bat 3.11 win32 true false F:\develop\libs\Python\_win32\Python-3.11
 
 CMD /C CALL make_python_api.bat 2.7  x64 true false F:\develop\libs\Python\Python-2.7
 CMD /C CALL make_python_api.bat 3.6  x64 true false F:\develop\libs\Python\Python-3.6
 CMD /C CALL make_python_api.bat 3.7  x64 true false F:\develop\libs\Python\Python-3.7
 CMD /C CALL make_python_api.bat 3.8  x64 true false F:\develop\libs\Python\Python-3.8
 CMD /C CALL make_python_api.bat 3.9  x64 true false F:\develop\libs\Python\Python-3.9
-CMD /C CALL make_python_api.bat 3.10 x64 true true  F:\develop\libs\Python\Python-3.10
+CMD /C CALL make_python_api.bat 3.10 x64 true false F:\develop\libs\Python\Python-3.10
+CMD /C CALL make_python_api.bat 3.11 x64 true true  F:\develop\libs\Python\Python-3.11
 
 PAUSE
