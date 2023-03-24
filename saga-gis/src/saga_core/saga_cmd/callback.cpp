b1570dc42f6ead6872ccfcd92775043a4893af98
Rainer Hurling <rhurlin@gwdg.de>
2023-03-24 13:37:41 +0100

    saga_cmd/callback.cpp: Fix whitespace

--- saga-gis/src/saga_core/saga_cmd/callback.cpp
+++ saga-gis/src/saga_core/saga_cmd/callback.cpp
@@ -157,7 +157,7 @@ void			CMD_Print			(const CSG_String &Text, const CSG_String &XML_Tag)
 	{
 		if( g_bUTF8 )
 		{
-			printf("%s\n", Text.to_UTF8 ().Get_Data());
+			printf("%s\n", Text.to_UTF8().Get_Data());
 		}
 		else
 		{
