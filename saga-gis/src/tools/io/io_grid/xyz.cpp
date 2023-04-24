856bde0535f25519d3e3909bce53d9c658b1f611
reklov_w <wichmann@laserdata.at>
2023-04-24 12:07:08 +0200

    Import Grid from XYZ: bug fix, the "Separator" parameter setting was wrongly assigned internally (e.g. tabulator ended up in semicolon)

--- saga-gis/src/tools/io/io_grid/xyz.cpp
+++ saga-gis/src/tools/io/io_grid/xyz.cpp
@@ -361,9 +361,9 @@ bool CXYZ_Import::On_Execute(void)
 	{
 	default: m_Delimiters = " \t\r\n";	break;
 	case  1: m_Delimiters =       " ";	break;
-	case  2: m_Delimiters =      "\t";	break;
-	case  3: m_Delimiters =       ",";	break;
-	case  4: m_Delimiters =       ";";	break;
+	case  2: m_Delimiters =       ",";	break;
+	case  3: m_Delimiters =       ";";	break;
+	case  4: m_Delimiters =      "\t";	break;
 	case  5: m_Delimiters = Parameters("USER")->asString();	break;
 	}
 
