8a8add6b54ab5232e16699df1aca2ff8a5968789
oconrad <olaf.conrad@uni-hamburg.de>
2023-07-11 14:15:05 +0200

    SAGA version (master) updated to next aspired version 9.2.0

--- saga-gis/src/accessories/helper/make_saga_release.bat
+++ saga-gis/src/accessories/helper/make_saga_release.bat
@@ -33,8 +33,8 @@ REM For all bug-fix-releases!
 REM Don't forget to activate the variable
 REM - SWITCH_TO_BRANCH -
 REM SET SWITCH_TO_BRANCH=saga-9.1
-SET SAGA_VER_TEXT=9.1.0
-SET SAGA_VER_NEXT=9.2.0
+SET SAGA_VER_TEXT=9.2.0
+SET SAGA_VER_NEXT=9.3.0
 SET SAGA_VERSION=saga-%SAGA_VER_TEXT%
 
 
--- saga-gis/src/saga_core/saga_api/saga_api.h
+++ saga-gis/src/saga_core/saga_api/saga_api.h
@@ -85,9 +85,9 @@
 
 //---------------------------------------------------------
 #define SAGA_MAJOR_VERSION		9
-#define SAGA_MINOR_VERSION		1
+#define SAGA_MINOR_VERSION		2
 #define SAGA_RELEASE_NUMBER		0
-#define SAGA_VERSION			SG_T("9.1.0")
+#define SAGA_VERSION			SG_T("9.2.0")
 
 
 ///////////////////////////////////////////////////////////
