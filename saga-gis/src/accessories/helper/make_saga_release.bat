bfca7e82665cfca1289c7c21e87eebb054230493
oconrad <olaf.conrad@uni-hamburg.de>
2023-07-26 18:34:20 +0200

    accessories/helper/make_saga_release.bat u. doxygen: version numbering info (omit patch number => 9.1 instead of 9.1.1)

--- saga-gis/src/accessories/helper/make_saga_release.bat
+++ saga-gis/src/accessories/helper/make_saga_release.bat
@@ -29,13 +29,16 @@ IF "%SAGA_BIN%" == "" (
 REM ___________________________________
 REM Version
 
-REM For all bug-fix-releases!
+SET SAGA_VERSION=9.2
+SET SAGA_RELEASE=9.2.0
+SET SAGA_VERSION_NEXT=9.3
+SET SAGA_RELEASE_NEXT=9.3.0
+SET SAGA_RELEASE_NAME=saga-%SAGA_RELEASE%
+
+REM !!! For all bug-fix-releases !!!
 REM Don't forget to activate the variable
 REM - SWITCH_TO_BRANCH -
-REM SET SWITCH_TO_BRANCH=saga-9.1
-SET SAGA_VER_TEXT=9.2.0
-SET SAGA_VER_NEXT=9.3.0
-SET SAGA_VERSION=saga-%SAGA_VER_TEXT%
+REM SET SWITCH_TO_BRANCH=saga-%SAGA_VERSION%
 
 
 REM ___________________________________
@@ -46,12 +49,12 @@ ECHO #           B E G I N            #
 ECHO #      with the preparation      #
 ECHO #    of the release files for    #
 ECHO #                                #
-ECHO #          SAGA %SAGA_VER_TEXT%            #
+ECHO #          %SAGA_RELEASE_NAME%            #
 ECHO #                                #
 ECHO ##################################
 ECHO __________________________________
 ECHO Things you should have updated and committed before:
-ECHO.  - dev_tools.dll
+ECHO.  - Windows Build (including dev_tools.dll)
 ECHO.  - Translation Files
 ECHO.
 ECHO Enter 'y' to continue!
@@ -66,8 +69,8 @@ REM ###################################
 
 CLS
 
-MKDIR "%SAGA_VERSION%"
-PUSHD "%SAGA_VERSION%"
+MKDIR "%SAGA_RELEASE_NAME%"
+PUSHD "%SAGA_RELEASE_NAME%"
 
 
 REM ___________________________________
@@ -77,9 +80,9 @@ REM ###################################
 
 REM ___________________________________
 REM GIT Source Code Repository
-%GITEXE% clone git://git.code.sf.net/p/saga-gis/code %SAGA_VERSION%
+%GITEXE% clone git://git.code.sf.net/p/saga-gis/code %SAGA_RELEASE_NAME%
 
-PUSHD %SAGA_VERSION%
+PUSHD %SAGA_RELEASE_NAME%
 
 IF /i NOT "%SWITCH_TO_BRANCH%" == "" (
 	ECHO switch to branch %SWITCH_TO_BRANCH%
@@ -91,17 +94,17 @@ POPD
 
 REM ___________________________________
 REM Zip Source Code
-%ZIPEXE% a -r -y -mx5 %SAGA_VERSION%_src.zip %SAGA_VERSION%
+%ZIPEXE% a -r -y -mx5 %SAGA_RELEASE_NAME%_src.zip %SAGA_RELEASE_NAME%
 
 REM Create a tarball
-%ZIPEXE% a -r -y -ttar %SAGA_VERSION%.tar %SAGA_VERSION%
-%ZIPEXE% a -y -mx5 -tgzip %SAGA_VERSION%.tar.gz %SAGA_VERSION%.tar
-DEL %SAGA_VERSION%.tar
+%ZIPEXE% a -r -y -ttar %SAGA_RELEASE_NAME%.tar %SAGA_RELEASE_NAME%
+%ZIPEXE% a -y -mx5 -tgzip %SAGA_RELEASE_NAME%.tar.gz %SAGA_RELEASE_NAME%.tar
+DEL %SAGA_RELEASE_NAME%.tar
 
 
 REM ___________________________________
 REM Drop Sources
-RMDIR /S/Q %SAGA_VERSION%
+RMDIR /S/Q %SAGA_RELEASE_NAME%
 
 
 REM ___________________________________
@@ -110,11 +113,11 @@ REM Doxygen API Documentation
 REM ###################################
 
 %DOXEXE% ..\doxygen_saga_api_html"
-%ZIPEXE% a -r -y -mx5 "%SAGA_VERSION%_api_doc.zip" "%SAGA_VERSION%_api_doc"
+%ZIPEXE% a -r -y -mx5 "%SAGA_RELEASE_NAME%_api_doc.zip" "%SAGA_RELEASE_NAME%_api_doc"
 
 %DOXEXE% ..\doxygen_saga_api_chm"
 
-RMDIR /S/Q "%SAGA_VERSION%_api_doc"
+RMDIR /S/Q "%SAGA_RELEASE_NAME%_api_doc"
 
 
 REM ___________________________________
@@ -158,8 +161,8 @@ REM ###################################
 REM ___________________________________
 REM collect files...
 
-MKDIR "%SAGA_VERSION%_x64"
-PUSHD "%SAGA_VERSION%_x64"
+MKDIR "%SAGA_RELEASE_NAME%_x64"
+PUSHD "%SAGA_RELEASE_NAME%_x64"
 XCOPY /C/S/Q/Y/H "%SAGA_BIN%"
 DEL /F *.ini *.cfg *.exp *.pdb *.tmp tools\*.exp tools\*.lib tools\*.pdb tools\dev_*.*
 RMDIR /S/Q PySAGA\__pycache__
@@ -168,21 +171,21 @@ POPD
 REM ___________________________________
 REM create zip file...
 
-%ZIPEXE% a -r -y -mx5 "%SAGA_VERSION%_x64.zip" "%SAGA_VERSION%_x64"
+%ZIPEXE% a -r -y -mx5 "%SAGA_RELEASE_NAME%_x64.zip" "%SAGA_RELEASE_NAME%_x64"
 
 REM ___________________________________
 REM create setup file...
 
-ATTRIB -H "%SAGA_VERSION%_x64\*.*" /S /D
-COPY "%SAGA_ROOT%\..\saga_setup_readme.rtf" "%SAGA_VERSION%_x64"
-COPY "%SAGA_ROOT%\..\saga_setup_x64.iss" "%SAGA_VERSION%_x64"
-%ISETUP% "%SAGA_VERSION%_x64\saga_setup_x64.iss"
-MOVE "%SAGA_VERSION%_x64\%SAGA_VERSION%_x64_setup.exe"
+ATTRIB -H "%SAGA_RELEASE_NAME%_x64\*.*" /S /D
+COPY "%SAGA_ROOT%\..\saga_setup_readme.rtf" "%SAGA_RELEASE_NAME%_x64"
+COPY "%SAGA_ROOT%\..\saga_setup_x64.iss" "%SAGA_RELEASE_NAME%_x64"
+%ISETUP% "%SAGA_RELEASE_NAME%_x64\saga_setup_x64.iss"
+MOVE "%SAGA_RELEASE_NAME%_x64\%SAGA_RELEASE_NAME%_x64_setup.exe"
 
 REM ___________________________________
 REM clean up...
 
-RMDIR /S/Q "%SAGA_VERSION%_x64"
+RMDIR /S/Q "%SAGA_RELEASE_NAME%_x64"
 
 
 REM ___________________________________
@@ -201,23 +204,29 @@ ECHO #        F I N I S H E D         #
 ECHO #      with the preparation      #
 ECHO #    of the release files for    #
 ECHO #                                #
-ECHO #          SAGA %SAGA_VER_TEXT%            #
+ECHO #          %SAGA_RELEASE_NAME%            #
 ECHO #                                #
 ECHO ##################################
 ECHO __________________________________
 ECHO What is left to do ?!
 ECHO.
-ECHO - Don't forget to make the Linux tarball!
-ECHO.    make dist
-ECHO.
 ECHO - Upload all release files
 ECHO.    including an up-to-date 'readme.txt'
 ECHO.
 ECHO - Upload API Documentation to saga-gis.org
+ECHO.    saga_api_doc (HTML)
 ECHO.
-ECHO - Create new branch: saga-%SAGA_VER_TEXT%
-ECHO.    and don't forget to activate the SWITCH_TO_BRANCH flag
+IF /i "%SWITCH_TO_BRANCH%" == "" (
+ECHO - Add new bug tracker milestone for next aspired version
+ECHO     major.minor version without patch number: saga-%SAGA_VERSION_NEXT%
+ECHO.    https://sourceforge.net/p/saga-gis/bugs/milestones
+ECHO.
+ECHO - Create new bug-fix-branch: saga-%SAGA_VERSION%
+ECHO.    activate the SWITCH_TO_BRANCH flag for this branch
 ECHO.    for all bug-fix-releases!
+ECHO.
+)
+ECHO - Create new tag: saga-%SAGA_RELEASE%
 ECHO.
 ECHO - Update version numbers accordingly:
 ECHO.    ./saga_setup_x64.iss
@@ -228,16 +237,13 @@ ECHO.    ./saga-gis/src/saga_core/saga_gui/man/saga_gui.1
 ECHO.    ./saga-gis/src/accessories/helper/doxygen_saga_api_chm
 ECHO.    ./saga-gis/src/accessories/helper/doxygen_saga_api_html
 ECHO.    ./saga-gis/src/accessories/helper/make_saga_release.bat (this file!)
-ECHO.  and commit: SAGA version updated to next aspired version %SAGA_VER_NEXT%
+ECHO.  and commit: SAGA version updated to next aspired version %SAGA_RELEASE_NEXT%
 ECHO.
 ECHO - Create SAGA Tools Reference Documentation
 ECHO.    sagadoc-code: ./parse_modules.py
 ECHO.    upload created version folder to saga-gis.org and update link
 ECHO.
-ECHO - Add new bug tracker milestone for next aspired version
-ECHO.    https://sourceforge.net/p/saga-gis/bugs/milestones
-ECHO.
-ECHO - Commit a comment like: SAGA version updated to %SAGA_VER_TEXT%
+ECHO - Commit a comment like: SAGA version updated to %SAGA_RELEASE%
 ECHO.    https://sourceforge.net/p/saga-gis/news/
 ECHO.
 ECHO __________________________________
