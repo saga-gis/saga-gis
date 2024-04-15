5ede5be7838b6fdc3a98eb3628a3a02604d5b4ec
oconrad <olaf.conrad@uni-hamburg.de>
2024-04-15 14:46:32 +0200

    SAGA version (master) updated to next aspired version 9.5.0

--- saga-gis/src/saga_core/saga_api/saga_api.h
+++ saga-gis/src/saga_core/saga_api/saga_api.h
@@ -85,9 +85,9 @@
 
 //---------------------------------------------------------
 #define SAGA_MAJOR_VERSION		9
-#define SAGA_MINOR_VERSION		4
+#define SAGA_MINOR_VERSION		5
 #define SAGA_RELEASE_NUMBER		0
-#define SAGA_VERSION			SG_T("9.4.0")
+#define SAGA_VERSION			SG_T("9.5.0")
 
 
 ///////////////////////////////////////////////////////////
