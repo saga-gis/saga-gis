853ca68151e319e8f65321ca87fc40f8b34f67f3
oconrad <olaf.conrad@uni-hamburg.de>
2024-05-29 15:50:40 +0200

    statistics_points, Minimum Distance Analysis: distance measurement dimension option added for 3d-points/point clouds

--- saga-gis/src/tools/statistics/statistics_points/GSPoints_Distances.cpp
+++ saga-gis/src/tools/statistics/statistics_points/GSPoints_Distances.cpp
@@ -66,8 +66,30 @@ CGSPoints_Distances::CGSPoints_Distances(void)
 		_TL("")
 	);
 
-	Parameters.Add_Shapes("", "POINTS", _TL("Points"                   ), _TL(""), PARAMETER_INPUT, SHAPE_TYPE_Point);
-	Parameters.Add_Table ("", "TABLE" , _TL("Minimum Distance Analysis"), _TL(""), PARAMETER_OUTPUT);
+	Parameters.Add_Shapes("", "POINTS"   , _TL("Points"                   ), _TL(""), PARAMETER_INPUT, SHAPE_TYPE_Point);
+	Parameters.Add_Table ("", "TABLE"    , _TL("Minimum Distance Analysis"), _TL(""), PARAMETER_OUTPUT);
+	Parameters.Add_Choice("", "DIMENSION", _TL("Distance Measurement"     ), _TL(""),
+		CSG_String::Format("%s|%s",
+			_TL("2-dimensional (planar)"),
+			_TL("3-dimensional")
+		), 0
+	);
+}
+
+
+///////////////////////////////////////////////////////////
+//														 //
+///////////////////////////////////////////////////////////
+
+//---------------------------------------------------------
+int CGSPoints_Distances::On_Parameters_Enable(CSG_Parameters *pParameters, CSG_Parameter *pParameter)
+{
+	if( pParameter->Cmp_Identifier("POINTS") )
+	{
+		pParameters->Set_Enabled("DIMENSION", pParameter->asShapes() && pParameter->asShapes()->Get_Vertex_Type() != SG_VERTEX_TYPE_XY);
+	}
+
+	return( CSG_Tool::On_Parameters_Enable(pParameters, pParameter) );
 }
 
 
@@ -78,32 +100,55 @@ CGSPoints_Distances::CGSPoints_Distances(void)
 //---------------------------------------------------------
 bool CGSPoints_Distances::On_Execute(void)
 {
-	CSG_Shapes *pPoints = Parameters("POINTS")->asShapes();
-	CSG_Table  *pTable  = Parameters("TABLE" )->asTable ();
+	CSG_Simple_Statistics s; CSG_Shapes *pPoints = Parameters("POINTS")->asShapes();
 
-	//-----------------------------------------------------
-	CSG_KDTree_2D Search(pPoints, 0); CSG_Simple_Statistics s;
-
-	for(sLong iPoint=0; iPoint<pPoints->Get_Count() && Set_Progress(iPoint, pPoints->Get_Count()); iPoint++)
+	if( pPoints->Get_Vertex_Type() == SG_VERTEX_TYPE_XY || Parameters("DIMENSION")->asInt() == 0 )
 	{
-		TSG_Point p = pPoints->Get_Shape(iPoint)->Get_Point(); size_t Index[2]; double Distance[2];
+		CSG_KDTree_2D Search(pPoints, 0);
 
-		if( Search.Get_Nearest_Points(p.x, p.y, 2, Index, Distance) && Distance[1] > 0. )
+		for(sLong iPoint=0; iPoint<pPoints->Get_Count() && Set_Progress(iPoint, pPoints->Get_Count()); iPoint++)
 		{
-			s += Distance[1];
+			size_t Index[2]; double Distance[2]; CSG_Point p = pPoints->Get_Shape(iPoint)->Get_Point();
+
+			if( Search.Get_Nearest_Points(p.x, p.y, 2, Index, Distance) && Distance[1] > 0. )
+			{
+				s += Distance[1];
+			}
+		}
+	}
+	else
+	{
+		CSG_KDTree_3D Search(pPoints, 0);
+
+		for(sLong iPoint=0; iPoint<pPoints->Get_Count() && Set_Progress(iPoint, pPoints->Get_Count()); iPoint++)
+		{
+			size_t Index[2]; double Distance[2]; CSG_Point_3D p = pPoints->Get_Shape(iPoint)->Get_Point_Z();
+
+			if( Search.Get_Nearest_Points(p.x, p.y, p.z, 2, Index, Distance) && Distance[1] > 0. )
+			{
+				s += Distance[1];
+			}
 		}
 	}
 
 	//-----------------------------------------------------
 	if( s.Get_Count() > 0 )
 	{
-		pTable->Destroy();
-		pTable->Fmt_Name("%s [%s]", _TL("Minimum Distance Analysis"), pPoints->Get_Name());
+		CSG_Table *pTable = Parameters("TABLE")->asTable(); pTable->Destroy();
+
+		if( pPoints->Get_Vertex_Type() == SG_VERTEX_TYPE_XY || Parameters("DIMENSION")->asInt() == 0 )
+		{
+			pTable->Fmt_Name("%s [%s]"   , _TL("Minimum Distance Analysis"), pPoints->Get_Name());
+		}
+		else
+		{
+			pTable->Fmt_Name("%s [3D %s]", _TL("Minimum Distance Analysis"), pPoints->Get_Name());
+		}
 
 		pTable->Add_Field("NAME" , SG_DATATYPE_String);
 		pTable->Add_Field("VALUE", SG_DATATYPE_Double);
 
-		#define SET_VALUE(name, value)	{ CSG_Table_Record *pRecord = pTable->Add_Record(); pRecord->Set_Value(0, name); pRecord->Set_Value(1, value); }
+		#define SET_VALUE(name, value) { CSG_Table_Record &r = *pTable->Add_Record(); r.Set_Value(0, name); r.Set_Value(1, value); }
 
 		SET_VALUE(_TL("Mean Average"      ), s.Get_Mean   ());
 		SET_VALUE(_TL("Minimum"           ), s.Get_Minimum());
