77980c6c6ce89f7a01a3c9204fb21c0867a503ce
Justus Spitzmüller <jspitzmueller@scilands.de>
2025-03-16 15:40:34 +0100

    ta_preprocessor/fill_minima: Boundary Bug fix

--- saga-gis/src/tools/terrain_analysis/ta_preprocessor/fill_minima.cpp
+++ saga-gis/src/tools/terrain_analysis/ta_preprocessor/fill_minima.cpp
@@ -79,165 +79,6 @@ represent potential shadow objects.
 #include <math.h>
 
 
-/* Routines for handling the hierarchical pixel queue which the
-   algorithm requires.
-*/
-typedef struct PQstruct {
-    int i, j;
-    struct PQstruct *next;
-} PQel;
-    
-typedef struct {
-    PQel *first, *last;
-    int n;
-} PQhdr;
-    
-typedef struct PQ {
-    int hMin;
-    int numLevels;
-    PQhdr *q;
-} PixelQueue;
-
-/* A new pixel structure */
-static PQel *newPix(int i, int j) {
-    PQel *p;
-        
-    p = (PQel *)calloc(1, sizeof(PQel));
-    p->i = i;
-    p->j = j;
-    p->next = NULL;
-    if (i>20000) {
-        //printf("i=%d\\n", i);
-        exit(1);
-    }
-    return p;
-}
-    
-/* Initialize pixel queue */
-static PixelQueue *PQ_init(int hMin, int hMax) {
-    PixelQueue *pixQ;
-    int numLevels, i;
-        
-    pixQ = (PixelQueue *)calloc(1, sizeof(PixelQueue));
-    numLevels = hMax - hMin + 1;
-    pixQ->hMin = hMin;
-    pixQ->numLevels = numLevels;
-    pixQ->q = (PQhdr *)calloc(numLevels, sizeof(PQhdr));
-    for (i=0; i<numLevels; i++) {
-        pixQ->q[i].first = NULL;
-        pixQ->q[i].last = NULL;
-        pixQ->q[i].n = 0;
-    }
-    return pixQ;
-}
-    
-/* Add a pixel at level h */
-static void PQ_add(PixelQueue *pixQ, PQel *p, int h) {
-    int ndx;
-    PQel *current, *newP;
-    PQhdr *thisQ;
-        
-    /* Take a copy of the pixel structure */
-    newP = newPix(p->i, p->j);
-        
-    ndx = h - pixQ->hMin;
-    if (ndx > pixQ->numLevels) {
-        //printf("Level h=%d too large. ndx=%d, numLevels=%d\\n", h, ndx, pixQ->numLevels);
-        exit(1);
-    }
-    if (ndx < 0) {
-        //printf("Ndx is negative, which is not allowed. ndx=%d, h=%d, hMin=%d\n", ndx, h, pixQ->hMin);
-        exit(1);
-    }
-    thisQ = &(pixQ->q[ndx]);
-    /* Add to end of queue at this level */
-    current = thisQ->last;
-    if (current != NULL) {
-        current->next = newP;
-    }
-    thisQ->last = newP;
-    thisQ->n++;
-    /* If head of queue is NULL, make the new one the head */
-    if (thisQ->first == NULL) {
-        thisQ->first = newP;
-    }
-}
-    
-/* Return TRUE if queue at level h is empty */
-static int PQ_empty(PixelQueue *pixQ, int h) {
-    int ndx, empty, n;
-    PQel *current;
-        
-    ndx = h - pixQ->hMin;
-    current = pixQ->q[ndx].first;
-    n = pixQ->q[ndx].n;
-    empty = (current == NULL);
-    if (empty && (n != 0)) {
-        //printf("Empty, but n=%d\\n", n);
-        exit(1);
-    }
-    if ((n == 0) && (! empty)) {
-        //printf("n=0, but not empty\\n");
-        while (current != NULL) {
-            //printf("    h=%d i=%d j=%d\\n", h, current->i, current->j);
-            current = current->next;
-        }
-        exit(1);
-    }
-    return empty;
-}
-    
-/* Return the first element in the queue at level h, and remove it
-   from the queue */
-static PQel *PQ_first(PixelQueue *pixQ, int h) {
-    int ndx;
-    PQel *current;
-    PQhdr *thisQ;
-        
-    ndx = h - pixQ->hMin;
-    thisQ = &(pixQ->q[ndx]);
-    current = thisQ->first;
-    /* Remove from head of queue */
-    if (current != NULL) {
-        thisQ->first = current->next;
-        if (thisQ->first == NULL) {
-            thisQ->last = NULL;
-        }
-        thisQ->n--;
-        if (thisQ->n < 0) {
-            //printf("n=%d in PQ_first()\\n", thisQ->n);
-            exit(1);
-        } else if (thisQ->n == 0) {
-            if (thisQ->first != NULL) {
-                //printf("n=0, but 'first' != NULL. first(i,j) = %d,%d\\n", 
-                //   thisQ->first->i, thisQ->first->j);
-            }
-        }
-    }
-    return current;
-}
-    
-/* Return a list of neighbouring pixels to given pixel p.  */
-static PQel *neighbours(PQel *p, int nRows, int nCols) {
-    int ii, jj, i, j;
-    PQel *pl, *pNew;
-        
-    pl = NULL;
-    for (ii=-1; ii<=1; ii++) {
-        for (jj=-1; jj<=1; jj++) {
-            if ((ii != 0) && (jj != 0)) {
-                i = p->i + ii;
-                j = p->j + jj;
-                if ((i >= 0) && (i < nRows) && (j >= 0) && (j < nCols)) {
-                    pNew = newPix(i, j);
-                    pNew->next = pl;
-                    pl = pNew;
-                }
-            }
-        }
-    }
-    return pl;
-}
 
 
 CFillMinima::CFillMinima(void)
@@ -274,47 +115,72 @@ CFillMinima::CFillMinima(void)
 		PARAMETER_OUTPUT, true, SG_DATATYPE_Byte 
 	);
 
-	Parameters.Add_Int(
-		"", "BOUNDARY", _TL("Boundary Value"), _TL(""), 1
+	Parameters.Add_Double(
+		"", "BOUNDARY", _TL("Boundary Value"), _TL(""), 0.197
 	);
 }
 
 
 //#define max(a,b) ((a) > (b) ? (a) : (b))
 
+struct Cell {
+    sLong x, y;
+	int height;
+    bool operator>(const Cell &other) const {
+        return height > other.height;
+    }
+};
+
 
 static int	xnb[] = { 0, 1, 1, 1, 0,-1,-1,-1};
 static int	ynb[] = { 1, 1, 0,-1,-1,-1, 0, 1};
 
+#include <vector>
+#include <queue>
+#include <utility>
+
 bool CFillMinima::On_Execute(void)
 {
-	CSG_Grid *pInput = Parameters("DEM")->asGrid();
-	CSG_Grid *pImg2 = Parameters("RESULT")->asGrid();
+	m_pInput = Parameters("DEM")->asGrid();
+	m_pImg2 = Parameters("RESULT")->asGrid();
 
-	pImg2->Create( Get_System(), pInput->Get_Type() );
-	pImg2->Set_Name( CSG_String::Format("%s_Fill", pInput->Get_Name()) );
+	m_pImg2->Create( Get_System(), m_pInput->Get_Type() );
+	m_pImg2->Set_Name( CSG_String::Format("%s_Fill", m_pInput->Get_Name()) );
 
-	double NoDataValue = pInput->Get_NoData_Value();
-	pImg2->Set_NoData_Value( NoDataValue );
-	pImg2->Set_Scaling( pInput->Get_Scaling(), pInput->Get_Offset() );
+	double NoDataValue = m_pInput->Get_NoData_Value();
+	m_pImg2->Set_NoData_Value( NoDataValue );
+	m_pImg2->Set_Scaling( m_pInput->Get_Scaling(), m_pInput->Get_Offset() );
 
-	int hMin = 0;
-	int hMax = 0;
+	int hMin = INT_MAX;
+	int hMax = INT_MIN;
 
 	Process_Set_Text(_TL("Creating statistics"));
 	for(sLong y=0; y<Get_NY() && Set_Progress_Rows(y); y++)
 	{
-		#pragma omp parallel for
+		int local_hMin = INT_MAX;
+		int local_hMax = INT_MIN;
+	
+		#pragma omp parallel for reduction(min:local_hMin) reduction(max:local_hMax)
 		for(sLong x=0; x<Get_NX(); x++)
 		{
-			int value = pInput->asInt(x, y, false);
-			hMin = std::min( hMin, value );
-			hMax = std::max( hMax, value );
+			if( !m_pInput->is_NoData(x,y) )
+			{
+				int value = m_pInput->asInt(x, y, false);
+				local_hMin = std::min( local_hMin, value );
+				local_hMax = std::max( local_hMax, value );
+			}
 		}
+
+		hMin = std::min(hMin, local_hMin);
+		hMax = std::max(hMax, local_hMax);
 	}
 	
 
-	int BoundaryVal = Parameters("BOUNDARY")->asInt();
+	std::priority_queue<Cell, std::vector<Cell>, std::greater<Cell>> pq;
+	
+	
+
+	int BoundaryVal = (int) ((Parameters("BOUNDARY")->asDouble() - m_pInput->Get_Offset()) / m_pInput->Get_Scaling() );
 	
 	Process_Set_Text(_TL("Initializing output"));
 	for(sLong y=0; y<Get_NY() && Set_Progress_Rows(y); y++)
@@ -322,129 +188,211 @@ bool CFillMinima::On_Execute(void)
 		#pragma omp parallel for
 		for(sLong x=0; x<Get_NX(); x++)
 		{
-			pImg2->Set_Value( x, y, pInput->is_NoData(x,y) ? NoDataValue : hMax, false);
+			m_pInput->is_NoData(x,y) ? m_pImg2->Set_NoData(x,y) : m_pImg2->Set_Value( x, y, hMax, false);
 		}
 	}
 
 	
-	PixelQueue *pixQ = PQ_init(hMin,hMax);
+	int Num_Threads = SG_OMP_Get_Max_Num_Threads();
 
-	sLong nx = pInput->Get_NX();
-	sLong ny = pInput->Get_NY();
+    int Chunk_Size 	= Get_NY() / Num_Threads;
 
-	Process_Set_Text(_TL("Initializing boundary"));
-	if( pInput->Get_NoData_Count() == 0 )
+
+	//PixelQueue *pixQ = Create_Queue( hMin, hMax, BoundaryVal, 0, Get_NX(), 924, 925 );
+
+    #pragma omp parallel
+    {
+        int Thread_ID = SG_OMP_Get_Thread_Num();
+
+		int yStart	= Thread_ID * Chunk_Size;
+		int yEnd 	= Thread_ID == Num_Threads-1 ? Get_NY() : (Thread_ID + 1) * Chunk_Size;
+
+		//int hMin = INT_MAX;
+		//int hMax = INT_MIN;
+
+		//for(sLong y=yStart; y<yEnd; y++)
+		//{
+		//	//#pragma omp parallel for reduction(min:local_hMin) reduction(max:local_hMax)
+		//	for(sLong x=0; x<Get_NX(); x++)
+		//	{
+		//		if( !m_pInput->is_NoData(x,y) )
+		//		{
+		//			int value = m_pInput->asInt(x, y, false);
+		//			hMin = std::min( hMin, value );
+		//			hMax = std::max( hMax, value );
+		//		}
+		//	}
+		//}
+
+		PixelQueue* pixQ = Create_Queue( hMin, hMax, BoundaryVal, 0, Get_NX(), yStart, yEnd );
+		bool b = Fill_Queue( pixQ, hMin, hMax, 0, Get_NX(), yStart, yEnd);
+    }
+
+	//int yStart	= 1 * Chunk_Size;
+	//int yEnd 	= 1 == Num_Threads-2 ? Get_NY() : (1 + 1) * Chunk_Size;
+	
+	//int yStart	= 0;
+	//int yEnd 	= Get_NY();
+	/*
+		int yStart	= 1 * Chunk_Size;
+		int yEnd 	= 1 == Num_Threads-2 ? Get_NY() : (1 + 1) * Chunk_Size;
+
+	int hMin = INT_MAX;
+	int hMax = INT_MIN;
+
+	for(sLong y=yStart; y<yEnd; y++)
 	{
-		int Range = Get_NX() + Get_NY();	
-		Set_Progress(0, Range);
-		
-		for( int x=0; x<nx; x++ )
+		//#pragma omp parallel for reduction(min:local_hMin) reduction(max:local_hMax)
+		for(sLong x=0; x<Get_NX(); x++)
 		{
-			PQel *p = newPix(0, x);
-			PQ_add(pixQ, p, BoundaryVal);
-			pImg2->Set_Value(x, 0, BoundaryVal, false);
-			
-			p = newPix(ny-1, x);
-			PQ_add(pixQ, p, BoundaryVal);
-			pImg2->Set_Value(x, ny-1, BoundaryVal, false);
-		}
-
-		Set_Progress(Get_NX(), Range);
-		
-		for( int y=1; y<ny-1; y++ )
-		{
-			PQel *p = newPix(y, nx-1);
-			PQ_add(pixQ, p, BoundaryVal);
-			pImg2->Set_Value(nx-1, y, BoundaryVal, false);
-			p = newPix(y, 0);
-			PQ_add(pixQ, p, BoundaryVal);
-			pImg2->Set_Value(0, y, BoundaryVal, false);
-		}
-		
-		//for( int x=0; x<nx; x++ )
-		//{
-		//	PQel *p = newPix(0, x);
-		//	PQ_add(pixQ, p, BoundaryVal);
-		//	pImg2->Set_Value(x, 0, BoundaryVal, false);
-		//}
-
-		//for( int y=1; y<ny-1; y++ )
-		//{
-		//	PQel *p = newPix(y, nx-1);
-		//	PQ_add(pixQ, p, BoundaryVal);
-		//	pImg2->Set_Value(nx-1, y, BoundaryVal, false);
-		//}
-		//
-		//for( int x=nx-1; x<=0; x-- )
-		//{
-		//	PQel *p = newPix(ny-1, x);
-		//	PQ_add(pixQ, p, BoundaryVal);
-		//	pImg2->Set_Value(x, ny-1, BoundaryVal, false);
-		//}
-		//
-		//for( int y=ny-1; y<=1 ; y++ )
-		//{
-		//	PQel *p = newPix(y, 0);
-		//	PQ_add(pixQ, p, BoundaryVal);
-		//	pImg2->Set_Value(0, y, BoundaryVal, false);
-		//}
-	}
-	else 
-	{
-		for( sLong y=0; y<Get_NY() && Set_Progress_Rows(y); y++ )
-		{
-			for( sLong x=0; x<Get_NX(); x++ )
+			if( !m_pInput->is_NoData(x,y) )
 			{
-	   			for( int i=0; i<8; i++ )
-		   		{
-					int xdiff = x + xnb[i];
-					int ydiff = y + ynb[i];
-					
-					// This also checks nodata (last bool)
-					if( !pInput->is_InGrid( xdiff, ydiff, true ) )
-					{
-						PQel *p = newPix(y, x);
-						PQ_add(pixQ, p, BoundaryVal);
-						pImg2->Set_Value(x, y, BoundaryVal, false);
-
-						// Get out of the loop
-						break;
-					}
-	   			}
+				int value = m_pInput->asInt(x, y, false);
+				hMin = std::min( hMin, value );
+				hMax = std::max( hMax, value );
 			}
 		}
 	}
+	for(sLong y=0; y<Get_NY() && Set_Progress_Rows(y); y++)
+	{
+		#pragma omp parallel for
+		for(sLong x=0; x<Get_NX(); x++)
+		{
+			m_pInput->is_NoData(x,y) ? m_pImg2->Set_NoData(x,y) : m_pImg2->Set_Value( x, y, hMax, false);
+		}
+	}
+	*/
+	/*
+	CSG_Grid Tick_Off( Get_System(), SG_DATATYPE_Bit);
+	Tick_Off.Set_NoData(0);
+	Tick_Off.Assign_NoData();
+	
+	for( sLong y=yStart; y<yEnd; y++ )
+	{
+		for( sLong x=0; x<Get_NX(); x++ )
+		{
+			if( m_pInput->is_NoData(x,y) )
+			{
+				for( int i=0; i<8; i++ )
+				{
+					int xdiff = x + xnb[i];
+					int ydiff = y + ynb[i];
+					
+					if( m_pInput->is_InGrid( xdiff, ydiff, true ) && Tick_Off.is_NoData(xdiff,ydiff) )
+					{
+
+                    	int imgval = m_pInput->asInt(xdiff, ydiff, false);
+						pq.push ( {x, y, imgval} );
+						
+						
+						Tick_Off.Set_Value(xdiff,ydiff,1.);
+					}
+				}
+			}
+	 	}
+	}
+	
+	Tick_Off.Assign_NoData();
 
 
-    /* Process until stability */
+    while (!pq.empty()) {
+        Cell cell = pq.top();
+        pq.pop();
+
+		std::vector<std::pair<sLong,sLong>> n({{0,1}, {1,0}, {0,-1}, {-1,0}});
+		
+		if( !Tick_Off.is_NoData(cell.x,cell.y) ) continue;
+		
+		Tick_Off.Set_Value(cell.x,cell.y, 1.0, true);
+
+        for (auto d : n) 
+		{
+            sLong nx = cell.x + d.first;
+			sLong ny = cell.y + d.second;
+            if (nx >= 0 && ny >= 0 && nx < Get_NX() && ny < Get_NY() && Tick_Off.is_NoData(nx,ny) ) {
+				int height = m_pImg2->asInt(nx,ny, false);
+				m_pImg2->Set_Value( nx, ny, std::max( height, cell.height ) );
+
+                pq.push({nx, ny, height});
+            }
+        }
+    }
+
+
+
+
+	return true;
+	*/
+
+
+	//PixelQueue* pixQ = Create_Queue( hMin, hMax, BoundaryVal, 0, Get_NX(), yStart, yEnd );
+	//bool b = Fill_Queue( pixQ, hMin, hMax, 0, Get_NX(), yStart, yEnd);
+
+	//for(sLong y=924; y<926 && Set_Progress_Rows(y); y++)
+	//{
+	//	#pragma omp parallel for
+	//	for(sLong x=0; x<Get_NX(); x++)
+	//	{
+	//		m_pInput->is_NoData(x,y) ? m_pImg2->Set_NoData(x,y) : m_pImg2->Set_Value( x, y, hMax, false);
+	//	}
+	//}
+
+	//bool b = Fill_Queue( pixQ, hMin, hMax, 0, Get_NX(), 0, 1850);
+    
+	//int Chunk_Size 	= Get_NY() / 8;
+	//	
+	//int yStart	= 0 * Chunk_Size;
+    //int yEnd 	= Thread_ID == 0 ? Get_NY() : (Thread_ID + 1) * Chunk_Size - 1;
+	//
+	//PixelQueue* pixQ = Create_Queue( hMin, hMax, BoundaryVal, 0, Get_NX(), yStart, yEnd );
+	//bool b = Fill_Queue( pixQ, hMin, hMax, 0, Get_NX(), yStart, yEnd);
+	
+	//PixelQueue* pixQ = Create_Queue( hMin, hMax, BoundaryVal, 0, Get_NX(), 0, Get_NY() );
+
+	//bool b = Fill_Queue( pixQ, hMin, hMax);
+
+
+	return( true );
+}
+
+
+bool CFillMinima::Fill_Queue( PixelQueue *pixQ, int hMin, int hMax, sLong xStart, sLong xEnd, sLong yStart, sLong yEnd )
+{
+	#ifdef _DEBUG 
+	CSG_Simple_Statistics Stats;
+	#endif
+
     PQel *p, *nbrs, *pNbr, *pNext;
     int hCrt = (int)hMin;
-	
-	Process_Set_Text(_TL("Fill minima"));
+
     do 
 	{
-		Set_Progress( hCrt, hMax );
         while (! PQ_empty(pixQ, hCrt)) 
 		{
             p = PQ_first(pixQ, hCrt);
-            nbrs = neighbours(p, Get_NY(), Get_NX());
+            nbrs = neighbours(p, yStart, yEnd, xStart, xEnd );
+            //nbrs = neighbours(p, Get_NY(), Get_NX());
             pNbr = nbrs;
             while (pNbr != NULL) 
 			{
                 int r = pNbr->i;
                 int c = pNbr->j;
-                /* Exclude null area of original image */
+                // Exclude null area of original image 
                 //if (! *((npy_bool*)PyArray_GETPTR2(pNullMask, r, c)))
-                if ( !pInput->is_NoData(c, r) )
+                if ( !m_pInput->is_NoData(c, r) )
 				{
-                    int imgval = pInput->asInt(c,r, false);
-                    int img2val = pImg2->asInt(c,r, false);
+                    int imgval = m_pInput->asInt(c,r, false);
+                    int img2val = m_pImg2->asInt(c,r, false);
                     if (img2val == hMax) 
 					{
                         img2val = std::max(hCrt, imgval);
-                        //*((npy_int16*)PyArray_GETPTR2(pimg2, r, c)) = img2val;
-						pImg2->Set_Value(c,r, img2val, false);
+                        // *((npy_int16*)PyArray_GETPTR2(pimg2, r, c)) = img2val;
+						m_pImg2->Set_Value(c,r, img2val, false);
                         PQ_add(pixQ, pNbr, img2val);
+						
+						#ifdef _DEBUG 
+						Stats += hCrt - img2val;
+						#endif
                     }
                 }
                 pNext = pNbr->next;
@@ -458,5 +406,90 @@ bool CFillMinima::On_Execute(void)
     
     free(pixQ);
 
+	#ifdef _DEBUG 
+	#pragma omp critical
+	{
+		Message_Fmt("Thread No: %d\n", SG_OMP_Get_Thread_Num() );
+		Message_Fmt("Operations: %d, Min: %.2f, Max: %.2f, Mean: %.2f\n", Stats.Get_Count(), Stats.Get_Minimum(), Stats.Get_Maximum(), Stats.Get_Mean());
+	}
+	#endif
 	return true;
+}
+
+PixelQueue* CFillMinima::Create_Queue( int hMin, int hMax, int BoundaryVal, sLong xStart, sLong xEnd, sLong yStart, sLong yEnd )
+{
+	#ifdef _DEBUG 
+	CSG_Simple_Statistics Top_Boundary, Down_Boundary, Left_Boundary, Right_Boundary, Boundary;
+	#endif
+
+	PixelQueue *pixQ = PQ_init(hMin,hMax);
+	
+	CSG_Grid Tick_Off( Get_System(), SG_DATATYPE_Bit);
+	Tick_Off.Set_NoData(0);
+	Tick_Off.Assign_NoData();
+	
+	for( sLong y=yStart; y<yEnd; y++ )
+	{
+		for( sLong x=xStart; x<xEnd; x++ )
+		{
+			if( m_pInput->is_NoData(x,y) )
+			{
+				for( int i=0; i<8; i++ )
+				{
+					int xdiff = x + xnb[i];
+					int ydiff = y + ynb[i];
+					
+					if( m_pInput->is_InGrid( xdiff, ydiff, true ) && Tick_Off.is_NoData(xdiff,ydiff) )
+					{
+						PQel *p = newPix(ydiff, xdiff);
+
+                    	int imgval = m_pInput->asInt(xdiff, ydiff, false);
+                    	m_pInput->Set_Value(xdiff, ydiff, BoundaryVal,false);
+						PQ_add(pixQ, p, BoundaryVal);
+						
+						//PQ_add(pixQ, p, BoundaryVal);
+						
+						Tick_Off.Set_Value(xdiff,ydiff,1.);
+
+						#ifdef _DEBUG 
+						Boundary += imgval;
+						#endif
+					}
+				}
+			}
+			else
+			{
+				if( x == xStart || x == xEnd-1 || y == yStart || y == yEnd-1 )
+				{
+					PQel *p = newPix(y, x);
+                    int imgval = m_pInput->asInt(x, y, false);
+                    m_pInput->Set_Value(x, y, std::max( BoundaryVal, imgval),false);
+					//PQ_add(pixQ, p, imgval);
+					PQ_add(pixQ, p, BoundaryVal);
+					Tick_Off.Set_Value( x, y, 1., true );
+
+					#ifdef _DEBUG 
+					if( 	 y == yEnd-1 && yEnd != Get_NY()) 	Top_Boundary 	+= imgval;
+					else if( y == yStart && yStart != 0 )		Down_Boundary 	+= imgval;
+					else if( x == xEnd-1 && xEnd != Get_NX()) 	Right_Boundary 	+= imgval;
+					else if( x == xStart && xStart != 0 ) 		Left_Boundary 	+= imgval;
+					else 										Boundary 		+= imgval;
+					#endif
+				}
+			}
+		}
+	}
+	#ifdef _DEBUG 
+	#pragma omp critical
+	{
+		Message_Fmt("Thread No: %d\n", SG_OMP_Get_Thread_Num() );
+		if( Boundary.Get_Count() ) 		Message_Fmt("Boundary Cells: %d, Min: %f.2, Max: %f.2\n", Boundary.Get_Count(), Boundary.Get_Minimum(), Boundary.Get_Maximum());
+		if( Top_Boundary.Get_Count() ) 	Message_Fmt("Top Tile Boundary Cells: %d, Min: %f.2, Max: %f.2\n", Top_Boundary.Get_Count(), Top_Boundary.Get_Minimum(), Top_Boundary.Get_Maximum());
+		if( Down_Boundary.Get_Count() ) Message_Fmt("Down Tile Boundary Cells: %d, Min: %f.2, Max: %f.2\n", Down_Boundary.Get_Count(), Down_Boundary.Get_Minimum(), Down_Boundary.Get_Maximum());
+		if( Left_Boundary.Get_Count() ) Message_Fmt("Left Tile Boundary Cells: %d, Min: %f.2, Max: %f.2\n", Left_Boundary.Get_Count(), Left_Boundary.Get_Minimum(), Left_Boundary.Get_Maximum());
+		if( Right_Boundary.Get_Count() )Message_Fmt("Right Tile Boundary Cells: %d, Min: %f.2, Max: %f.2\n", Right_Boundary.Get_Count(), Right_Boundary.Get_Minimum(), Right_Boundary.Get_Maximum());
+	}
+	#endif
+
+	return( pixQ );
 }
--- saga-gis/src/tools/terrain_analysis/ta_preprocessor/fill_minima.h
+++ saga-gis/src/tools/terrain_analysis/ta_preprocessor/fill_minima.h
@@ -56,7 +56,167 @@
 
 //---------------------------------------------------------
 #include <saga_api/saga_api.h>
+#include <limits.h>
 
+/* Routines for handling the hierarchical pixel queue which the
+   algorithm requires.
+*/
+typedef struct PQstruct {
+    int i, j;
+    struct PQstruct *next;
+} PQel;
+    
+typedef struct {
+    PQel *first, *last;
+    int n;
+} PQhdr;
+    
+typedef struct PQ {
+    int hMin;
+    int numLevels;
+    PQhdr *q;
+} PixelQueue;
+
+/* A new pixel structure */
+static PQel *newPix(int i, int j) {
+    PQel *p;
+        
+    p = (PQel *)calloc(1, sizeof(PQel));
+    p->i = i;
+    p->j = j;
+    p->next = NULL;
+    if (i>20000) {
+        //printf("i=%d\\n", i);
+        exit(1);
+    }
+    return p;
+}
+    
+/* Initialize pixel queue */
+static PixelQueue *PQ_init(int hMin, int hMax) {
+    PixelQueue *pixQ;
+    int numLevels, i;
+        
+    pixQ = (PixelQueue *)calloc(1, sizeof(PixelQueue));
+    numLevels = hMax - hMin + 1;
+    pixQ->hMin = hMin;
+    pixQ->numLevels = numLevels;
+    pixQ->q = (PQhdr *)calloc(numLevels, sizeof(PQhdr));
+    for (i=0; i<numLevels; i++) {
+        pixQ->q[i].first = NULL;
+        pixQ->q[i].last = NULL;
+        pixQ->q[i].n = 0;
+    }
+    return pixQ;
+}
+    
+/* Add a pixel at level h */
+static void PQ_add(PixelQueue *pixQ, PQel *p, int h) {
+    int ndx;
+    PQel *current, *newP;
+    PQhdr *thisQ;
+        
+    /* Take a copy of the pixel structure */
+    newP = newPix(p->i, p->j);
+        
+    ndx = h - pixQ->hMin;
+    if (ndx > pixQ->numLevels) {
+        //printf("Level h=%d too large. ndx=%d, numLevels=%d\\n", h, ndx, pixQ->numLevels);
+        exit(1);
+    }
+    if (ndx < 0) {
+        //printf("Ndx is negative, which is not allowed. ndx=%d, h=%d, hMin=%d\n", ndx, h, pixQ->hMin);
+        exit(1);
+    }
+    thisQ = &(pixQ->q[ndx]);
+    /* Add to end of queue at this level */
+    current = thisQ->last;
+    if (current != NULL) {
+        current->next = newP;
+    }
+    thisQ->last = newP;
+    thisQ->n++;
+    /* If head of queue is NULL, make the new one the head */
+    if (thisQ->first == NULL) {
+        thisQ->first = newP;
+    }
+}
+    
+/* Return TRUE if queue at level h is empty */
+static int PQ_empty(PixelQueue *pixQ, int h) {
+    int ndx, empty, n;
+    PQel *current;
+        
+    ndx = h - pixQ->hMin;
+    current = pixQ->q[ndx].first;
+    n = pixQ->q[ndx].n;
+    empty = (current == NULL);
+    if (empty && (n != 0)) {
+        //printf("Empty, but n=%d\\n", n);
+        exit(1);
+    }
+    if ((n == 0) && (! empty)) {
+        //printf("n=0, but not empty\\n");
+        while (current != NULL) {
+            //printf("    h=%d i=%d j=%d\\n", h, current->i, current->j);
+            current = current->next;
+        }
+        exit(1);
+    }
+    return empty;
+}
+    
+/* Return the first element in the queue at level h, and remove it
+   from the queue */
+static PQel *PQ_first(PixelQueue *pixQ, int h) {
+    int ndx;
+    PQel *current;
+    PQhdr *thisQ;
+        
+    ndx = h - pixQ->hMin;
+    thisQ = &(pixQ->q[ndx]);
+    current = thisQ->first;
+    /* Remove from head of queue */
+    if (current != NULL) {
+        thisQ->first = current->next;
+        if (thisQ->first == NULL) {
+            thisQ->last = NULL;
+        }
+        thisQ->n--;
+        if (thisQ->n < 0) {
+            //printf("n=%d in PQ_first()\\n", thisQ->n);
+            exit(1);
+        } else if (thisQ->n == 0) {
+            if (thisQ->first != NULL) {
+                //printf("n=0, but 'first' != NULL. first(i,j) = %d,%d\\n", 
+                //   thisQ->first->i, thisQ->first->j);
+            }
+        }
+    }
+    return current;
+}
+    
+/* Return a list of neighbouring pixels to given pixel p.  */
+static PQel *neighbours(PQel *p, int bRows, int nRows, int bCols, int nCols) {
+    int ii, jj, i, j;
+    PQel *pl, *pNew;
+        
+    pl = NULL;
+    for (ii=-1; ii<=1; ii++) {
+        for (jj=-1; jj<=1; jj++) {
+            if (!(ii == 0 && jj == 0)) {
+                i = p->i + ii;
+                j = p->j + jj;
+                if ((i >= bRows) && (i < nRows) && (j >= bCols) && (j < nCols)) {
+                    pNew = newPix(i, j);
+                    pNew->next = pl;
+                    pl = pNew;
+                }
+            }
+        }
+    }
+    return pl;
+}
 
 ///////////////////////////////////////////////////////////
 //														 //
@@ -74,9 +234,15 @@ public:
 protected:
 
 	virtual bool		On_Execute		(void);
+	
+	PixelQueue* 		Create_Queue	(int hMin, int hMax, int BoundaryVal, sLong xStart, sLong xEnd, sLong yStart, sLong yEnd);
+	bool 				Fill_Queue( PixelQueue *pixQ, int hMin, int hMax, sLong xStart, sLong xEnd, sLong yStart, sLong yEnd );
 
 
 private:
+
+	CSG_Grid *m_pInput 	= NULL;
+	CSG_Grid *m_pImg2 	= NULL;
 	
 };
 
